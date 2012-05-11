ab946df7a11660cb09217e91a5fa3622718af046
Adam Jacob <adam@opscode.com>
2012-05-11 12:23:02 -0700

    Do not include final link targert on ln

--- config/software/erlang.rb
+++ config/software/erlang.rb
@@ -34,7 +34,7 @@ build do
   # set up the erlang include dir
   command "mkdir -p #{install_dir}/embedded/erlang/include"
   %w{ncurses openssl zlib.h zconf.h}.each do |link|
-    command "ln -fs #{install_dir}/embedded/include/#{link} #{install_dir}/embedded/erlang/include/#{link}"
+    command "ln -fs #{install_dir}/embedded/include/#{link} #{install_dir}/embedded/erlang/include"
   end
 
   # TODO: build cross-platform. this is for linux
