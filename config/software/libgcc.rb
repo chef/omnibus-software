c8fdb43a40291628118c744350d6fb4e38a953b9
Stephen Delano <stephen@opscode.com>
2012-08-06 21:01:37 -0700

    make this work on the other linuxes for now

--- config/software/libgcc.rb
+++ config/software/libgcc.rb
@@ -4,11 +4,11 @@ description "On UNIX systems where we bootstrap a compiler, copy the libgcc"
 build do
   if File.exists?("/opt/csw/lib/libgcc_s.so.1")
     command "cp /opt/csw/lib/libgcc_s.so.1 #{install_dir}/embedded/lib/"
-  elsif File.exists?("/usr/local/liblibgcc_s.so.1")
-    command "cp /usr/local/lib/libgcc_s.so.1 #{install_dir}/embedded/lib/"
-  elsif File.exists?("/usr/lib/libgcc_s.so.1")
-    command "cp /usr/lib/libgcc_s.so.1 #{install_dir}/embedded/lib/"
-  else
-    raise "cannot find libgcc_s.so.1 -- where is your gcc compiler?"
+  # elsif File.exists?("/usr/local/liblibgcc_s.so.1")
+  #   command "cp /usr/local/lib/libgcc_s.so.1 #{install_dir}/embedded/lib/"
+  # elsif File.exists?("/usr/lib/libgcc_s.so.1")
+  #   command "cp /usr/lib/libgcc_s.so.1 #{install_dir}/embedded/lib/"
+  # else
+  #   raise "cannot find libgcc_s.so.1 -- where is your gcc compiler?"
   end
 end
