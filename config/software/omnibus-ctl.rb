9f4b087fb9339f0846b7354996db3d90ac0e1034
Seth Chisamore <schisamo@opscode.com>
2012-09-04 12:52:57 -0400

    point at omnibus-ctl master

--- config/software/omnibus-ctl.rb
+++ config/software/omnibus-ctl.rb
@@ -5,9 +5,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@
 #
 
 name "omnibus-ctl"
-version "0.0.1"
+version "master"
 dependencies ["ruby","rubygems","bundler"]
 
 source :git => "git://github.com/opscode/omnibus-ctl.git"
