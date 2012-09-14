88636c4614ee285d8f623abae3cf90bd0f33ad10
Seth Chisamore <schisamo@opscode.com>
2012-09-13 22:44:25 -0400

    install the pure-Ruby version of the readline library
    The readline library that ships with Ruby contains a native extension
    that is known to cause segfaults when compiled against `libedit` on
    certain versions of Ubuntu. Since very few other Ruby libraries use
    readline we'll just make sure a (more compatible) version exists with
    the Ruby install.

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -62,13 +62,14 @@ build do
   command ["./configure",
            "--prefix=#{install_dir}/embedded",
            "--with-opt-dir=#{install_dir}/embedded",
-           "--with-out-ext=iconv,fiddle",
+           "--with-out-ext=iconv,fiddle,readline",
            "--enable-shared",
            "--with-ext=psych",
-           "--disable-install-doc",
-           "--enable-libedit"].join(" "), :env => env
+           "--disable-install-doc"].join(" "), :env => env
   command "make -j #{max_build_jobs}", :env => env
   command "make install", :env => env
+  # install the pure-Ruby version of the readline lib just in case
+  gem "install rb-readline --no-rdoc --no-ri"
 #  if (platform == "solaris2" and Omnibus.config.solaris_compiler == "gcc")
 #    command "/opt/omnibus/bootstrap/bin/chrpath -r #{install_dir}/embedded/lib #{install_dir}/embedded/lib/libruby.so.1"
 #  end
