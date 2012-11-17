0d014179f5dcf4d5a9c78871e07d2b110de72a74
Seth Chisamore <schisamo@opscode.com>
2012-11-17 16:48:02 -0500

    version-manifest: pull build_version from project object
    This will ensure builds of the chef project display the proper 
    overridden version from the chef software definition.

--- config/software/version-manifest.rb
+++ config/software/version-manifest.rb
@@ -3,10 +3,12 @@ description "generates a version manifest file"
 always_build true
 
 project_name = project.name
+project_build_version = project.build_version
+
 build do
   block do
     File.open("#{install_dir}/version-manifest.txt", "w") do |f|
-      f.puts "#{project_name} #{Omnibus::BuildVersion.full}"
+      f.puts "#{project_name} #{project_build_version}"
       f.puts ""
       f.puts Omnibus::Reports.pretty_version_map(project)
     end
