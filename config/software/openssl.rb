a38b96bfacdf45b7c28b6748f9403781ddb401df
Sean OMeara <someara@opscode.com>
2013-01-29 00:17:09 +0000

    [CHEF-2576] SmartOS support for Omnibus Chef
    The SmartOS 1.8.4 release comes very modern packages, including
    GCC 4.7, which is causing most of the trouble.
    
    ncurses patches
    ===============
    Pristine source tarball does not compile out of the box. The patch set
    was taken directly from the pkgsrc, here:
    http://ftp.netbsd.org/pub/pkgsrc/current/pkgsrc/devel/ncurses/patches/
    
    Most of the patches are around the configure scripts and makefiles,
    adding support for various platforms (openbsd for example), put a few
    actually fix up some genuine bugs in the form of mis-matched function
    prototypes that are choking the newer GCC.
    
    I figured it was best to take the entire patch set wholesale instead
    of cherry picking them to avoid confusion.
    
    Ruby patches
    ============
    
    rvm-cflags.patch
    ----------------
    (and the configure flag) was taken from the RVM project to work around
    a well documented problem with the Illumos fork of OpenSolaris:
    
    http://bugs.ruby-lang.org/issues/5384
    https://www.illumos.org/issues/1587
    https://github.com/wayneeseguin/rvm/issues/719
    
    Basically, it ships an old libelf.h that is causing problems.
    
    This has apparently been fixed in Solaris 11, but I can't provide
    links because the lawnmower ate them.
    
    ruby-openssl-1.0.1c.patch
    -------------------------
    This patch was generated by me, and is needed to compile against the
    newer OpenSSL. Like the ncurses patches from pkgsrc, it just fixes
    mismatched function prototypes for the newer GCC.

--- config/software/openssl.rb
+++ config/software/openssl.rb
@@ -48,6 +48,11 @@ build do
           else
             raise "Sorry, #{Omnibus.config.solaris_compiler} is not a valid compiler selection."
           end
+          when "smartos"
+            {
+              "CFLAGS"  => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+              "LDFLAGS" => "-L#{install_dir}/embedded/lib"
+            }
         else
           {
             "CFLAGS" => "-I#{install_dir}/embedded/include",
@@ -64,6 +69,18 @@ build do
                         "--with-zlib-include=#{install_dir}/embedded/include",
                         "zlib",
                         "shared"].join(" ")
+                      when "smartos"
+                            ["/bin/bash ./Configure",
+                             "solaris64-x86_64-gcc",
+                             "--prefix=#{install_dir}/embedded",
+                             "--with-zlib-lib=#{install_dir}/embedded/lib",
+                             "--with-zlib-include=#{install_dir}/embedded/include",
+                            "zlib",
+                            "shared",
+                             "-L#{install_dir}/embedded/lib",
+                             "-I#{install_dir}/embedded/include",
+                             "-R#{install_dir}/embedded/lib",
+                            "-static-libgcc"].join(" ")
                       when "solaris2"
                         if Omnibus.config.solaris_compiler == "gcc"
                           if architecture == "sparc"
