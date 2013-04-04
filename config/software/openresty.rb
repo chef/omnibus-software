95f80956fbd1b84b213f4eadb9089ebaeda71a3a
Ho-Sheng Hsiao <hosh@opscode.com>
2013-04-04 14:35:17 -0700

    [openresty] Added openresty build based upon nginx.
    Added an openresty build definition. It should be a drop-in replacement
    for standard nginx.
    
     - Since openresty is a metapackage, its configure script descends into
       the source files and calls `configure` for nginx. In order to link to
       the embedded shared libraries, we added linker and cc flags to force a
       lookup in the embedded directories first
    
     - The sbin and nginx.conf had to be explicitly set to the omnibus
       convensions.
    
     - Openresty cookbook will automatically detect AIO capabilities and
       automatically enable AIO compile flags. It appears that Ubuntu 10.04
       have the necessary kernel version. However, this requires compiling
       additional AIO libraries. This is a possible improvement for future
       builds.

---
+++ config/software/openresty.rb
@@ -0,0 +1,66 @@
+#
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "openresty"
+version "1.2.7.1"
+
+dependency "pcre"
+dependency "openssl"
+dependency "zlib"
+
+source :url => "http://agentzh.org/misc/nginx/ngx_openresty-1.2.7.1.tar.gz",
+       :md5 => "87e51a962a9cc911ba85dfc95d671117"
+
+relative_path "ngx_openresty-1.2.7.1"
+
+build do
+  env = {
+    "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+  }
+
+  command ["./configure",
+           "--prefix=#{install_dir}/embedded",
+           "--sbin-path=#{install_dir}/embedded/sbin/nginx",
+           "--conf-path=#{install_dir}/embedded/conf/nginx.conf",
+           "--with-http_ssl_module",
+           "--with-debug",
+           # Building Nginx with non-system OpenSSL
+           # http://www.ruby-forum.com/topic/207287#902308
+           "--with-ld-opt=\"-L#{install_dir}/embedded/lib -Wl,-rpath,#{install_dir}/embedded/lib -lssl -lcrypto -ldl -lz\"",
+           "--with-cc-opt=\"-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include\"",
+           # Options inspired by the OpenResty Cookbook
+           '--with-md5-asm',
+           '--with-sha1-asm',
+           '--with-pcre-jit',
+           '--with-luajit', # Necessary?
+           '--without-http_ssi_module',
+           '--without-mail_smtp_module',
+           '--without-mail_imap_module',
+           '--without-mail_pop3_module',
+           '--with-ipv6',
+           # AIO support define in Openresty cookbook. Requires Kernel >= 2.6.22
+           # Ubuntu 10.04 reports: 2.6.32-38-server #83-Ubuntu SMP
+           # However, they require libatomic-ops-dev and libaio
+           #'--with-file-aio',
+           #'--with-libatomic'
+          ].join(" "), :env => env
+  # When building with -j > 1, make emits errors about "Jobserver cannot be reached"
+  command "make", :env => env
+  command "make install"
+end
