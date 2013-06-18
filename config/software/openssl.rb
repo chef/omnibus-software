ac51f2831d95999a14be59e9fc7046f755ba4823
Lamont Granquist <lamont@scriptkiddie.org>
2013-06-18 13:20:20 -0700

    remove vestigial solaris studio compiler

--- config/software/openssl.rb
+++ config/software/openssl.rb
@@ -43,20 +43,11 @@ build do
             "CXX" => "xlC"
         }
         when "solaris2"
-          if Omnibus.config.solaris_compiler == "studio"
-            {
-              "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-              "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
-            }
-          elsif Omnibus.config.solaris_compiler == "gcc"
-            {
-              "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-              "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
-              "LD_OPTIONS" => "-R#{install_dir}/embedded/lib"
-            }
-          else
-            raise "Sorry, #{Omnibus.config.solaris_compiler} is not a valid compiler selection."
-          end
+          {
+            "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+            "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
+            "LD_OPTIONS" => "-R#{install_dir}/embedded/lib"
+          }
         else
           {
             "CFLAGS" => "-I#{install_dir}/embedded/include",
