3030de6dd56ba63bcca14e9195582c2ce586fc69
Seth Chisamore <schisamo@opscode.com>
2013-06-25 10:54:31 -0400

    OpenResty: remove `LDFLAGS` unknown `-R` option
    GCC 4.6+ rejects unknown options, previously passed to the linker. Fix 
    is to remove the `-R` option which is not needed here.

--- config/software/openresty.rb
+++ config/software/openresty.rb
@@ -29,7 +29,7 @@ relative_path "ngx_openresty-#{version}"
 
 build do
   env = {
-    "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
     "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
     "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
   }
