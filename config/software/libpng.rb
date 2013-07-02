6abb8803c523ee8818f22669e7fd9026ec9b10fe
Christopher Maier <cm@opscode.com>
2013-07-02 09:58:56 -0400

    Bump libpng yet again

--- config/software/libpng.rb
+++ config/software/libpng.rb
@@ -16,12 +16,12 @@
 #
 
 name "libpng"
-version "1.5.16"
+version "1.5.17"
 
 dependency "zlib"
 
 source :url => "ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng15/libpng-#{version}.tar.gz",
-       :md5 => "836cc4673e282cb089018e70c3c4a7fa"
+       :md5 => "d2e27dbd8c6579d1582b3f128fd284b4"
 
 relative_path "libpng-#{version}"
 
