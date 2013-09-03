7ef9ab32d63ab1b37f0b521cc6ce6c3263eb052c
kaustubh-d <kaustubh@clogeny.com>
2013-09-03 01:24:35 -0500

    aix: use the 64 bit version of libgcc.

--- config/software/libgcc.rb
+++ config/software/libgcc.rb
@@ -31,7 +31,7 @@ end
 if platform == "aix"
   build do
     if File.exists?("/opt/freeware/lib/libgcc_s.a")
-      command "cp -f /opt/freeware/lib/libgcc_s.a #{install_dir}/embedded/lib/"
+      command "cp -f /opt/freeware/lib/pthread/ppc64/libgcc_s.a #{install_dir}/embedded/lib/"
     else
       raise "cannot find libgcc_s.a -- where is your gcc compiler?"
     end
