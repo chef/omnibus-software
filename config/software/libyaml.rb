a5744ed9cd006acc223f3fc079977c2fa5f64d50
kaustubh-d <kaustubh@clogeny.com>
2013-09-02 23:47:59 -0500

    aix: pass the env so object_mode is picked up during make.

--- config/software/libyaml.rb
+++ config/software/libyaml.rb
@@ -61,6 +61,6 @@ configure_env =
 
 build do
   command "./configure --prefix=#{install_dir}/embedded", :env => configure_env
-  command "make -j #{max_build_jobs}"
-  command "make -j #{max_build_jobs} install"
+  command "make -j #{max_build_jobs}", :env => configure_env
+  command "make -j #{max_build_jobs} install", :env => configure_env
 end
