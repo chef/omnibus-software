1c8dc82f25476b51c4a07f8ee3e9003aee9aba92
kaustubh-d <kaustubh@clogeny.com>
2013-09-04 06:18:10 -0500

    aix: enable rtl for ncurses.

--- config/software/ncurses.rb
+++ config/software/ncurses.rb
@@ -28,7 +28,7 @@ relative_path "ncurses-5.9"
 env = case platform
       when "aix"
         {
-          "LDFLAGS" => "-Wl,-blibpath:#{install_dir}/embedded/lib:/usr/lib:/lib -L#{install_dir}/embedded/lib",
+          "LDFLAGS" => "-brtl -Wl,-blibpath:#{install_dir}/embedded/lib:/usr/lib:/lib -L#{install_dir}/embedded/lib",
           "CFLAGS" => "-I#{install_dir}/embedded/include",
           "OBJECT_MODE" => "64",
           "CC" => "xlc -q64",
