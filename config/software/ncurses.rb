016706180958b0c2c0a84fc64148b29c154fd099
Deepali Jagtap <deepali.jagtap@clogeny.com>
2013-09-30 02:14:37 -0500

    Correction to LDFLAGS

--- config/software/ncurses.rb
+++ config/software/ncurses.rb
@@ -29,7 +29,7 @@ relative_path "ncurses-5.9"
 env = case platform
       when "aix"
         {
-          "LDFLAGS" => "-brtl -Wl,-blibpath:#{install_dir}/embedded/lib:/usr/lib:/lib -L#{install_dir}/embedded/lib",
+          "LDFLAGS" => "-Wl,-brtl -Wl,-blibpath:#{install_dir}/embedded/lib:/usr/lib:/lib -L#{install_dir}/embedded/lib",
           "CFLAGS" => "-I#{install_dir}/embedded/include",
           "OBJECT_MODE" => "64",
           "CC" => "gcc -maix64",
