aea79c69383024128200c4a955fcbeddf9032e7e
Robby Dyer <rdyer@tropo.com>
2013-10-17 10:03:15 -0400

    Adding libpcap.rb

---
+++ config/software/libpcap.rb
@@ -0,0 +1,37 @@
+#
+# Copyright:: Copyright (c) 2013 Robby Dyer
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+name "libpcap"
+version "1.4.0"
+
+source :url => "http://www.tcpdump.org/release/libpcap-#{version}.tar.gz",
+       :md5 => "56e88a5aabdd1e04414985ac24f7e76c"
+
+relative_path "libpcap-#{version}"
+
+env = {
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+}
+
+build do
+  command ["./configure",
+           "--prefix=#{install_dir}/embedded",
+           ].join(" "), :env => env
+  command "make -j #{max_build_jobs}"
+  command "make install", :env => env
+end
