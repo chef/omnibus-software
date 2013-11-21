f659bc4e02316161200386156e44f101026d1623
waltaskew <waltaskew@gmail.com>
2013-11-21 16:21:10 -0600

    add copyright notice

--- config/software/bzip2.rb
+++ config/software/bzip2.rb
@@ -1,3 +1,19 @@
+#
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Install bzip2 and its shared library, libbz2.so
 # This library object is required for building Python with the bz2 module,
 # and should be picked up automatically when building Python.
