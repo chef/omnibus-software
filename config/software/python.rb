7a1e44d2d8546e13394871658eef415cb8cbbe8d
waltaskew <waltaskew@gmail.com>
2013-12-19 12:50:53 -0600

    add bzip2 dep to python

--- config/software/python.rb
+++ config/software/python.rb
@@ -21,6 +21,7 @@ version "2.7.5"
 dependency "ncurses"
 dependency "zlib"
 dependency "openssl"
+dependency "bzip2"
 
 source :url => "http://python.org/ftp/python/#{version}/Python-#{version}.tgz",
        :md5 => 'b4f01a1d0ba0b46b05c73b2ac909b1df'
