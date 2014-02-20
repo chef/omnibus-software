ef8269831294d9e5bc660466980eb9ca2d7299ab
Lamont Granquist <lamont@scriptkiddie.org>
2014-02-19 17:50:10 -0800

    remove solaris studio compiler cruft

--- config/software/popt.rb
+++ config/software/popt.rb
@@ -5,9 +5,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,21 +26,11 @@ relative_path "popt-1.16"
 env =
   case platform
   when "solaris2"
-    if Omnibus.config.solaris_compiler == "studio"
     {
       "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
       "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
       "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
     }
-    elsif Omnibus.config.solaris_compiler == "gcc"
-    {
-      "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
-      "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-    }
-   else
-    raise "Sorry, #{Omnibus.config.solaris_compiler} is not a valid compiler selection."
-   end
   else
     {
       "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
