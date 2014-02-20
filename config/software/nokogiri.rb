c6f6d04a6fa21fd2f4634fcc5cb25288d6c14c22
Lamont Granquist <lamont@scriptkiddie.org>
2014-02-19 17:47:51 -0800

    nokogiri 1.6.1 support
    - we need to override nokogiri's bundled libxml2 and libxslt which
      will link themselves against system libs and their deps will fail
      our healthcheck
    - also on macs we need to point pkg-config at the embedded pkg-config
      files otherwise we'll wind up linking against system libs (this should
      be baked into omnibus by default!)

--- config/software/nokogiri.rb
+++ config/software/nokogiri.rb
@@ -20,15 +20,23 @@ version "1.5.4"
 
 dependencies ["ruby", "rubygems", "libxml2", "libxslt", "libiconv"]
 
+# nokogiri uses pkg-config, and on a mac that will find the system pkg-config
+# which will find the system pkg-configs which will pull in libicucore from the
+# libxml2 pkg-config spec.  override pkg-configs path here to point into our
+# /opt/chef/embedded pkg-configs.  this should probably be done more generally,
+# in core ominbus-ruby.
+env = { "PKG_CONFIG_PATH" => "#{install_dir}/embedded/lib/pkgconfig", "NOKOGIRI_USE_SYSTEM_LIBRARIES" => "true" }
+
 build do
   gem ["install",
        "nokogiri",
        "-v #{version}",
        "--",
+       "--use-system-libraries",
        "--with-xml2-lib=#{install_dir}/embedded/lib",
        "--with-xml2-include=#{install_dir}/embedded/include/libxml2",
        "--with-xslt-lib=#{install_dir}/embedded/lib",
        "--with-xslt-include=#{install_dir}/embedded/include/libxslt",
        "--with-iconv-include=#{install_dir}/embedded/include",
-       "--with-iconv-lib=#{install_dir}/embedded/lib"].join(" ")
+       "--with-iconv-lib=#{install_dir}/embedded/lib"].join(" "), :env => env
 end
