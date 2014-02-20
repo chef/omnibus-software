3706872ccc25ea1ae08f0d42085745731454ade5
Lamont Granquist <lamont@scriptkiddie.org>
2014-02-19 17:43:14 -0800

    removing studio compiler, aux gem bindir fix
    - aux gems should have gone into /opt/chef/embedded/bin

--- config/software/chef.rb
+++ config/software/chef.rb
@@ -33,20 +33,11 @@ always_build true
 env =
   case platform
   when "solaris2"
-    if Omnibus.config.solaris_compiler == "studio"
-    {
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
-    }
-    elsif Omnibus.config.solaris_compiler == "gcc"
     {
       "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
       "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
       "LD_OPTIONS" => "-R#{install_dir}/embedded/lib"
     }
-    else
-      raise "Sorry, #{Omnibus.config.solaris_compiler} is not a valid compiler selection."
-    end
   when "aix"
     {
       "LDFLAGS" => "-Wl,-blibpath:#{install_dir}/embedded/lib:/usr/lib:/lib -L#{install_dir}/embedded/lib",
@@ -134,7 +125,6 @@ build do
 
   gem ["install",
        auxiliary_gems.join(" "),
-       "-n #{install_dir}/bin",
        "--no-rdoc --no-ri"].join(" "), :env => env.merge({"PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}"})
 
   #
