cf6e49836c52498a34a5a7ce27b91eb8c0377126
danielsdeleo <dan@opscode.com>
2014-03-03 15:35:08 -0800

    `-static-libgcc` doesn't work on Mac
    It's not clear why this was used in the first place. `git blame` pointed
    at Adam J, but he didn't remember. I thought this might be required on
    linux but I was able to successfully build on Ubuntu without this
    option.

--- config/software/popt.rb
+++ config/software/popt.rb
@@ -34,7 +34,7 @@ env =
   else
     {
       "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
+      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
       "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
     }
   end
