7ff7430098952facb73522860fdb4b3801be2ac1
sersut <serdar@opscode.com>
2014-03-17 15:21:34 -0700

    Add pkg-config that is needed by makedepend.

--- config/software/pkg-config.rb
+++ config/software/pkg-config.rb
@@ -15,17 +15,13 @@
 # limitations under the License.
 #
 
-name "makedepend"
-default_version "1.0.5"
+name "pkg-config"
+default_version "0.28"
 
-source :url => 'http://xorg.freedesktop.org/releases/individual/util/makedepend-1.0.5.tar.gz',
-  :md5 => 'efb2d7c7e22840947863efaedc175747'
+source :url => 'http://pkgconfig.freedesktop.org/releases/pkg-config-0.28.tar.gz',
+  :md5 => 'aa3c86e67551adc3ac865160e34a2a0d'
 
-relative_path 'makedepend-1.0.5'
-
-dependency "xproto"
-dependency 'util-macros'
-dependency 'pkg-config'
+relative_path 'pkg-config-0.28'
 
 configure_env =
   case platform
@@ -59,15 +55,10 @@ configure_env =
     }
   end
 
-configure_env["PKG_CONFIG_PATH"] = "#{install_dir}/embedded/lib/pkgconfig" +
-  File::PATH_SEPARATOR +
-  "#{install_dir}/embedded/share/pkgconfig"
-
-# For pkg-config
-configure_env["PATH"] = "#{install_dir}/embedded/bin" + File::PATH_SEPARATOR + ENV["PATH"]
+paths = [ "#{install_dir}/embedded/bin/pkgconfig" ]
 
 build do
-  command "./configure --prefix=#{install_dir}/embedded", :env => configure_env
+  command "./configure --prefix=#{install_dir}/embedded --disable-debug --disable-host-tool --with-internal-glib --with-pc-path=#{paths*':'}", :env => configure_env
   command "make -j #{max_build_jobs}", :env => configure_env
   command "make -j #{max_build_jobs} install", :env => configure_env
 end
