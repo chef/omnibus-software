02d06a74c02340b592e1864e7ab843bc14fa352a
Seth Falcon <seth@opscode.com>
2014-03-24 12:19:34 -0700

    Update preparation for git-cache use

--- config/software/preparation.rb
+++ config/software/preparation.rb
@@ -17,11 +17,14 @@
 
 name "preparation"
 description "the steps required to preprare the build"
+default_version '1.0.0'
 
 build do
   block do
     %w{embedded/lib embedded/bin bin}.each do |dir|
-      FileUtils.mkdir_p(File.expand_path(dir, install_dir))
+      dir_fullpath = File.expand_path(File.join(install_dir, dir))
+      FileUtils.mkdir_p(dir_fullpath)
+      FileUtils.touch(File.join(dir_fullpath, '.gitkeep'))
     end
   end
 end
