9742e0abf409bd776dd4ce9db5996487f9400d24
Matt Whiteley <mwhiteley@engineyard.com>
2014-04-04 10:11:48 -0700

    Ruby 1.9.3-p545

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -16,7 +16,7 @@
 #
 
 name "ruby"
-default_version "1.9.3-p484"
+default_version "1.9.3-p545"
 
 dependency "zlib"
 dependency "ncurses"
@@ -27,8 +27,8 @@ dependency "libiconv"
 dependency "gdbm"
 dependency "libgcc" if (platform == "solaris2" and Omnibus.config.solaris_compiler == "gcc")
 
-version "1.9.3-p484" do
-  source md5: '8ac0dee72fe12d75c8b2d0ef5d0c2968'
+version "1.9.3-p545" do
+  source md5: '8e8f6e4d7d0bb54e0edf8d9c4120f40c'
 end
 
 version "2.1.1" do
