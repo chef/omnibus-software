c418c27c3e7178972aa324a0b935032a5db7c2af
Remi Hakim <remi@datadoghq.com>
2014-04-23 15:36:44 -0400

    Add psutil software def

---
+++ config/software/psutil.rb
@@ -0,0 +1,9 @@
+name "psutil"
+default_version "2.1.0"
+
+dependency "python"
+dependency "pip"
+
+build do
+  command "#{install_dir}/embedded/bin/pip install -I --build #{project_dir} --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+end
