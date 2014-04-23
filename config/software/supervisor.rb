f26eb361cdd15bcbb0608243a861721072158512
Remi Hakim <remi@datadoghq.com>
2014-04-23 15:23:16 -0400

    Add supervisor software definitiopn

---
+++ config/software/supervisor.rb
@@ -0,0 +1,9 @@
+name "supervisor"
+default_version "3.0"
+
+dependency "python"
+dependency "pip"
+
+build do
+  command "#{install_dir}/embedded/bin/pip install -I --build #{project_dir} --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+end
