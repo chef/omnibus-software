3bd36e9b5d9eadb4ebeb842df4be6c26af01db77
Remi Hakim <remi@datadoghq.com>
2014-05-07 11:38:11 -0400

    pycurl is dependency of tornado, tornado can also take use of futures

--- config/software/tornado.rb
+++ config/software/tornado.rb
@@ -3,6 +3,8 @@ default_version "3.2"
 
 dependency "python"
 dependency "pip"
+dependency "pycurl"
+dependency "futures"
 
 build do
   command "#{install_dir}/embedded/bin/pip install -I --build #{project_dir} --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
