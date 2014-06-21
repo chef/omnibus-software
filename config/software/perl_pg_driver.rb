924427dcfecb6cf0a1cf8a4f2b3aa32b988707b1
Seth Chisamore <schisamo@getchef.com>
2014-06-21 15:58:25 -0400

    Use `cpanminus` to install Perl module deps
    We’ll also ensure we don’t run tests when installing CPAN modules. Go
    read this if you need a reason why this is insane:
    http://www.openswartz.com/2012/01/31/stop-running-tests-on-install/
    
    We’ve hit issues where a modules test’s fail (and thus the module is NOT
    installed) and the default `cpan` client continues along and exits with a
    zero status!

--- config/software/perl_pg_driver.rb
+++ config/software/perl_pg_driver.rb
@@ -1,15 +1,13 @@
 name "perl_pg_driver"
 
 dependency "perl"
+dependency "cpanminus"
 dependency "postgresql"
-
-# Ensure we install with the properly configured embedded `cpan` client
-omnibus_cpan_client = "#{install_dir}/embedded/bin/cpan -j #{cache_dir}/cpan/OmnibusConfig.pm"
 
 env = {
   "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}"
 }
 
 build do
-  command "yes | #{omnibus_cpan_client} DBD::Pg", :env => env
+  command "cpanm -v --notest DBD::Pg", :env => env
 end
--- config/software/sqitch.rb
+++ config/software/sqitch.rb
@@ -2,6 +2,7 @@ name "sqitch"
 default_version "0.973"
 
 dependency "perl"
+dependency "cpanminus"
 
 source :url => "http://www.cpan.org/authors/id/D/DW/DWHEELER/App-Sqitch-#{version}.tar.gz",
        :md5 => "0994e9f906a7a4a2e97049c8dbaef584"
@@ -12,13 +13,10 @@ env = {
   "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}"
 }
 
-# Ensure we install with the properly configured embedded `cpan` client
-omnibus_cpan_client = "#{install_dir}/embedded/bin/cpan -j #{cache_dir}/cpan/OmnibusConfig.pm"
-
 # See https://github.com/theory/sqitch for more
 build do
   command "perl Build.PL", :env => env
-  command "./Build installdeps --cpan_client '#{omnibus_cpan_client}'", :env => env
+  command "./Build installdeps --cpan_client 'cpanm -v --notest'", :env => env
   command "./Build", :env => env
   command "./Build install", :env => env
 end
