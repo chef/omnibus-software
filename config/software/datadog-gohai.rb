fb594a30a257a688cdb1a1e4f17de34b92e870a5
Tristan Michelet <tristan@datadoghq.com>
2014-06-26 20:55:17 +0000

    Revert "use go_path as a variable"
    This reverts commit 91bf251df91c3c70c7196c39a8b8e5b3d06e2ee3.

--- config/software/datadog-gohai.rb
+++ config/software/datadog-gohai.rb
@@ -2,15 +2,13 @@ name "datadog-gohai"
 default_version "last-stable"
 always_build true
 
-go_path "/var/cache/omnibus/src/datadog-gohai"
-
 env = {
   "GOROOT" => "/usr/local/go",
-  "GOPATH" => go_path
+  "GOPATH" => "/var/cache/omnibus/src/datadog-gohai"
 }
 
 build do
    command "$GOROOT/bin/go get -d -u github.com/DataDog/gohai", :env => env
-   command "git checkout #{default_version} && git pull", :env => env, :cwd => "#{go_path}/src/github.com/DataDog/gohai"
+   command "git checkout #{default_version} && git pull", :env => env, :cwd => "/var/cache/omnibus/src/datadog-gohai/src/github.com/DataDog/gohai"
    command "$GOROOT/bin/go build -o #{install_dir}/bin/gohai $GOPATH/src/github.com/DataDog/gohai/gohai.go", :env => env
 end
