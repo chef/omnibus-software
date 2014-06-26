f7b63d68882acbe60d8b8d670ca00fe3873c5fb7
Tristan Michelet <tristan@datadoghq.com>
2014-06-26 20:14:45 +0000

    add env to gohai software command

--- config/software/datadog-gohai.rb
+++ config/software/datadog-gohai.rb
@@ -9,6 +9,6 @@ env = {
 
 build do
    command "$GOROOT/bin/go get -d -u github.com/DataDog/gohai", :env => env
-   command "cd $GOPATH/src/github.com/DataDog/gohai/ && git checkout #{default_version} %% git pull"
+   command "cd $GOPATH/src/github.com/DataDog/gohai/ && git checkout #{default_version} %% git pull", :env => env
    command "$GOROOT/bin/go build -o #{install_dir}/bin/gohai $GOPATH/src/github.com/DataDog/gohai/gohai.go", :env => env
 end
