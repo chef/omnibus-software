e85e381d9b25e11228be4a91b5c91d9bc25cd1e5
Remi Hakim <remi@datadoghq.com>
2014-07-03 17:59:11 -0400

    Use proper dir

--- config/software/python-rrdtool.rb
+++ config/software/python-rrdtool.rb
@@ -7,11 +7,11 @@ build do
     command "wget http://dd-agent.s3.amazonaws.com/python-rrdtool/deb/#{ENV['ARCH']}/rrdtool.so", :cwd => "#{install_dir}/embedded/lib/python2.7/"
   elsif ENV['PKG_TYPE'] == 'rpm'
    command "sudo yum -y install cairo-devel libxml2-devel pango-devel pango libpng-devel freetype freetype-devel libart_lgpl-devel gcc groff perl-ExtUtils-MakeMaker"
-   command "sudo wget http://files.directadmin.com/services/9.0/ExtUtils-MakeMaker-6.31.tar.gz", :cwd => "/root/"
-   command "sudo tar xvzf ExtUtils-MakeMaker-6.31.tar.gz", :cwd => "/root/"
-   command "sudo perl Makefile.PL", :cwd => "/root/ExtUtils-MakeMaker-6.31"
-   command "sudo make", :cwd => "/root/ExtUtils-MakeMaker-6.31"
-   command "sudo make install", :cwd => "/root/ExtUtils-MakeMaker-6.31"
+   command "sudo wget http://files.directadmin.com/services/9.0/ExtUtils-MakeMaker-6.31.tar.gz", :cwd => "/tmp/"
+   command "sudo tar xvzf ExtUtils-MakeMaker-6.31.tar.gz", :cwd => "/tmp/"
+   command "sudo perl Makefile.PL", :cwd => "/tmp/ExtUtils-MakeMaker-6.31"
+   command "sudo make", :cwd => "/tmp/ExtUtils-MakeMaker-6.31"
+   command "sudo make install", :cwd => "/tmp/ExtUtils-MakeMaker-6.31"
    command "sudo wget http://oss.oetiker.ch/rrdtool/pub/rrdtool-#{version}.tar.gz", :cwd => "/opt/"
    command "sudo tar -xzvf rrdtool-#{version}.tar.gz", :cwd => "/opt/"
    command "sudo .configure", :cwd => "/opt/rrdtool-#{version}", :env => {"PKG_CONFIG_PATH" => "/usr/lib/pkgconfig/"}
