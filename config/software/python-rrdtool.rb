db4a3cc85e3d5aaf1ca7c27383611b5dc8c453c4
Remi Hakim <remi@datadoghq.com>
2014-07-03 13:09:19 -0400

    Update python-rrdtool.rb

--- config/software/python-rrdtool.rb
+++ config/software/python-rrdtool.rb
@@ -3,5 +3,10 @@ default_version "s3"
 dependency "python"
 
 build do
-   command "wget http://dd-agent.s3.amazonaws.com/python-rrdtool/#{ENV['ARCH']}/rrdtool.so", :cwd => "#{install_dir}/embedded/lib/python2.7/"
+  if ENV['PKG_TYPE'] == 'deb'
+    command "wget http://dd-agent.s3.amazonaws.com/python-rrdtool/deb/#{ENV['ARCH']}/rrdtool.so", :cwd => "#{install_dir}/embedded/lib/python2.7/"
+  elsif ENV['PKG_TYPE'] == 'rpm'
+    command "wget http://dd-agent.s3.amazonaws.com/python-rrdtool/rpm/#{ENV['ARCH']}/rrdtoolmodule.so", :cwd => "#{install_dir}/embedded/lib/python2.7/"
+  end
+  
 end
