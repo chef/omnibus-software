46e7b054c42e4c84215ae087561a77c7f49e1be5
Léo Cavaillé <leo@datadoghq.com>
2014-07-15 19:15:58 -0400

    Add superlance to our configs

---
+++ config/software/superlance.rb
@@ -0,0 +1,9 @@
+name "superlance"
+default_version "0.10"
+
+dependency "python"
+dependency "pip"
+
+build do
+  command "#{install_dir}/embedded/bin/pip install -I --build #{project_dir} --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+end
