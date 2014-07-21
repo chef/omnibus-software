050fdc3e36593495164388e3304e6516f853b276
Remi Hakim <remi@datadoghq.com>
2014-07-21 16:56:10 -0400

    Revert "Install both snakebite 1.x and 2.x"
    This reverts commit 8fad2377adc668ecac02b5d69f6b21c57f33a015.

---
+++ config/software/snakebite.rb
@@ -0,0 +1,9 @@
+name "snakebite"
+default_version "1.3.9"
+
+dependency "python"
+dependency "pip"
+
+build do
+  command "#{install_dir}/embedded/bin/pip install -I --build #{project_dir} --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+end
