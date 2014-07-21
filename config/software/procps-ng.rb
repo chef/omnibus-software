4227ab65ffff2b83de47b00c7658be892f3b3be6
Remi Hakim <remi@datadoghq.com>
2014-07-21 13:43:49 -0400

    Move procps binaries to the right location

--- config/software/procps-ng.rb
+++ config/software/procps-ng.rb
@@ -19,4 +19,6 @@ build do
   :env => env)
   command "make -j #{max_build_jobs}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
   command "make install"
+  command "mv #{install_dir}/embedded/usr/bin/* #{install_dir}/embedded/bin/"
+  command "rm -rf #{install_dir}/embedded/usr/bin"
 end
