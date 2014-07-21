600e51b5979aa0491786dae05e37aae17d7433cb
Remi Hakim <remi@datadoghq.com>
2014-07-21 16:25:59 -0400

    Update cacerts.rb

--- config/software/cacerts.rb
+++ config/software/cacerts.rb
@@ -22,15 +22,7 @@ version "2014.07.15" do
   source :md5 => "fd48275847fa10a8007008379ee902f1"
 end
 
-version "2014.04.22" do
-  source :md5 => "9f92a0d9f605e227ae068e605f4c86fa"
-end
 
-version "2014.01.28" do
-  source :md5 => "5d108f8ab86afacc6663aafca8604dd3"
-end
-
-source :url => "http://curl.haxx.se/ca/cacert.pem"
 relative_path "cacerts-#{version}"
 
 build do
@@ -44,11 +36,11 @@ build do
     # fix worked. Rather than trying to fix this now, we're filing a bug and copying the cacert.pem
     # directly from the cache instead.
 
-    FileUtils.cp(File.expand_path("cacert.pem", Config.cache_dir),
+    FileUtils.cp(File.expand_path("cacert.pem", Omnibus.config.cache_dir),
                  File.expand_path("embedded/ssl/certs/cacert.pem", install_dir))
   end
 
-  unless Ohai['platform'] == 'windows'
+  unless platform == 'windows'
     command "ln -sf #{install_dir}/embedded/ssl/certs/cacert.pem #{install_dir}/embedded/ssl/cert.pem"
   end
 end
