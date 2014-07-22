ee69b04c8e9c1b27bc33b5f05d73ea3a74c62c46
Remi Hakim <remi@datadoghq.com>
2014-07-22 11:52:26 -0400

    Update ntplib.rb

--- config/software/ntplib.rb
+++ config/software/ntplib.rb
@@ -5,5 +5,6 @@ dependency "python"
 dependency "pip"
 
 build do
+  command "rm -rf /var/cache/omnibus/src/ntplib"
   command "#{install_dir}/embedded/bin/pip install --force-reinstall -I --build #{project_dir} --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
 end
