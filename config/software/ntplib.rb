f3c59b85d3a3f283c53bce71561e88782ca3584d
Remi Hakim <remi@datadoghq.com>
2014-07-22 11:45:05 -0400

    Force reinstall as it makes the build to fail otherwise
    See http://corgi.datad0g.com:8080/job/build-agent-omnibus/ARCHITECTURE=x64,DISTRIBUTION=debian/439/console

--- config/software/ntplib.rb
+++ config/software/ntplib.rb
@@ -5,5 +5,5 @@ dependency "python"
 dependency "pip"
 
 build do
-  command "#{install_dir}/embedded/bin/pip install -I --build #{project_dir} --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+  command "#{install_dir}/embedded/bin/pip install --force-reinstall -I --build #{project_dir} --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
 end
