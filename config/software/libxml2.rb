8ee075ac15516287b4b0e282c9918d6cfc35d16c
Seth Vargo <sethvargo@gmail.com>
2014-07-25 17:37:09 -0400

    Cleanup libxml2

--- config/software/libxml2.rb
+++ config/software/libxml2.rb
@@ -34,18 +34,15 @@ source url: "ftp://xmlsoft.org/libxml2/libxml2-#{version}.tar.gz"
 relative_path "libxml2-#{version}"
 
 build do
-  cmd = ["./configure",
-         "--prefix=#{install_dir}/embedded",
-         "--with-zlib=#{install_dir}/embedded",
-         "--with-iconv=#{install_dir}/embedded",
-         "--without-python",
-         "--without-icu"].join(" ")
-  env = {
-    "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-  }
-  command cmd, :env => env
-  command "make -j #{max_build_jobs}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
-  command "make install", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
+  env = with_standard_compiler_flags
+
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded" \
+          " --with-zlib=#{install_dir}/embedded" \
+          " --with-iconv=#{install_dir}/embedded" \
+          " --without-python" \
+          " --without-icu", env: env
+
+  command "make -j #{max_build_jobs}", env: env
+  command "make install", env: env
 end
