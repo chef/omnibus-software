2effa16437e37de88af0c40888d677e85a4200cb
Seth Vargo <sethvargo@gmail.com>
2014-07-25 15:41:29 -0400

    Version omnibus-software with omnibus

--- lib/omnibus-software.rb
+++ lib/omnibus-software.rb
@@ -1,13 +1,12 @@
 #
-# Copyright:: Copyright (c) 2012-2014 Chef Software, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2012-2014 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,5 +14,6 @@
 # limitations under the License.
 #
 
-require "omnibus-software/version"
-
+module OmnibusSoftware
+  VERSION = '4.0.0'
+end
