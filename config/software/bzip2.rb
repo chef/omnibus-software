a2bf9600e67e9f1da6b8210a5b1850169e90a86e
Seth Vargo <sethvargo@gmail.com>
2014-07-25 16:29:45 -0400

    Cleanup bzip2

--- config/software/bzip2.rb
+++ config/software/bzip2.rb
@@ -23,24 +23,24 @@ default_version "1.0.6"
 dependency "zlib"
 dependency "openssl"
 
-source :url => "http://www.bzip.org/#{version}/#{name}-#{version}.tar.gz",
-       :md5 => "00b516f4704d4a7cb50a1d97e6e8e15b"
+source url: "http://www.bzip.org/#{version}/#{name}-#{version}.tar.gz",
+       md5: "00b516f4704d4a7cb50a1d97e6e8e15b"
 
 relative_path "#{name}-#{version}"
 
-prefix="#{install_dir}/embedded"
-libdir="#{prefix}/lib"
-
-env = {
-  "LDFLAGS" => "-L#{libdir} -I#{prefix}/include",
-  "CFLAGS" => "-L#{libdir} -I#{prefix}/include -fPIC",
-  "LD_RUN_PATH" => libdir
-}
-
 build do
-  patch :source => 'makefile_take_env_vars.patch'
-  patch :source => 'soname_install_dir.patch' if mac_os_x_mavericks?
-  command "make PREFIX=#{prefix} VERSION=#{version}", :env => env
-  command "make PREFIX=#{prefix} VERSION=#{version} -f Makefile-libbz2_so", :env => env
-  command "make install VERSION=#{version} PREFIX=#{prefix}", :env => env
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  # Avoid warning where .rodata cannot be used when making a shared object
+  env["CFLAGS"] << " -fPIC"
+
+  # The list of arguments to pass to make
+  args = "PREFIX='#{install_dir}/embedded' VERSION='#{version}'"
+
+  patch source: 'makefile_take_env_vars.patch'
+  patch source: 'soname_install_dir.patch' if mac_os_x_mavericks?
+
+  command "make #{args}", env: env
+  command "make #{args} -f Makefile-libbz2_so", env: env
+  command "make #{args} install", env: env
 end
