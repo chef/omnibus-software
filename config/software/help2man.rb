935fdc04a91e7a11f498e0f8dd2217d1b6e7be3c
Seth Vargo <sethvargo@gmail.com>
2014-07-25 17:13:20 -0400

    Cleanup help2man

--- config/software/help2man.rb
+++ config/software/help2man.rb
@@ -17,13 +17,14 @@
 name "help2man"
 default_version "1.40.5"
 
-source :url => "http://ftp.gnu.org/gnu/help2man/help2man-1.40.5.tar.gz",
-       :md5 => "75a7d2f93765cd367aab98986a75f88c"
+source url: "http://ftp.gnu.org/gnu/help2man/help2man-1.40.5.tar.gz",
+       md5: "75a7d2f93765cd367aab98986a75f88c"
 
 relative_path "help2man-1.40.5"
 
 build do
   command "./configure --prefix=#{install_dir}/embedded"
+
   command "make"
   command "make install"
 end
