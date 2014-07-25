1050a6df20255c13896159607927a23c7c07d2e7
Seth Vargo <sethvargo@gmail.com>
2014-07-25 17:14:35 -0400

    Cleanup icu

--- config/software/icu.rb
+++ config/software/icu.rb
@@ -17,23 +17,17 @@
 name "icu"
 default_version "4.8.1.1"
 
-source :url => "http://download.icu-project.org/files/icu4c/4.8.1.1/icu4c-4_8_1_1-src.tgz",
-:md5 => "ea93970a0275be6b42f56953cd332c17"
+source url: "http://download.icu-project.org/files/icu4c/4.8.1.1/icu4c-4_8_1_1-src.tgz",
+       md5: "ea93970a0275be6b42f56953cd332c17"
 
 relative_path "icu"
 
-working_dir = "#{project_dir}/source"
-
 build do
-  command("./configure --prefix=#{install_dir}/embedded",
-          :env => {
-            "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
-          },
-          :cwd => working_dir)
-  command("make -j #{max_build_jobs}",
-          :env => {
-            "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-          },
-          :cwd => working_dir)
-  command "make install", :cwd => working_dir
+  env = with_standard_compiler_flags
+
+  Dir.chdir("#{project_dir}/source") do
+    command "./configure --prefix=#{install_dir}/embedded", env: env
+    command "make -j #{max_build_jobs}", env: env
+    command "make install", env: env
+  end
 end
