607c811840497050152b4f291e808c5f2ca624e4
Seth Vargo <sethvargo@gmail.com>
2014-07-25 19:52:14 -0400

    Cleanup makedepend

--- config/software/makedepend.rb
+++ config/software/makedepend.rb
@@ -17,56 +17,20 @@
 name "makedepend"
 default_version "1.0.5"
 
-source :url => 'http://xorg.freedesktop.org/releases/individual/util/makedepend-1.0.5.tar.gz',
-  :md5 => 'efb2d7c7e22840947863efaedc175747'
+source url: "http://xorg.freedesktop.org/releases/individual/util/makedepend-1.0.5.tar.gz",
+       md5: "efb2d7c7e22840947863efaedc175747"
 
-relative_path 'makedepend-1.0.5'
+relative_path "makedepend-1.0.5"
 
 dependency "xproto"
-dependency 'util-macros'
-dependency 'pkg-config'
-
-configure_env =
-  case Ohai['platform']
-  when "aix"
-    {
-      "CC" => "xlc -q64",
-      "CXX" => "xlC -q64",
-      "LD" => "ld -b64",
-      "CFLAGS" => "-q64 -I#{install_dir}/embedded/include -O",
-      "LDFLAGS" => "-q64 -Wl,-blibpath:/usr/lib:/lib",
-      "OBJECT_MODE" => "64",
-      "ARFLAGS" => "-X64 cru",
-      "LD" => "ld -b64",
-      "OBJECT_MODE" => "64",
-      "ARFLAGS" => "-X64 cru "
-    }
-  when "mac_os_x"
-    {
-      "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "CFLAGS" => "-I#{install_dir}/embedded/include -L#{install_dir}/embedded/lib"
-    }
-  when "solaris2"
-    {
-      "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
-    }
-  else
-    {
-      "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "CFLAGS" => "-I#{install_dir}/embedded/include -L#{install_dir}/embedded/lib"
-    }
-  end
-
-configure_env["PKG_CONFIG_PATH"] = "#{install_dir}/embedded/lib/pkgconfig" +
-  File::PATH_SEPARATOR +
-  "#{install_dir}/embedded/share/pkgconfig"
-
-# For pkg-config
-configure_env["PATH"] = "#{install_dir}/embedded/bin" + File::PATH_SEPARATOR + ENV["PATH"]
+dependency "util-macros"
+dependency "pkg-config"
 
 build do
-  command "./configure --prefix=#{install_dir}/embedded", :env => configure_env
-  command "make -j #{max_build_jobs}", :env => configure_env
-  command "make -j #{max_build_jobs} install", :env => configure_env
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command "./configure --prefix=#{install_dir}/embedded", env: env
+
+  command "make -j #{max_build_jobs}", env: env
+  command "make -j #{max_build_jobs} install", env: env
 end
