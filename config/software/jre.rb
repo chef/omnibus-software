8a6134dbf66eeb4ea251c3627bc77d88df1582ea
Seth Vargo <sethvargo@gmail.com>
2014-07-25 17:16:30 -0400

    Cleanup jre

--- config/software/jre.rb
+++ config/software/jre.rb
@@ -25,23 +25,19 @@ whitelist_file "jre/lib"
 whitelist_file "jre/plugin"
 
 if Ohai['kernel']['machine'] =~ /x86_64/
-  # TODO: download x86 version on x86 machines
-  source :url => "http://download.oracle.com/otn-pub/java/jdk/7u3-b04/jre-7u3-linux-x64.tar.gz",
-         :md5 => "3d3e206cea84129f1daa8e62bf656a28",
-         :cookie => 'oraclelicense=accept-securebackup-cookie;gpw_e24=http://www.oracle.com/technetwork/java/javase/downloads/jre7-downloads-1880261.html',
-         :warning => "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html"
+  source url: "http://download.oracle.com/otn-pub/java/jdk/7u3-b04/jre-7u3-linux-x64.tar.gz",
+         md5: "3d3e206cea84129f1daa8e62bf656a28",
+         cookie: 'oraclelicense=accept-securebackup-cookie;gpw_e24=http://www.oracle.com/technetwork/java/javase/downloads/jre7-downloads-1880261.html',
+         warning: "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html"
 else
-  source :url => "http://download.oracle.com/otn-pub/java/jdk/7u3-b04/jre-7u3-linux-i586.tar.gz",
-         :md5 => "cfce10a05f8d152d39aef892f2cd4011",
-         :cookie => 'oraclelicense=accept-securebackup-cookie;gpw_e24=http://www.oracle.com/technetwork/java/javase/downloads/jre7-downloads-1880261.html',
-         :warning => "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html"
+  source url: "http://download.oracle.com/otn-pub/java/jdk/7u3-b04/jre-7u3-linux-i586.tar.gz",
+         md5: "cfce10a05f8d152d39aef892f2cd4011",
+         cookie: 'oraclelicense=accept-securebackup-cookie;gpw_e24=http://www.oracle.com/technetwork/java/javase/downloads/jre7-downloads-1880261.html',
+         warning: "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html"
 end
 
 relative_path "jre1.7.0_03"
 
-jre_dir = "#{install_dir}/embedded/jre"
-
 build do
-  command "mkdir -p #{jre_dir}"
-  command "#{install_dir}/embedded/bin/rsync -a . #{jre_dir}/"
+  sync ".", "#{install_dir}/embedded/jre"
 end
