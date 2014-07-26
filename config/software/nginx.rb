60979a364d4b2acd14d76f321069305bd83fc85a
Seth Vargo <sethvargo@gmail.com>
2014-07-25 20:04:48 -0400

    Cleanup nginx

--- config/software/nginx.rb
+++ config/software/nginx.rb
@@ -20,24 +20,26 @@ default_version "1.4.4"
 dependency "pcre"
 dependency "openssl"
 
-source :url => "http://nginx.org/download/nginx-#{version}.tar.gz",
-       :md5 => "5dfaba1cbeae9087f3949860a02caa9f"
+source url: "http://nginx.org/download/nginx-#{version}.tar.gz",
+       md5: "5dfaba1cbeae9087f3949860a02caa9f"
 
 relative_path "nginx-#{version}"
 
 build do
-  command ["./configure",
-           "--prefix=#{install_dir}/embedded",
-           "--with-http_ssl_module",
-           "--with-http_stub_status_module",
-           "--with-ipv6",
-           "--with-debug",
-           "--with-ld-opt=-L#{install_dir}/embedded/lib",
-           "--with-cc-opt=\"-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include\""].join(" ")
-  command "make -j #{max_build_jobs}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded" \
+          " --with-http_ssl_module" \
+          " --with-http_stub_status_module" \
+          " --with-ipv6" \
+          " --with-debug" \
+          " --with-cc-opt=\"-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include\"" \
+          " --with-ld-opt=-L#{install_dir}/embedded/lib", env: env
+
+  command "make -j #{max_build_jobs}", env: env
   command "make install"
 
-  # ensure the logs directory is available on rebuild from git cache
-  command "mkdir -p #{install_dir}/embedded/logs"
-  command "touch #{install_dir}/embedded/logs/.gitkeep"
+  # Ensure the logs directory is available on rebuild from git cache
+  touch "#{install_dir}/embedded/logs/.gitkeep"
 end
