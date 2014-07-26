979b014ff51eaad6611c4d9939bba25a71efdf0a
Seth Vargo <sethvargo@gmail.com>
2014-07-26 13:42:15 -0400

    Cleanup perl-extutils-makemaker

--- config/software/perl-extutils-makemaker.rb
+++ config/software/perl-extutils-makemaker.rb
@@ -1,10 +1,26 @@
+#
+# Copyright 2014 Chef Software, Inc.omnibus
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 name "perl-extutils-makemaker"
 default_version "6.78"
 
 dependency "perl"
 
-source :url => "http://search.cpan.org/CPAN/authors/id/B/BI/BINGOS/ExtUtils-MakeMaker-#{version}.tar.gz",
-       :md5 => "843886bc1060b5e5c619e34029343eba"
+source url: "http://search.cpan.org/CPAN/authors/id/B/BI/BINGOS/ExtUtils-MakeMaker-#{version}.tar.gz",
+       md5: "843886bc1060b5e5c619e34029343eba"
 
 relative_path "ExtUtils-MakeMaker-#{version}"
 
