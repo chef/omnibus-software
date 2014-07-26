32b28166a82004e602eb92c1399b7e8a3e727a66
Seth Vargo <sethvargo@gmail.com>
2014-07-26 14:05:56 -0400

    Cleanup ruby-windows-devkit

--- config/software/ruby-windows-devkit.rb
+++ config/software/ruby-windows-devkit.rb
@@ -19,11 +19,16 @@ default_version "4.5.2-20111229-1559"
 
 dependency "ruby-windows"
 
-source :url => "http://cloud.github.com/downloads/oneclick/rubyinstaller/DevKit-tdm-32-#{version}-sfx.exe",
-       :md5 => "4bf8f2dd1d582c8733a67027583e19a6"
+source url: "http://cloud.github.com/downloads/oneclick/rubyinstaller/DevKit-tdm-32-#{version}-sfx.exe",
+       md5: "4bf8f2dd1d582c8733a67027583e19a6"
 
 build do
-  command "DevKit-tdm-32-#{version}-sfx.exe -y -o#{windows_safe_path(install_dir, 'embedded')}"
-  command "echo - #{install_dir}/embedded > config.yml", :cwd => "#{install_dir}/embedded"
-  ruby "dk.rb install", :cwd => "#{install_dir}/embedded"
+  embedded_dir = "#{install_dir}/embedded"
+
+  command "DevKit-tdm-32-#{version}-sfx.exe -y -o#{windows_safe_path(embedded_dir)}"
+
+  Dir.chdir(embedded) do
+    command "echo - #{install_dir}/embedded > config.yml"
+    ruby "dk.rb install"
+  end
 end
