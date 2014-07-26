4b6007bd9e83ffe7c67af4f36a6ffa62f244a2eb
Seth Vargo <sethvargo@gmail.com>
2014-07-26 14:04:12 -0400

    Cleanup rsync

--- config/software/rsync.rb
+++ config/software/rsync.rb
@@ -19,29 +19,16 @@ default_version "3.0.9"
 
 dependency "popt"
 
-source :url => "http://rsync.samba.org/ftp/rsync/src/rsync-3.0.9.tar.gz",
-       :md5 => "5ee72266fe2c1822333c407e1761b92b"
+source url: "http://rsync.samba.org/ftp/rsync/src/rsync-#{version}.tar.gz",
+       md5: "5ee72266fe2c1822333c407e1761b92b"
 
-relative_path "rsync-3.0.9"
-
-env =
-  case Ohai['platform']
-  when "solaris2"
-      {
-        "LDFLAGS" => "-R #{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-        "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
-        "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-      }
-  else
-    {
-      "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-    }
-  end
+relative_path "rsync-#{version}"
 
 build do
-  command "./configure --prefix=#{install_dir}/embedded --disable-iconv", :env => env
-  command "make -j #{max_build_jobs}", :env => env
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded" \
+          " --disable-iconv", env: env
+
+  command "make -j #{max_build_jobs}", env: env
   command "make install"
 end
