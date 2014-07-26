a43ee1161b5ac7c94e2aebd0bd923d573d3c0ba7
Seth Vargo <sethvargo@gmail.com>
2014-07-26 13:50:06 -0400

    Cleanup popt

--- config/software/popt.rb
+++ config/software/popt.rb
@@ -17,30 +17,19 @@
 name "popt"
 default_version "1.16"
 
-source :url => "http://rpm5.org/files/popt/popt-1.16.tar.gz",
-       :md5 => "3743beefa3dd6247a73f8f7a32c14c33"
+source url: "http://rpm5.org/files/popt/popt-#{version}.tar.gz",
+       md5: "3743beefa3dd6247a73f8f7a32c14c33"
 
-relative_path "popt-1.16"
-
-env =
-  case Ohai['platform']
-  when "solaris2"
-    {
-      "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-    }
-  else
-    {
-      "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-    }
-  end
+relative_path "popt-#{version}"
 
 build do
+  env = with_standard_compiler_flags
+
   # --disable-nls => Disable localization support.
-  command "./configure --prefix=#{install_dir}/embedded --disable-nls", :env => env
-  command "make -j #{max_build_jobs}", :env => env
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded" \
+          " --disable-nls", env: env
+
+  command "make -j #{max_build_jobs}", env: env
   command "make install"
 end
