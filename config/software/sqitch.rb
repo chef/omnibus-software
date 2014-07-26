18674b42ebce3d819adc807593e94d48b3049542
Seth Vargo <sethvargo@gmail.com>
2014-07-26 14:57:31 -0400

    Cleanup sqitch

--- config/software/sqitch.rb
+++ config/software/sqitch.rb
@@ -1,22 +1,36 @@
+#
+# Copyright 2014 Chef Software, Inc.omnibus
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 name "sqitch"
 default_version "0.973"
 
 dependency "perl"
 dependency "cpanminus"
 
-source :url => "http://www.cpan.org/authors/id/D/DW/DWHEELER/App-Sqitch-#{version}.tar.gz",
-       :md5 => "0994e9f906a7a4a2e97049c8dbaef584"
+source url: "http://www.cpan.org/authors/id/D/DW/DWHEELER/App-Sqitch-#{version}.tar.gz",
+       md5: "0994e9f906a7a4a2e97049c8dbaef584"
 
 relative_path "App-Sqitch-#{version}"
 
-env = {
-  "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}"
-}
-
 # See https://github.com/theory/sqitch for more
 build do
-  command "perl Build.PL", :env => env
-  command "./Build installdeps --cpan_client 'cpanm -v --notest'", :env => env
-  command "./Build", :env => env
-  command "./Build install", :env => env
+  env = with_embedded_path
+
+  command "perl Build.PL", env: env
+  command "./Build installdeps --cpan_client 'cpanm -v --notest'", env: env
+  command "./Build", env: env
+  command "./Build install", env: env
 end
