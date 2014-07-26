02648969bfea7816ceefb8f5eec0d0e6cfa713e7
Seth Vargo <sethvargo@gmail.com>
2014-07-26 14:40:47 -0400

    Cleanup server-jre

--- config/software/server-jre.rb
+++ config/software/server-jre.rb
@@ -17,29 +17,26 @@
 name "server-jre"
 default_version "7u25"
 
-dependency "rsync"
-
 whitelist_file "jre/bin/javaws"
 whitelist_file "jre/bin/policytool"
 whitelist_file "jre/lib"
 whitelist_file "jre/plugin"
 whitelist_file "jre/bin/appletviewer"
 
-if Ohai['kernel']['machine'] =~ /x86_64/
+if Ohai["kernel"]["machine"] =~ /x86_64/
   # TODO: download x86 version on x86 machines
-  source :url => "http://download.oracle.com/otn-pub/java/jdk/7u25-b15/server-jre-7u25-linux-x64.tar.gz",
-         :md5 => "7164bd8619d731a2e8c01d0c60110f80",
-         :cookie => 'oraclelicensejre-7u25-oth-JPR=accept-securebackup-cookie;gpw_e24=http://www.oracle.com/technetwork/java/javase/downloads/server-jre7-downloads-1931105.html',
-         :warning => "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html"
+  source url:     "http://download.oracle.com/otn-pub/java/jdk/7u25-b15/server-jre-7u25-linux-x64.tar.gz",
+         md5:     "7164bd8619d731a2e8c01d0c60110f80",
+         cookie:  "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie",
+         warning: "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html",
+         unsafe:  true
 else
   raise "Server-jre can only be installed on x86_64 systems."
 end
 
 relative_path "jdk1.7.0_25"
 
-jre_dir = "#{install_dir}/embedded/jre"
-
 build do
-  command "mkdir -p #{jre_dir}"
-  command "#{install_dir}/embedded/bin/rsync -a . #{jre_dir}/"
+  mkdir "#{install_dir}/embedded/jre"
+  sync  "#{project_dir}/", "#{install_dir}/embedded/jre"
 end
