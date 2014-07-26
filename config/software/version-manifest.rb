3f7dca1a62ec86e6f363b881eb9598d871ea8eea
Seth Vargo <sethvargo@gmail.com>
2014-07-26 15:07:39 -0400

    Cleanup version-manifest

--- config/software/version-manifest.rb
+++ config/software/version-manifest.rb
@@ -20,11 +20,8 @@ default_version "0.0.1"
 
 build do
   block do
-    project_name = project.name
-    project_build_version = project.build_version
-
     File.open("#{install_dir}/version-manifest.txt", "w") do |f|
-      f.puts "#{project_name} #{project_build_version}"
+      f.puts "#{project.name} #{project.build_version}"
       f.puts ""
       f.puts Omnibus::Reports.pretty_version_map(project)
     end
