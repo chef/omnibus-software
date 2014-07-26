f2a659b027ee418e36615521fadca24a1e5d5220
Seth Vargo <sethvargo@gmail.com>
2014-07-26 13:43:35 -0400

    Cleanup perl

--- config/software/perl.rb
+++ config/software/perl.rb
@@ -1,26 +1,37 @@
+#
+# Copyright 2014 Chef Software, Inc.omnibus
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 name "perl"
 default_version "5.18.1"
 
-source :url => "http://www.cpan.org/src/5.0/perl-#{version}.tar.gz",
-       :md5 => "304cb5bd18e48c44edd6053337d3386d"
+source url: "http://www.cpan.org/src/5.0/perl-#{version}.tar.gz",
+       md5: "304cb5bd18e48c44edd6053337d3386d"
 
 relative_path "perl-#{version}"
 
-env = {
-  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-}
-
 build do
-  command [
-            "sh Configure",
-            "-de",
-            "-Dprefix=#{install_dir}/embedded",
-            "-Duseshrplib", ## Compile shared libperl
-            "-Dusethreads", ## Compile ithread support
-            "-Dnoextensions='DB_File GDBM_File NDBM_File ODBM_File'"
-           ].join(" "), :env => env
-  command "make -j #{max_build_jobs}"
-  command "make install", :env => env
+  env = with_standard_compiler_flags
+
+  command "sh Configure" \
+          " -de" \
+          " -Dprefix=#{install_dir}/embedded" \
+          " -Duseshrplib" \
+          " -Dusethreads" \
+          " -Dnoextensions='DB_File GDBM_File NDBM_File ODBM_File'", env: env
+
+  command "make -j #{max_build_jobs}", env: env
+  command "make install", env: env
 end
