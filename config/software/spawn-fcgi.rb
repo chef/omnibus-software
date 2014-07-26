847517668a5f31719c37f019c2193a88fc3c622b
Seth Vargo <sethvargo@gmail.com>
2014-07-26 14:44:41 -0400

    Cleanup spawn-fcgi

--- config/software/spawn-fcgi.rb
+++ config/software/spawn-fcgi.rb
@@ -20,20 +20,17 @@ default_version "1.6.3"
 dependency "fcgi"
 dependency "fcgiwrap"
 
-source :url => "http://www.lighttpd.net/download/spawn-fcgi-1.6.3.tar.gz",
-       :md5 => "6d75f9e9435056fa1e574d836d823cd0"
+source url: "http://www.lighttpd.net/download/spawn-fcgi-#{version}.tar.gz",
+       md5: "6d75f9e9435056fa1e574d836d823cd0"
 
-relative_path "spawn-fcgi-1.6.3"
-
-configure_env = {
-  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-  "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}"
-}
+relative_path "spawn-fcgi-#{version}"
 
 build do
-  command "./configure --prefix=#{install_dir}/embedded", :env => configure_env
-  command "make -j #{max_build_jobs}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded", env: env
+
+  command "make -j #{max_build_jobs}", env: env
   command "make install"
 end
