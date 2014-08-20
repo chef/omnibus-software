01e615ff886b0be28f9ef9409dbbe647782990ba
Seth Vargo <sethvargo@gmail.com>
2014-08-20 14:32:21 -0400

    Use `make` instead of `command` in erlang

--- config/software/erlang.rb
+++ config/software/erlang.rb
@@ -70,6 +70,6 @@ build do
           " --with-ssl=#{install_dir}/embedded" \
           " --disable-debug", env: env
 
-  command "make -j #{max_build_jobs}", env: env
-  command "make install", env: env
+  make "-j #{max_build_jobs}", env: env
+  make "install", env: env
 end
