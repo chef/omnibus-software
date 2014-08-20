36d8f78972643d4f6ea12e8f696d0e6efeef2f11
Seth Vargo <sethvargo@gmail.com>
2014-08-20 14:37:09 -0400

    Use `make` instead of `command` in spawn-fgci

--- config/software/spawn-fcgi.rb
+++ config/software/spawn-fcgi.rb
@@ -31,6 +31,6 @@ build do
   command "./configure" \
           " --prefix=#{install_dir}/embedded", env: env
 
-  command "make -j #{max_build_jobs}", env: env
-  command "make install", env: env
+  make "-j #{max_build_jobs}", env: env
+  make "install", env: env
 end
