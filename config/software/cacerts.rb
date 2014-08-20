34bb6038eb7220d810645e8afe16850786eb8199
Ben Snape <ben.snape@itv.com>
2014-08-20 11:29:59 +0100

    bumped cacerts version to 2014/08/20

--- config/software/cacerts.rb
+++ config/software/cacerts.rb
@@ -17,7 +17,11 @@
 name "cacerts"
 
 # Date of the file is in a comment at the start, or in the changelog
-default_version "2014.07.15"
+default_version "2014.08.20"
+
+version "2014.08.20" do
+  source md5: "c9f4f7f4d6a5ef6633e893577a09865e"
+end
 
 version "2014.07.15" do
   source md5: "fd48275847fa10a8007008379ee902f1"
