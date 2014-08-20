a85e51a1e2b52508b6ea3c75c6d06adb2bfd1b9a
Seth Vargo <sethvargo@gmail.com>
2014-08-20 15:15:58 -0400

    Use Chef Sugar in libedit definition

--- config/software/libedit.rb
+++ config/software/libedit.rb
@@ -37,7 +37,7 @@ build do
 
   # The patch is from the FreeBSD ports tree and is for GCC compatibility.
   # http://svnweb.freebsd.org/ports/head/devel/libedit/files/patch-vi.c?annotate=300896
-  if ohai['platform'] == "freebsd"
+  if freebsd?
     patch source: "freebsd-vi-fix.patch"
   end
 
