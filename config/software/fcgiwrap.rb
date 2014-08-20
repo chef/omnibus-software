fbd5c040b73097120e213f771d22fd33c97b7572
Seth Vargo <sethvargo@gmail.com>
2014-08-20 14:32:39 -0400

    Use `make` instead of `command` in fcgiwrap

--- config/software/fcgiwrap.rb
+++ config/software/fcgiwrap.rb
@@ -30,6 +30,6 @@ build do
   command "autoreconf -i", env: env
   command "./configure --prefix=#{install_dir}/embedded", env: env
 
-  command "make -j #{max_build_jobs}", env: env
-  command "make install", env: env
+  make "-j #{max_build_jobs}", env: env
+  make "install", env: env
 end
