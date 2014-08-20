cf21a71c870a2c75355302866d2884cf01fefdd1
Seth Vargo <sethvargo@gmail.com>
2014-08-20 14:32:46 -0400

    Use `make` instead of `command` in gdbm

--- config/software/gdbm.rb
+++ config/software/gdbm.rb
@@ -38,6 +38,6 @@ build do
             " --prefix=#{install_dir}/embedded", env: env
   end
 
-  command "make -j #{max_build_jobs}", env: env
-  command "make install", env: env
+  make "-j #{max_build_jobs}", env: env
+  make "install", env: env
 end
