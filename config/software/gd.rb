c8757514d93cf9fa3497fc62af4ef087668e4700
Seth Vargo <sethvargo@gmail.com>
2014-08-20 14:32:42 -0400

    Use `make` instead of `command` in gd

--- config/software/gd.rb
+++ config/software/gd.rb
@@ -45,6 +45,6 @@ build do
           " --without-fontconfig" \
           " --without-xpm", env: env
 
-  command "make -j #{max_build_jobs}", env: env
-  command "make install", env: env
+  make "-j #{max_build_jobs}", env: env
+  make "install", env: env
 end
