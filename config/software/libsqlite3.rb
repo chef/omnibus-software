9b9aba9474395c6cd50202467b103be7b51e5d73
Remi Hakim <remi@datadoghq.com>
2014-10-08 15:47:24 -0400

    the binary js not used and rely on the gnu readline library

--- config/software/libsqlite3.rb
+++ config/software/libsqlite3.rb
@@ -19,4 +19,5 @@ build do
   command "make -j #{workers}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
   command "sudo make install"
   command "sudo chown -R vagrant #{install_dir}"
+  command "sudo rm  #{install_dir}/bin/sqlite3"
 end
