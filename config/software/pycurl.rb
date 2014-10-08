1d48abe7d0d90911c1a574ec43bba0a39e4b4408
Remi Hakim <remi@datadoghq.com>
2014-10-08 14:38:48 -0400

    Platform is deprecated. Use Ohai['platform'] instead

--- config/software/pycurl.rb
+++ config/software/pycurl.rb
@@ -4,8 +4,8 @@ default_version "7.19.3.1"
 dependency "python"
 dependency "pip"
 dependency "curl"
-dependency "gdbm" if (platform == "mac_os_x" or platform == "freebsd" or platform == "aix")
-dependency "libgcc" if (platform == "solaris2" and Omnibus.config.solaris_compiler == "gcc")
+dependency "gdbm" if (Ohai['platform'] == "mac_os_x" or Ohai['platform'] == "freebsd" or Ohai['platform'] == "aix")
+dependency "libgcc" if (Ohai['platform'] == "solaris2" and Omnibus.config.solaris_compiler == "gcc")
 
 build do
   build_env = {
