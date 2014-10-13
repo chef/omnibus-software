0a21722536502737fe9cb373119c41cbcd3da2a7
Nathan L Smith <smith@getchef.com>
2014-10-13 10:45:03 -0500

    update cops to make existing stuff pass

--- config/software/openssl.rb
+++ config/software/openssl.rb
@@ -37,7 +37,7 @@ end
 relative_path "openssl-#{version}"
 
 build do
-  patch :source => "openssl-1.0.1f-do-not-build-docs.patch"
+  patch source: "openssl-1.0.1f-do-not-build-docs.patch"
 
   env = case ohai["platform"]
         when "mac_os_x"
