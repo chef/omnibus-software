55d8e3ff720c906e7b0664d27b328ca0182249ed
Lamont Granquist <lamont@scriptkiddie.org>
2014-12-18 13:12:59 -0800

    backport of new rubygems version from master

--- config/software/rubygems.rb
+++ config/software/rubygems.rb
@@ -1,6 +1,5 @@
 #
-# Copyright:: Copyright (c) 2012-2014 Chef Software, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2012-2014 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,12 +27,25 @@ version "1.8.24" do
   source md5: "3a555b9d579f6a1a1e110628f5110c6b"
 end
 
+# NOTE: this is the last version of rubygems before the 2.2.x change to native gem install location
+#
+#  https://github.com/rubygems/rubygems/issues/874
+#
+# This is a breaking change for omnibus clients.  Chef-11 needs to be pinned to 2.1.11 for eternity.
+version "2.1.11" do
+  source md5: "b561b7aaa70d387e230688066e46e448"
+end
+
 version "2.2.1" do
   source md5: "1f0017af0ad3d3ed52665132f80e7443"
 end
 
 version "2.4.1" do
   source md5: "7e39c31806bbf9268296d03bd97ce718"
+end
+
+version "2.4.4" do
+  source md5: "440a89ad6a3b1b7a69b034233cc4658e"
 end
 
 source url: "http://production.cf.rubygems.org/rubygems/rubygems-#{version}.tgz"
