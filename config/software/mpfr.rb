a8a48388dcfaf6780350665818f4dcad218bc21c
Scott Hain <shain@getchef.com>
2014-12-30 10:30:39 -0800

    Added initial gcc deps for solaris 10

---
+++ config/software/mpfr.rb
@@ -0,0 +1,37 @@
+#
+# Copyright 2014 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "mpfr"
+default_version "3.1.2"
+
+dependency "gmp"
+
+version("3.1.2") { source md5: "181aa7bb0e452c409f2788a4a7f38476" }
+
+source url: "http://www.mpfr.org/mpfr-current/mpfr-#{version}.tar.gz"
+
+relative_path "mpfr-#{version}"
+
+env = with_standard_compiler_flags(with_embedded_path)
+
+build do
+  configure_command = ["./configure",
+                       "--prefix=#{install_dir}/embedded"]
+
+  command configure_command.join(" "), env: env
+  make "-j #{workers}", env: env
+  make "-j #{workers} install", env: env
+end
