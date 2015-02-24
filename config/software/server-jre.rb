b802c94540ec36d5a9bee0168793d103e9e12906
Oliver Ferrigni <oliver@opscode.com>
2015-02-24 12:50:55 -0600

    Bump to 1.8 of jre

--- config/software/server-jre.rb
+++ config/software/server-jre.rb
@@ -15,7 +15,7 @@
 #
 
 name "server-jre"
-default_version "7u25"
+default_version "8u31"
 
 whitelist_file "jre/bin/javaws"
 whitelist_file "jre/bin/policytool"
@@ -25,8 +25,8 @@ whitelist_file "jre/bin/appletviewer"
 
 if _64_bit?
   # TODO: download x86 version on x86 machines
-  source url:     "http://download.oracle.com/otn-pub/java/jdk/7u25-b15/server-jre-7u25-linux-x64.tar.gz",
-         md5:     "7164bd8619d731a2e8c01d0c60110f80",
+  source url:     "http://download.oracle.com/otn-pub/java/jdk/8u31-b13/server-jre-8u31-linux-x64.tar.gz",
+         md5:     "9d69cdc00c536b8c9f5b26a3128bd2a1",
          cookie:  "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie",
          warning: "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html",
          unsafe:  true
@@ -34,7 +34,7 @@ else
   raise "Server-jre can only be installed on x86_64 systems."
 end
 
-relative_path "jdk1.7.0_25"
+relative_path "jdk1.8.0_31"
 
 build do
   mkdir "#{install_dir}/embedded/jre"
