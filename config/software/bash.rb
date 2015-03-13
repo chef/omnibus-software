d6eaf8bba08550f1edd3e9c3dcbf9b1a32056ce9
Omnibus <omnibus@getchef.com>
2015-03-13 09:04:41 -0700

    added bash defs

---
+++ config/software/bash.rb
@@ -0,0 +1,37 @@
+#
+# Copyright 2015 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "bash"
+default_version "4.3"
+
+dependency "libiconv"
+
+version("4.3") { source md5: "81348932d5da294953e15d4814c74dd1" }
+
+source url: "http://ftp.gnu.org/gnu/bash/bash-#{version}.tar.gz"
+
+relative_path "bash-#{version}"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  configure_command = ["./configure",
+                       "--prefix=#{install_dir}/embedded"]
+
+  command configure_command.join(" "), env: env
+  make "-j #{workers}", env: env
+  make "-j #{workers} install", env: env
+end
