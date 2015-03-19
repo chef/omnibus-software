d3a24503550c4ae10f5e0aed1c6971e61a1a0e1f
Masahiro Nakagawa <repeatedly@gmail.com>
2015-03-20 04:58:29 +0900

    Use working mirror for libossp-uuid software. fixes #373

--- config/software/libossp-uuid.rb
+++ config/software/libossp-uuid.rb
@@ -21,7 +21,9 @@ version "1.6.2" do
   source md5: "5db0d43a9022a6ebbbc25337ae28942f"
 end
 
-source url: "ftp://ftp.ossp.org/pkg/lib/uuid/uuid-#{version}.tar.gz"
+# ftp on ftp.ossp.org is unavaiable so we must use another mirror site.
+# From homebrew discussion, following url is better for now: https://github.com/Homebrew/homebrew/pull/14288
+source url: "http://gnome-build-stage-1.googlecode.com/files/uuid-#{version}.tar.gz"
 
 relative_path "uuid-#{version}"
 
