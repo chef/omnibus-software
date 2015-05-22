cfc9570fabafc5c6f9e133f4481a1b0fb9f5264e
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-05-22 15:49:21 -0400

    Disclaimer added on apparently useless dependencies

--- config/software/meld3.rb
+++ config/software/meld3.rb
@@ -1,3 +1,10 @@
+#
+# NOTICE : meld3 has been added as a supervisor dependency. It may look useless
+# in the first place since supervisor is installed using pip/setup.py which are
+# supposed to install dependencies as well but there is a certificate issue on
+# OSX when using setup.py. Therefore we have to make sure our dependency is
+# installed before supervisor gets setup.
+#
 name "meld3"
 default_version "0.6.7"
 
