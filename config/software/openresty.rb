a4270508dadecd49577ef97c977191f09a9a171a
Steven Danna <steve@chef.io>
2015-06-03 18:03:31 +0100

    Add /usr/sbin and /sbin to the PATH during openresty build

--- config/software/openresty.rb
+++ config/software/openresty.rb
@@ -30,6 +30,7 @@ relative_path "ngx_openresty-#{version}"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
+  env['PATH'] += "#{env['PATH']}:/usr/sbin:/sbin"
 
   configure = [
     "./configure",
