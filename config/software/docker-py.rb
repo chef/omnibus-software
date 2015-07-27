d33e3ae858e376dc6d76135e38ec382bb7759b74
Haissam Kaj <haissam.kaj@datadoghq.com>
2015-07-27 19:23:33 -0400

    Ship license for docker-py

--- config/software/docker-py.rb
+++ config/software/docker-py.rb
@@ -5,5 +5,6 @@ dependency "python"
 dependency "pip"
 
 build do
+  ship_license "Apachev2"
   command "#{install_dir}/embedded/bin/pip install --force-reinstall -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}", :cwd => "/tmp"
 end
