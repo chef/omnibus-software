de4aa0cc8fe65882c1bd07731222da97947db97a
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2015-07-27 15:46:21 -0400

    Allow 32 and 64-bit ruby-windows.

--- config/software/ruby-windows-devkit.rb
+++ config/software/ruby-windows-devkit.rb
@@ -15,36 +15,34 @@
 #
 
 name "ruby-windows-devkit"
-default_version "4.5.2-20111229-1559"
+default_version "4.7.2-20130224"
 
 dependency "ruby-windows"
 
-version "4.5.2-20111229-1559" do
-  source url: "http://cloud.github.com/downloads/oneclick/rubyinstaller/DevKit-tdm-32-#{version}-sfx.exe",
-         md5: "4bf8f2dd1d582c8733a67027583e19a6"
+if i386?
+  version "4.7.2-20130224" do
+    source url: "http://cdn.rubyinstaller.org/archives/devkits/DevKit-mingw64-32-#{version}-1151-sfx.exe",
+           md5: "9383f12958aafc425923e322460a84de"
+  end
+else
+  version "4.7.2-20130224" do
+    source url: "http://cdn.rubyinstaller.org/archives/devkits/DevKit-mingw64-64-#{version}-1432-sfx.exe",
+           md5: "ce99d873c1acc8bffc639bd4e764b849"
+  end
 end
-
-version "4.7.2-20130224-1151" do
-  source url: "http://cdn.rubyinstaller.org/archives/devkits/DevKit-mingw64-32-#{version}-sfx.exe",
-         md5: "9383f12958aafc425923e322460a84de"
-end
-
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
   embedded_dir = "#{install_dir}/embedded"
 
-  version "4.5.2-20111229-1559" do
-    command "DevKit-tdm-32-#{version}-sfx.exe -y -o#{windows_safe_path(embedded_dir)}", env: env
+  version "4.7.2-20130224" do
+    command "#{project_file} -y -o#{windows_safe_path(embedded_dir)}", env: env
   end
 
-  version "4.7.2-20130224-1151" do
-    command "DevKit-mingw64-32-#{version}-sfx.exe -y -o#{windows_safe_path(embedded_dir)}", env: env
-  end
 
   command "echo - #{install_dir}/embedded > config.yml", cwd: embedded_dir
   ruby "dk.rb install", env: env, cwd: embedded_dir
 
-  # may gems that ship with native extensions assume tar will be available in the PATH
+  # many gems that ship with native extensions assume tar will be available in the PATH
   copy "#{install_dir}/embedded/mingw/bin/bsdtar.exe", "#{install_dir}/embedded/mingw/bin/tar.exe"
 end
