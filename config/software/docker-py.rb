7e6033ec6d0e21c57061fbed804f95762c01c0e1
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-09-17 17:54:09 -0400

    tests

--- config/software/docker-py.rb
+++ config/software/docker-py.rb
@@ -8,7 +8,7 @@ build do
   ship_license "Apachev2"
   # We don't use docker-py on Windows yet but it will come one day :)
   if ohai['platform'] == 'windows'
-    pip_call "install --force-reinstall --install-option=\"--install-scripts='#{windows_safe_path(install_dir)}\\bin'\" #{name}==#{version}"
+    pip_call "install --force-reinstall --install-option=\"--install-scripts=#{windows_safe_path(install_dir)}\\bin\" #{name}==#{version}"
   else
     pip_call "install --force-reinstall --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}", :cwd => "/tmp"
   end
