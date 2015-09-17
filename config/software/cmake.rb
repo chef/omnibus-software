a6023cefcafdf01c9d592827035a3b364fe3ca9f
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-09-17 17:02:31 -0400

    Removed last DSL requiring `uselessfile`
    Only libgcc is left but it's giving use issues on Mac, I'll deal with it
    later.

--- config/software/cmake.rb
+++ config/software/cmake.rb
@@ -1,22 +1,14 @@
 name 'cmake'
 default_version '3.1.3'
 
-if ohai['platform'] != 'windows'
-  source :url => "http://www.cmake.org/files/v#{version[0..2]}/cmake-#{version}.tar.gz",
-         :md5 => '5697a77503bb5636f4b4057dcc02aa32'
+source :url => "http://www.cmake.org/files/v#{version[0..2]}/cmake-#{version}.tar.gz",
+       :md5 => '5697a77503bb5636f4b4057dcc02aa32'
 
-  relative_path "cmake-#{version}"
+relative_path "cmake-#{version}"
 
-  build do
-    command "./configure --prefix=#{install_dir}/embedded"
-    patch :source => "build_on_mac_10.10.patch" if ohai['platform_family'] == 'mac_os_x'
-    command "make -j #{workers}"
-    command "make install"
-  end
-
-else
-  # We create a dummy file for the omnibus git_cache to work on Windows
-  build do
-    command "touch #{install_dir}/uselessfile"
-  end
+build do
+  command "./configure --prefix=#{install_dir}/embedded"
+  patch :source => "build_on_mac_10.10.patch" if ohai['platform_family'] == 'mac_os_x'
+  command "make -j #{workers}"
+  command "make install"
 end
--- config/software/vc_python.rb
+++ config/software/vc_python.rb
@@ -1,4 +1,13 @@
-# Microsoft Visual C++ for Python
+#
+# Microsoft Visual C++ Compiler for Python
+# Required by PyCrypto
+#
+# This one is actually stored in an S3 bucket of ours because downloading it
+# from the microsoft website is extremely slow (around 45 minutes)
+#
+# Here's the original download URL:
+# http://www.microsoft.com/en-us/download/details.aspx?id=44266
+#
 
 name 'vc_python'
 default_version '2.7'
@@ -9,7 +18,5 @@ source :url => 'https://s3.amazonaws.com/dd-agent-omnibus/vc_for_python_27.msi',
 build do
     command "start /wait msiexec /x vc_for_python_27.msi /qn"
     command "start /wait msiexec /i vc_for_python_27.msi /qn"
-    # TODO : ship that, pycrypto probably needs it !!!
-    touch "#{install_dir}/uselessfile"
 end
 
