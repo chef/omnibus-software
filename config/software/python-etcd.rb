b449a73d380265b6a73191cc547cabcb31940bf7
Haissam Kaj <haissam.kaj@gmail.com>
2015-12-15 17:43:27 +0000

    Add python-consul dependency, fix licence for python-etcd

--- config/software/python-etcd.rb
+++ config/software/python-etcd.rb
@@ -5,6 +5,6 @@ dependency "python"
 dependency "pip"
 
 build do
-  ship_license "https://github.com/jplana/python-etcd/blob/master/LICENSE.txt"
+  ship_license "https://raw.githubusercontent.com/jplana/python-etcd/master/LICENSE.txt"
   command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" python-etcd==#{version}"
 end
