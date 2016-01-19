3d20001d8a9e4736fc7ee15f8a60990c15cd2e5d
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2016-01-19 17:15:02 -0500

    Add datadogpy

---
+++ config/software/datadogpy.rb
@@ -0,0 +1,10 @@
+name "datadogpy"
+default_version "0.10.0"
+
+dependency "python"
+dependency "pip"
+
+build do
+  ship_license "https://raw.githubusercontent.com/DataDog/datadogpy/master/LICENSE"
+  command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" datadog==#{version}"
+end
