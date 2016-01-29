98bf60f9c10b418a1cf7af8cd88d142042c61d99
Lamont Granquist <lamont@scriptkiddie.org>
2016-01-29 13:24:04 -0800

    bump default ncurses version

--- config/software/ncurses.rb
+++ config/software/ncurses.rb
@@ -1,5 +1,5 @@
 #
-# Copyright 2012-2014 Chef Software, Inc.
+# Copyright 2012-2016 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
 #
 
 name "ncurses"
-default_version "5.9"
+default_version "6.0-20150810"
 
 dependency "libtool" if aix?
 dependency "patch" if solaris2?
