26071235139d939c8c9348cf7de855c1b35e4464
John Keiser <john@johnkeiser.com>
2016-03-18 12:35:06 -0700

    Bump default version to 1.2.0 (which actually compiles on windows)

--- config/software/dep-selector-libgecode.rb
+++ config/software/dep-selector-libgecode.rb
@@ -15,7 +15,7 @@
 #
 
 name "dep-selector-libgecode"
-default_version "1.0.2"
+default_version "1.2.0"
 
 license "Apache-2.0"
 license_file "https://github.com/chef/dep-selector-libgecode/blob/master/LICENSE.txt"
