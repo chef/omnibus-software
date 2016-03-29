976dd45a6c389a2e7a5d8b66726c244807262de5
Quentin Madec <quentin.madec@datadoghq.com>
2016-03-29 14:32:16 +0200

    [windows] fix pywin32 import error
    Pywin32 raises an error when importing `pywintypes` with Python 2.7.11.
    This patch fixes this issue by looking for the correct DLL in a few more
    directories listed by Python.

--- config/software/pywin32.rb
+++ config/software/pywin32.rb
@@ -20,4 +20,15 @@ build do
     # Switch on the architecture
     pip "install pywin32-#{version}-cp27-none-win_amd64.whl "\
              "--install-option=\"--prefix=#{windows_safe_path(install_dir)}\\embedded\""
+    # pywintypes is patched, it doesn't work on Python > 2.7 otherwise
+    # Since we don't install it from source, we can't use the patch option of omnibus
+    # Here is a manual patch, using the same options as omnibus
+    patch_path = File.realpath(
+      File.join(
+        File.dirname(__FILE__), '..',
+        'patches', 'pywin32', 'fix_pywintypes_dll_import.patch'
+      )
+    )
+    win32_path = File.join(install_dir, 'embedded', 'Lib', 'site-packages', 'win32')
+    command "patch -d \"#{win32_path}\" -p1 -i \"#{patch_path}\""
 end
