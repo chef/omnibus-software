e0accf95686f045b88efb9be8a15f89364737821
Salim Alam <salam@chef.io>
2016-04-05 16:03:48 -0700

    Revert "revert the TK Gemfile changes after build"
    This reverts commit 97c8b82586afd0d15ee12c7ffa3d5c076ee24591.

--- config/software/test-kitchen.rb
+++ config/software/test-kitchen.rb
@@ -26,16 +26,12 @@ dependency "nokogiri"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
-  gemfile = "#{project_dir}/Gemfile"
-  tmp_gemfile = "#{gemfile}_old"
 
   block do
     # make sure test-kitchen build honors chef constraints
-    require 'fileutils'
-    FileUtils.cp(gemfile, tmp_gemfile)
     chef_gem_line = "gem \"chef\", path: \"../../chef/chef\""
-    unless IO.readlines(gemfile)[-1] =~ /#{chef_gem_line}/
-      open(gemfile, 'a') { |f| f.puts chef_gem_line }
+    unless IO.readlines("#{project_dir}/Gemfile")[-1] =~ /#{chef_gem_line}/
+      open("#{project_dir}/Gemfile", 'a') { |f| f.puts chef_gem_line }
     end
   end
 
@@ -44,9 +40,4 @@ build do
 
   gem "install pkg/test-kitchen-*.gem" \
       " --no-ri --no-rdoc", env: env
-
-  block do
-    File.delete(gemfile)
-    File.rename(tmp_gemfile, gemfile)
-  end
 end
