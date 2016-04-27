0c4eb4750b068cfd0ef6e17cd688c65a00b41bb8
tyler-ball <tyleraball@gmail.com>
2016-04-27 14:40:11 -0600

    another one

--- config/software/rust.rb
+++ config/software/rust.rb
@@ -68,7 +68,7 @@ end
 relative_path relative_path_template % { host_triple: host_triple, arch: arch }
 
 build do
-  env = with_standard_compiler_flags(with_embedded_path)
+  env = with_standard_compiler_flags(with_embedded_path, bfd_flags: true)
 
   command "sh install.sh" \
           " --prefix=#{install_dir}/embedded" \
