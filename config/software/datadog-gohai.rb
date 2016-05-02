e1e92a8365fae7c2b3039a0327ba996e2f275a81
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2016-05-02 17:36:11 -0400

    [gohai] Add seelog dep

--- config/software/datadog-gohai.rb
+++ config/software/datadog-gohai.rb
@@ -21,6 +21,8 @@ build do
    # Checkout gohai's deps
    command "#{gobin} get -u github.com/shirou/gopsutil", :env => env
    command "git checkout v2.0.0", :env => env, :cwd => "/var/cache/omnibus/src/datadog-gohai/src/github.com/shirou/gopsutil"
+   command "#{gobin} get -u github.com/cihub/seelog", :env => env
+   command "git checkout v2.6", :env => env, :cwd => "/var/cache/omnibus/src/datadog-gohai/src/github.com/cihub/seelog"
    # Checkout and build gohai
    command "git checkout #{version} && git pull", :env => env, :cwd => "/var/cache/omnibus/src/datadog-gohai/src/github.com/DataDog/gohai"
    command "cd $GOPATH/src/github.com/DataDog/gohai && #{gobin} run make.go '#{gobin}' && mv gohai #{install_dir}/bin/gohai", :env => env
