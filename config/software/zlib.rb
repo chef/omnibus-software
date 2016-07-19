b2b861cffc418262d474dc9090954021e74eb410
Quentin Madec <quentin.madec@datadoghq.com>
2016-07-19 17:59:45 -0400

    [zlib] replace sourceforge link

--- config/software/zlib.rb
+++ config/software/zlib.rb
@@ -24,7 +24,7 @@ version "1.2.6" do
   source md5: "618e944d7c7cd6521551e30b32322f4a"
 end
 
-source url: "http://iweb.dl.sourceforge.net/project/libpng/zlib/#{version}/zlib-#{version}.tar.gz"
+source url: "http://zlib.net/zlib-#{version}.tar.gz"
 
 relative_path "zlib-#{version}"
 
