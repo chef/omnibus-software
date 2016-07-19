2f2cc72a1a7cfea4ac484f2180daa066d237c86d
Quentin Madec <quentin.madec@datadoghq.com>
2016-07-19 17:07:26 -0400

    [pycurl] use our patched pycurl
    Pycurl (curl) was not working on Windows 64 bits. This was patched
    upstream, but pycurl did not yet release a custom package for it.
    Thus we built a custom package and are currently using it.

--- config/software/pycurl.rb
+++ config/software/pycurl.rb
@@ -32,5 +32,16 @@ else
 
   build do
     pip "install #{wheel_name}"
+
+    # Delete these lines as soon as we have upgraded to pycurl > 7.43.0
+    # This is a custom built pycurl
+    python_lib_path = File.join(install_dir, 'embedded', 'Lib', 'site-packages')
+    command 'powershell.exe -Command wget -outfile pycurl.pyd https://s3.amazonaws.com/dd-agent-omnibus/pycurl.pyd'
+    pycurl_sha256 = '47 c6 b1 ca aa 11 a5 a1 cf 39 38 a1 0b f3 2a 21 e8 72 3c 16 ec bb 4a f8 b8 20 87 4d be 29 ba 31'
+    command "CertUtil -hashfile pycurl.pyd SHA256 | grep '#{pycurl_sha256}' && mv pycurl.pyd #{python_lib_path}"
+
+    command 'powershell.exe -Command wget -outfile msvcr110.dll https://s3.amazonaws.com/dd-agent-omnibus/msvcr110.dll'
+    vcruntime_sha256 = '0c bb d9 69 1f 08 43 4d a3 61 78 74 f9 9c 6d d8 75 38 cb d6 5b 5d 8b c3 9f ce 37 8d 4e d2 9e ed'
+    command "CertUtil -hashfile msvcr110.dll SHA256 | grep '#{vcruntime_sha256}' && mv msvcr110.dll #{python_lib_path}"
   end
 end
