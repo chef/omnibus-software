a608617a82610f4ce89cfdafa38a36ff2023abbf
Massimiliano Pippi <massi@datadoghq.com>
2016-07-22 14:50:32 +0200

    bumped curl version, add http2 support

---
+++ config/software/cython.rb
@@ -0,0 +1,10 @@
+name "cython"
+default_version "0.24"
+
+dependency "python"
+dependency "pip"
+
+build do
+  ship_license "https://raw.githubusercontent.com/cython/cython/master/LICENSE.txt"
+  command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--no-cython-compile\" cython==#{version}"
+end
---
+++ config/software/nghttp2.rb
@@ -0,0 +1,33 @@
+name "nghttp2"
+default_version "1.12.0"
+
+dependency "openssl"
+dependency "cython"
+
+source :url => "https://github.com/nghttp2/nghttp2/releases/download/v1.12.0/nghttp2-#{version}.tar.gz",
+       :md5 => "04235f1d7170a2efce535068319180a1"
+
+relative_path "nghttp2-#{version}"
+
+env = {
+  "CYTHON" => "#{install_dir}/embedded/bin/cython",
+  "PYTHON" => "#{install_dir}/embedded/bin/python2.7",
+  "PYTHON_CPPFLAGS" => "-I#{install_dir}/embedded/include/python2.7",
+  "PYTHON_LDFLAGS" => "-L#{install_dir}/embedded/lib -lpython2.7",
+  "OPENSSL_CFLAGS" => "-I#{install_dir}/embedded/include/openssl",
+  "OPENSSL_LIBS" => "-L#{install_dir}/embedded/lib"
+}
+
+build do
+  command [
+    "./configure",
+    "--enable-python-bindings",
+    "--disable-app",
+    "--disable-examples",
+    "--disable-hpack-tools",
+    "--prefix=#{install_dir}/embedded",
+    ""
+  ].join(" "), :env => env
+  command "make -j #{workers}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
+  command "make install"
+end
