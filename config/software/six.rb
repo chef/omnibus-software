9c2532cba1e160831d6ffdf15c9f8ddf1d64c4e7
Quentin Madec <quentin.madec@datadoghq.com>
2016-11-23 17:26:38 -0500

    [six] use pip DSL

--- config/software/six.rb
+++ config/software/six.rb
@@ -6,5 +6,5 @@ dependency "pip"
 
 build do
   ship_license "https://bitbucket.org/gutworth/six/raw/e5218c3f66a2614acb7572204a27e2b508682168/LICENSE"
-  command "#{install_dir}/embedded/bin/pip install #{name}==#{version}"
+  pip "install #{name}==#{version}"
 end
