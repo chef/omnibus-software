3e0e6d0390938ce77aefb58bb7edb9d0a9979d36
Quentin Madec <degemer@users.noreply.github.com>
2017-01-24 11:57:12 -0500

    [setuptools] pin version on Windows (#100)
    * [setuptools] pin version on Windows
    
    * [setuptools] pin version

--- config/software/setuptools.rb
+++ config/software/setuptools.rb
@@ -22,21 +22,16 @@ dependency "python"
 
 relative_path "setuptools-#{version}"
 
-if ohai["platform"] == "windows"
-  # FIXME: this file changes, which breaks the build. Let's put it on S3
-  source :url => "http://dd-agent-omnibus.s3.amazonaws.com/ez_setup.py",
-         :md5 => "ce2f1224d50f529b6093e3feb0799f09"
+source :url => "https://github.com/pypa/setuptools/archive/v#{version}.tar.gz",
+       :sha256 => "d3b2c63a5cb6816ace0883bc3f6aca9e7890c61d80ac0d608a183f85825a7cc0"
 
-  build do
-    command "\"#{windows_safe_path(install_dir)}\\embedded\\python.exe\" ez_setup.py "
+build do
+  python_path = "#{install_dir}/embedded/bin/python"
+  if ohai["platform"] == "windows"
+    python_path = "#{windows_safe_path(install_dir)}\\embedded\\python.exe"
   end
-else
-  source :url => "https://github.com/pypa/setuptools/archive/v#{version}.tar.gz",
-         :sha256 => "d3b2c63a5cb6816ace0883bc3f6aca9e7890c61d80ac0d608a183f85825a7cc0"
 
-  build do
-    ship_license "PSFL"
-    command "#{install_dir}/embedded/bin/python bootstrap.py"
-    command "#{install_dir}/embedded/bin/python setup.py install --prefix=#{install_dir}/embedded"
-  end
+  ship_license "PSFL"
+  command "#{python_path} bootstrap.py"
+  command "#{python_path} setup.py install --prefix=#{windows_safe_path(install_dir)}/embedded"
 end
