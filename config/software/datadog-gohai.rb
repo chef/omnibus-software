a6fad53cc312a0177b90ccce9770762da601b963
Quentin Madec <quentin.madec@datadoghq.com>
2017-02-06 13:49:39 -0500

    Revert "[gohai] use glide for dependency management"
    This reverts commit 5eea1a78378bb270bb4b9d22dd070d4bfb3037b1.

--- config/software/datadog-gohai.rb
+++ config/software/datadog-gohai.rb
@@ -1,7 +1,7 @@
 name "datadog-gohai"
 default_version "last-stable"
 
-source github: "DataDog/gohai"
+always_build true
 
 env = {
   "GOPATH" => "#{Omnibus::Config.cache_dir}/src/#{name}",
@@ -19,9 +19,14 @@ end
 build do
   ship_license "https://raw.githubusercontent.com/DataDog/gohai/#{version}/LICENSE"
   ship_license "https://raw.githubusercontent.com/DataDog/gohai/#{version}/THIRD_PARTY_LICENSES.md"
-  # dep manager
-  command "#{gobin} get github.com/Masterminds/glide", env: env
+  # Go get gohai
+  command "#{gobin} get -d -u github.com/DataDog/gohai", :env => env
+  # Checkout gohai's deps
+  command "#{gobin} get -u github.com/shirou/gopsutil", :env => env
+  command "git checkout v2.0.0", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-gohai/src/github.com/shirou/gopsutil"
+  command "#{gobin} get -u github.com/cihub/seelog", :env => env
+  command "git checkout v2.6", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-gohai/src/github.com/cihub/seelog"
   # Checkout and build gohai
-  command "#{Omnibus::Config.cache_dir}/src/#{name}/bin/glide install", env: env
-  command "#{gobin} run make.go && mv datadog-gohai #{install_dir}/bin/gohai", env: env
+  command "git checkout #{version} && git pull", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-gohai/src/github.com/DataDog/gohai"
+  command "cd #{env['GOPATH']}/src/github.com/DataDog/gohai && #{gobin} run make.go #{gobin} && mv gohai #{install_dir}/bin/gohai", :env => env
 end
