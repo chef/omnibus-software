cdfd81c03d128304b1bf571e6848f8f067d83ce2
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-02-10 13:28:37 -0500

    [pysnmp] break omnibus-pip circular dependency.

--- config/software/pysnmp-mibs.rb
+++ config/software/pysnmp-mibs.rb
@@ -3,6 +3,7 @@ default_version "0.1.4"
 
 dependency "python"
 dependency "pip"
+dependency "pysnmp"
 
 build do
   ship_license "https://gist.githubusercontent.com/remh/519324dc1b69f7488239/raw/2bbf2888194fef8ae75651e551b61f90cb49c482/pysnmp.license"
