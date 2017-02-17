653f542cb7dbd2bfe63e9c75a888c7ad5ed944d7
Derek Brown <derek.brown@datadoghq.com>
2017-02-17 13:29:12 -0800

    Add ability to specify UPGRADE_HELPER_BRANCH in environment

--- config/software/datadog-upgrade-helper.rb
+++ config/software/datadog-upgrade-helper.rb
@@ -1,9 +1,15 @@
 name "datadog-upgrade-helper"
-default_version "master"
+
 source github: "DataDog/dd-agent-windows-upgrade-helper"
 env = {
   "GOPATH" => "#{Omnibus::Config.cache_dir}/src/#{name}",
 }
+helper_branch = ENV["UPGRADE_HELPER_BRANCH"]
+if helper_branch.nil? || helper_branch.empty?
+  default_version "master"
+else
+  default_version helper_branch
+end
 
 if ohai["platform"] == "windows"
   gobin = "C:/Go/bin/go"
