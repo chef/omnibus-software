624b9fa67caeb1293158d449d3484f23cd9a98b7
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-03-16 16:04:24 -0400

    [net-snmp] cleaning up.

--- config/software/net-snmp.rb
+++ config/software/net-snmp.rb
@@ -26,23 +26,27 @@ source url: "https://downloads.sourceforge.net/project/net-snmp/net-snmp/#{versi
 
 relative_path "net-snmp-#{version}"
 
-configure_env = {
-  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+build_env = {
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib",
+  "CFLAGS" => "-I#{install_dir}/embedded/include",
   "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
   "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}",
 }
 
 net_snmp_configure = ["./configure",
-                      "--prefix=#{install_dir}/embedded",
                       "--with-defaults",
+                      "--prefix=#{install_dir}/embedded",
+                      "--with-ldflags=-L#{install_dir}/embedded/lib",
+                      "--with-cflags=-I#{install_dir}/embedded/include",
+                      "--with-zlib=#{install_dir}/embedded/lib",
+                      "--libdir=#{install_dir}/embedded/lib",
                       "--disable-embedded-perl",
                       "--without-perl-modules",
                       "--enable-shared",
                       "--enable-static"]
 build do
   ship_license "https://gist.githubusercontent.com/truthbk/219266c31f7d664c749dba525eb8a7b0/raw/82539d51d5b1e545e1ceb241b25f476956b636f9/net-snmp.license"
-  command net_snmp_configure.join(" "), :env => configure_env
-  command "make -j #{workers}"
+  command net_snmp_configure.join(" "), :env => build_env
+  command "make -j #{workers}", :env => build_env
   command "make install"
 end
