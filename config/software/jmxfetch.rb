276b3896824d79db8aebb3e87c1ccecbb6f8fe15
Jaime Fullaondo <truthbk@gmail.com>
2017-05-17 10:40:20 -0400

    [jmxfetch][jmxterm] add software definitions. (#119)
    * [jmxfetch] add software definition.
    
    [jmxfetch] pull from s3 bucket.
    
    * [zlib] version bump to 1.2.11.
    
    * [jmxfetch] add md5sum to definition.
    
    * [jmxfetch] set md5 of jarfile actually in bucket.
    
    * [jmxterm] adding software definition for jmxterm.
    
    * [jmxfetch] consolidating var names.
    
    * [rubocop] fix multiple issues.
    
    * [jmxfetch] updating md5sum to _real_ hash on official release.
    
    * [jmxfetch] update bucket and default version.
    
    [jmxfetch] updating buckets again.
    
    * [jmxfetch] set to latest jmxfetch.

---
+++ config/software/jmxfetch.rb
@@ -0,0 +1,30 @@
+name "jmxfetch"
+
+jmx_version = ENV["JMX_VERSION"]
+if jmx_version.nil? || jmx_version.empty?
+  default_version "0.13.1"
+else
+  default_version jmx_version
+end
+
+version "0.12.0" do
+  source md5: "2a04e4f02de90b7bbd94e581afb73c8f"
+end
+
+version "0.13.0" do
+  source sha256: "741a74e8aed6bcfd726399bf0e5094bb622f7a046004a57e3e39e11f53f72aa0"
+end
+
+version "0.13.1" do
+  source sha256: "7dcd8370c500b68e53465e6cc4b578302919cd272c12d2a32b4766eb41c03339"
+end
+
+source :url => "https://dd-jmxfetch.s3.amazonaws.com/jmxfetch-#{version}-jar-with-dependencies.jar"
+
+relative_path "jmxfetch"
+
+build do
+  ship_license "https://raw.githubusercontent.com/DataDog/jmxfetch/master/LICENSE"
+  mkdir "#{install_dir}/agent/checks/libs"
+  copy "jmxfetch-*-jar-with-dependencies.jar", "#{install_dir}/agent/checks/libs"
+end
---
+++ config/software/jmxterm.rb
@@ -0,0 +1,17 @@
+name "jmxterm"
+
+default_version "1.0"
+
+version "1.0" do
+  source md5: "f7fcba346072f88632bb7db798a21dd2"
+end
+
+source :url => "https://dd-jmxfetch.s3.amazonaws.com/jmxterm-#{version}-DATADOG-uber.jar"
+
+relative_path "jmxterm"
+
+build do
+  ship_license "https://raw.githubusercontent.com/DataDog/jmxfetch/master/LICENSE"
+  mkdir "#{install_dir}/agent/checks/libs"
+  copy "jmxterm-*-DATADOG-uber.jar", "#{install_dir}/agent/checks/libs"
+end
