07a295ba9cc3fdfe532229a28040ceeae890c40d
Stephan Renatus <srenatus@chef.io>
2017-06-21 17:36:09 +0200

    postgresql: add versioned and bin-only versioned variants
    This is for upgrading PG: the project depends on both
    
        postgres-versioned@9.6.3
    
    and
    
        postgres-bin-versioned@9.2.21
    
    thus you end up with the "full" postgres in
    
        /opt/PROJECT/embedded/postgresql/9.6/{share,bin,lib}
    
    and a more minimal, but functional, one in
    
        /opt/PROJECT/embedded/postgresql/9.2/{bin,lib}
    
    This allows for smooth upgrades, since pg_upgrade (the in-place upgrade
    utility shipped with PG) requires the binaries of the old version to be
    present.
    
    Also, it circumvents omnibus' restriction of not being able to depend on
    one software definition _twice_
    
    I couldn't find a better way to _not_ put the same code in three
    different software definitions, but I'd be glad to learn of one! :)
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- config/software/postgresql-bin-versioned.rb
+++ config/software/postgresql-bin-versioned.rb
@@ -14,18 +14,27 @@
 # limitations under the License.
 #
 
-name "postgresql"
+# Upgrading postgres requires both the old and the new bin directories to be
+# present. To account for that, and not ship a whole lot of stuff for PG 9.2,
+# which we want to upgrade _away from_, this software definition only gets us
+# oldbindir, e.g. /opt/opscode/embedded/postgresql/9.2/bin.
+#
+# Having this separate from postgresql-versioned also allows for depending on
+# two different versions of one software definition.
+
+name "postgresql-bin-versioned"
 
 pg_common_path = File.expand_path("../../common/postgresql.rb", __FILE__)
 instance_eval(IO.read(pg_common_path), pg_common_path)
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
+  short_version = version.gsub(/^([0-9]+).([0-9]+).[0-9]+$/, '\1.\2')
 
   update_config_guess(target: "config")
 
   command "./configure" \
-          " --prefix=#{install_dir}/embedded" \
+          " --prefix=#{install_dir}/embedded/postgresql/#{short_version}" \
           " --with-libedit-preferred" \
           " --with-openssl" \
           " --with-ossp-uuid" \
@@ -33,5 +42,7 @@ build do
           " --with-libraries=#{install_dir}/embedded/lib", env: env
 
   make "world -j #{workers}", env: env
-  make "install-world", env: env
+  make "-C src/interfaces/libpq install", env: env # libpq.so
+  make "-C src/backend install-bin", env: env      # postgres binary
+  make "-C src/bin install", env: env              # pg_*, psql binaries
 end
