2085826488270621711f76cd39a31fa11be3493c
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2017-06-23 11:08:43 -0400

    [docker-py] Fix install scripts dir sep (#149)

--- config/software/docker-py.rb
+++ config/software/docker-py.rb
@@ -6,5 +6,5 @@ dependency "pip"
 
 build do
   ship_license "Apachev2"
-  pip "install --install-option=\"--install-scripts=#{windows_safe_path(install_dir)}\\bin\" #{name}==#{version}"
+  pip "install --install-option=\"--install-scripts=#{windows_safe_path(install_dir)}/bin\" #{name}==#{version}"
 end
