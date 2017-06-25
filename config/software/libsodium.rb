1e5d814c4f9e419835488c0b527d8048ba683075
Lionel Félicité <deogracia@free.fr>
2017-06-26 00:01:08 +0200

    Fix my typo in version comparison

--- config/software/libsodium.rb
+++ config/software/libsodium.rb
@@ -37,7 +37,7 @@ version "0.7.1" do
   source md5: "c224fe3923d1dcfe418c65c8a7246316"
 end
 
-if Gem::Version.new("#{version}") > Gem::Version.new('1.0.9')
+if Gem::Version.new("#{version}") > Gem::Version.new('1.0.8')
   source url: "https://download.libsodium.org/libsodium/releases/libsodium-#{version}.tar.gz"
 else
   source url: "https://download.libsodium.org/libsodium/releases/old/libsodium-#{version}.tar.gz"
