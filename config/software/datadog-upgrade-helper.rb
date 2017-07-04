67698a50276da857f98ab26ad30dbab8527b7f55
Derek Brown <derek.brown@datadoghq.com>
2017-07-03 19:51:57 -0700

    [windows] use the system go, in the path, rather than hard-coded

--- config/software/datadog-upgrade-helper.rb
+++ config/software/datadog-upgrade-helper.rb
@@ -11,10 +11,6 @@ else
   default_version helper_branch
 end
 
-if ohai["platform"] == "windows"
-  gobin = "C:/Go/bin/go"
-end
-
 build do
-  command "#{gobin} build && mv datadog-upgrade-helper #{install_dir}/bin/upgrade-helper", :env => env
+  command "go build && mv datadog-upgrade-helper #{install_dir}/bin/upgrade-helper", :env => env
 end
