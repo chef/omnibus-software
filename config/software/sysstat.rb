3f635eea5509dc7f3de6c7d225ad54d1114eee3b
Jaime Fullaondo <truthbk@gmail.com>
2017-08-31 18:17:31 +0200

    [sysstat] disable sensors - wasnt provided in the past. (#163)

--- config/software/sysstat.rb
+++ config/software/sysstat.rb
@@ -17,7 +17,7 @@ build do
   ship_license "https://raw.githubusercontent.com/sysstat/sysstat/master/COPYING"
   command(["./configure",
        "--prefix=#{install_dir}/embedded",
-       "--disable-nls"].join(" "),
+       "--disable-nls", "--disable-sensors"].join(" "),
     :env => env)
   command "make -j #{workers}", :env => { "LD_RUN_PATH" => "#{install_dir}/embedded/lib" }
   command "make install"
