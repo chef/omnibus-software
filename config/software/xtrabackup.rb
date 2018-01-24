97ccd2dcef8338d7bc54c8ccdf9122045bcfa1e5
mkryva <maksym.kryva@gmail.com>
2018-01-24 17:03:49 +0200

    Update software

--- config/software/xtrabackup.rb
+++ config/software/xtrabackup.rb
@@ -1,9 +1,7 @@
 name "percona-xtrabackup"
 default_version "2.4.9"
 
-source url: "https://github.com/percona/percona-xtrabackup/archive/percona-xtrabackup-2.4.9.tar.gz",
-       md5: "a652101df66cc8eed92824d316f41766"
-
+source url: "https://github.com/percona/percona-xtrabackup/archive/percona-xtrabackup-2.4.9.tar.gz"
 relative_path "percona-xtrabackup-percona-xtrabackup-2.4.9"
 
 build do
