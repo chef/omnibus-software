a670136599639405b39736c0c8cdf3d3c5cd6801
mkryva <maksym.kryva@gmail.com>
2018-01-25 10:27:24 +0200

    Update xtrabackup software

--- config/software/xtrabackup.rb
+++ config/software/xtrabackup.rb
@@ -1,16 +1,19 @@
 name "percona-xtrabackup"
 default_version "2.4.9"
 
+skip_transitive_dependency_licensing true
+
 source url: "https://github.com/percona/percona-xtrabackup/archive/percona-xtrabackup-2.4.9.tar.gz",
-       :sha256 => "53e613e12dbd93277fb3004b66d8d2c12476e4febca2bcd2d0f2115dc18cb265",
+       :md5 => "be54cc7f8843fcfa3334917b732ac234",
        :extract => :seven_zip
 relative_path "percona-xtrabackup-percona-xtrabackup-2.4.9"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
-  command "cmake -DBUILD_CONFIG=xtrabackup_release -DWITH_MAN_PAGES=OFF -DDOWNLOAD_BOOST=1 -DWITH_BOOST=/usr/local/boost_1_59_0",
-          env: env
-  make "-j #{workers}", env: env
-  make "install", env: env
-  command "mv -v /usr/local/xtrabackup/bin/* /usr/bin/"
+  command "cmake -DBUILD_CONFIG=xtrabackup_release " \
+          "-DWITH_MAN_PAGES=OFF -DDOWNLOAD_BOOST=1 " \
+          "-DWITH_BOOST=#{install_dir}/embedded/lib/boost_1_59_0", env: env
+
+  make "prefix=#{install_dir}/embedded -j #{workers}", env: env
+  make "prefix=#{install_dir}/embedded install", env: env
 end
