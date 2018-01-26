c10a73ee4617093cc6d4fa82a30ceb5f0dde0c7a
mkryva <maksym.kryva@gmail.com>
2018-01-26 15:33:17 +0200

    Replaced s3 archive to s3 (missed space)

--- config/software/xtrabackup.rb
+++ config/software/xtrabackup.rb
@@ -12,7 +12,7 @@ build do
   env = with_standard_compiler_flags(with_embedded_path)
   command "cmake -DBUILD_CONFIG=xtrabackup_release " \
           "-DWITH_MAN_PAGES=OFF -DDOWNLOAD_BOOST=1 " \
-          "-DWITH_BOOST=#{install_dir}/embedded/lib/boost_1_59_0" \
+          "-DWITH_BOOST=#{install_dir}/embedded/lib/boost_1_59_0 " \
           "-DINSTALL_BINDIR=#{install_dir}/embedded/bin", env: env
 
   make "-j #{workers}", env: env
