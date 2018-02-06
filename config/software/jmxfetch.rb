31fbcf4f3b4fff44b786bb06a8b4567a6d382361
Arthur <bellal.arthur@gmail.com>
2018-02-06 18:34:00 +0100

    Fix JMXFetch jar permissions (#180)

--- config/software/jmxfetch.rb
+++ config/software/jmxfetch.rb
@@ -52,5 +52,6 @@ relative_path "jmxfetch"
 build do
   ship_license "https://raw.githubusercontent.com/DataDog/jmxfetch/master/LICENSE"
   mkdir jar_dir
-  copy "jmxfetch-*-jar-with-dependencies.jar", jar_dir
+  copy "jmxfetch-#{version}-jar-with-dependencies.jar", jar_dir
+  block { File.chmod(0644, "#{jar_dir}/jmxfetch-#{version}-jar-with-dependencies.jar") }
 end
