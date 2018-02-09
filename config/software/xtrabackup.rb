97ec4165a53016f70ae8d64d18850c9b1620da34
Aleksandr Kuzminsky <aleks@twindb.com>
2018-02-08 22:40:13 -0800

    build xtrabackup in one thread because if fails otherwise

--- config/software/xtrabackup.rb
+++ config/software/xtrabackup.rb
@@ -9,14 +9,16 @@ source url: "https://s3.amazonaws.com/twindb-release/percona-xtrabackup-2.4.9.ta
 relative_path "percona-xtrabackup-2.4.9"
 whitelist_file /.*/
 
+workers = 1
+
 build do
-  env = with_standard_compiler_flags(with_embedded_path)
-  command "cmake -DBUILD_CONFIG=xtrabackup_release " \
+    env = with_standard_compiler_flags(with_embedded_path)
+    command "cmake -DBUILD_CONFIG=xtrabackup_release " \
           "-DWITH_MAN_PAGES=OFF -DDOWNLOAD_BOOST=1 " \
           "-DWITH_BOOST=#{install_dir}/libboost " \
           "-DWITH_SSL=system " \
           "-DINSTALL_BINDIR=#{install_dir}/embedded/bin", env: env
 
-  make "-j #{workers}", env: env
-  make "install", env: env
+    make "-j #{workers}", env: env
+    make "install", env: env
 end
