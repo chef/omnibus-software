6205508517388f75c43795a7bd7fcef1fd642067
Aleksandr Kuzminsky <aleks@twindb.com>
2018-02-23 21:52:47 -0800

    sourceforge.net changed zlib mirror

--- config/software/zlib.rb
+++ config/software/zlib.rb
@@ -24,7 +24,8 @@ version "1.2.6" do
   source md5: "618e944d7c7cd6521551e30b32322f4a"
 end
 
-source url: "http://downloads.sourceforge.net/project/libpng/zlib/#{version}/zlib-#{version}.tar.gz"
+# source url: "http://downloads.sourceforge.net/project/libpng/zlib/#{version}/zlib-#{version}.tar.gz"
+source url: "https://ayera.dl.sourceforge.net/project/libpng/zlib/#{version}/zlib-#{version}.tar.gz"
 
 license "Zlib"
 license_file "README"
