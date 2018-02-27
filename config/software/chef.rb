a6d734848b76d045e3e5e2228dbfe6ba7503ad54
Lamont Granquist <lamont@scriptkiddie.org>
2018-02-27 14:03:32 -0800

    also exclude dev and test from chef appbundle
    these need to be installed in order to run rspec tests, but we
    don't need to pin in the bin stub.
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- config/software/chef.rb
+++ config/software/chef.rb
@@ -89,7 +89,7 @@ build do
     copy "distro/powershell/chef/*", "#{install_dir}/modules/chef"
   end
 
-  appbundle "chef", lockdir: project_dir, without: excluded_groups, env: env
+  appbundle "chef", lockdir: project_dir, without: excluded_groups + %w{development test}, env: env
   appbundle "ohai", lockdir: project_dir, without: %w{changelog}, env: env
 
   # Clean up
