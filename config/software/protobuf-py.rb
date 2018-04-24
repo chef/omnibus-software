112834c9f5c83ce5a2392e4f97df3dec110b054c
Jaime Fullaondo <truthbk@gmail.com>
2018-04-24 10:29:45 +0200

    [protobuf] static build + wheel, no bloat + RHEL support (#192)
    * [protobuf] static build and just static wheel is all we need
    
    * [protobuf] adding patch for RHEL GCC 4.1 - no debug symbols on wheel
    
    [protobuf] adjusting patch level
    
    * [protobuf] on windows ship pure-python wheel
    
    * [protobuf] only patch on RHEL5
    
    [protobuf] && not `and`

--- config/software/protobuf-py.rb
+++ config/software/protobuf-py.rb
@@ -1,6 +1,5 @@
 name "protobuf-py"
 default_version "3.5.1"
-rhel_pip_version = "3.5.1"
 
 dependency "python"
 dependency "setuptools"
@@ -12,39 +11,41 @@ source :url => "https://github.com/google/protobuf/releases/download/v#{version}
 
 relative_path "protobuf-#{version}/python"
 
-env = {}
+env = {
+  "CFLAGS" => "-fPIC",
+  "CXXFLAGS" => "-fPIC",
+}
 
 if ohai["platform_family"] == "mac_os_x"
   env["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
 end
 
 build do
-  ship_license "https://raw.githubusercontent.com/google/protobuf/3.1.x/LICENSE"
+  ship_license "https://raw.githubusercontent.com/google/protobuf/3.5.x/LICENSE"
 
-  # Note: RHEL5 is equipped with gcc4.1 that is not supported by Protobuf (it actually crashes during the build)
-  # so we use the official package from PyPI and skip the CPP extension for the time being.
-  if ohai["platform_family"] == "debian"
+  if windows?
+    pip "install protobuf==#{version}"
+  else
     # C++ runtime
     command ["cd .. && ./configure",
                 "--prefix=#{install_dir}/embedded",
-                "--enable-static=no",
-                "--without-zlib"].join(" ")
+                "--without-zlib"].join(" "), :env => env
 
     # You might want to temporarily uncomment the following line to check build sanity (e.g. when upgrading the
     # library) but there's no need to perform the check every time.
     # command "cd .. && make check"
     command "cd .. && make -j #{workers}"
-    command "cd .. && make install"
 
+    if ohai["platform_family"] == "rhel" && ohai["platform_version"].to_i == 5
+      patch :source => "setup-py-no-debug-symbols-for-gcc-41.patch", :plevel => 2
+    end
     # Python lib
-    command "#{install_dir}/embedded/bin/python setup.py build --cpp_implementation", :env => env
+    command ["#{install_dir}/embedded/bin/python",
+           "setup.py",
+           "build",
+           "--cpp_implementation",
+           "--compile_static_extension"].join(" "), :env => env
     command "#{install_dir}/embedded/bin/python setup.py test --cpp_implementation", :env => env
     pip "install . --install-option=\"--cpp_implementation\""
-
-    # We don't need protoc anymore
-    delete "#{install_dir}/embedded/lib/libprotoc.*"
-    delete "#{install_dir}/embedded/bin/protoc"
-  else
-    pip "install protobuf==#{rhel_pip_version}"
   end
 end
