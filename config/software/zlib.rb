1ebba07da4f1cd3f92d7f96da1723f2c670cb222
Tim Smith <tsmith@chef.io>
2018-06-01 10:02:32 -0700

    Minor formatting updates and switching to sha256
    Why not
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- config/software/zlib.rb
+++ config/software/zlib.rb
@@ -17,15 +17,9 @@
 name "zlib"
 default_version "1.2.11"
 
-version "1.2.11" do
-  source sha256: "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
-end
-version "1.2.8" do
-  source md5: "44d667c142d7cda120332623eab69f40"
-end
-version "1.2.6" do
-  source md5: "618e944d7c7cd6521551e30b32322f4a"
-end
+version("1.2.11") { source sha256: "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1" }
+version("1.2.8") { source sha256: "36658cb768a54c1d4dec43c3116c27ed893e88b02ecfcb44f2166f9c0b7f2a0d" }
+version("1.2.6") { source sha256: "21235e08552e6feba09ea5e8d750805b3391c62fb81c71a235c0044dc7a8a61b" }
 
 source url: "https://zlib.net/fossils/zlib-#{version}.tar.gz"
 
