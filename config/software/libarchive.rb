a97801cd9a51331bf9d690a96dc7fcd44b62a8f8
Tim Smith <tsmith@chef.io>
2018-09-14 11:11:38 -0700

    Update libarchive to 3.3.3
    Changes:
    
    Jul 19, 2018: Avoid super-linear slowdown on malformed mtree files
    Jan 27, 2018: Many fixes for building with Visual Studio
    Oct 19, 2017: NO_OVERWRITE doesn't change existing directory attributes
    Aug 12, 2017: New support for Zstandard read and write filters
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- config/software/libarchive.rb
+++ config/software/libarchive.rb
@@ -18,12 +18,13 @@
 # https://github.com/berkshelf/api.berkshelf.com
 
 name "libarchive"
-default_version "3.3.2"
+default_version "3.3.3"
 
 license "BSD-2-Clause"
 license_file "COPYING"
 skip_transitive_dependency_licensing true
 
+version("3.3.3") { source sha256: "ba7eb1781c9fbbae178c4c6bad1c6eb08edab9a1496c64833d1715d022b30e2e" }
 version("3.3.2") { source sha256: "ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa0ce" }
 version("3.1.2") { source sha256: "eb87eacd8fe49e8d90c8fdc189813023ccc319c5e752b01fb6ad0cc7b2c53d5e" }
 
