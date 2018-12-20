9aa99dcc64461dde9430864ab0a32701dc62f0e4
Rémi Calixte <remi.calixte@datadoghq.com>
2018-12-20 16:09:25 -0500

    Add python3 for windows

--- config/software/python3.rb
+++ config/software/python3.rb
@@ -2,60 +2,72 @@ name "python3"
 
 default_version "3.7.1"
 
-dependency "libffi"
-dependency "ncurses"
-dependency "zlib"
-dependency "openssl"
-dependency "bzip2"
-dependency "libsqlite3"
-dependency "liblzma"
-dependency "readline"
+if ohai["platform"] != "windows"
+  dependency "libffi"
+  dependency "ncurses"
+  dependency "zlib"
+  dependency "openssl"
+  dependency "bzip2"
+  dependency "libsqlite3"
+  dependency "liblzma"
+  dependency "readline"
 
-version "3.6.7" do
-  source :sha256 => "b7c36f7ed8f7143b2c46153b7332db2227669f583ea0cce753facf549d1a4239"
-end
+  version "3.6.7" do
+    source :sha256 => "b7c36f7ed8f7143b2c46153b7332db2227669f583ea0cce753facf549d1a4239"
+  end
 
-version "3.7.1" do
-  source :sha256 => "36c1b81ac29d0f8341f727ef40864d99d8206897be96be73dc34d4739c9c9f06"
-end
+  version "3.7.1" do
+    source :sha256 => "36c1b81ac29d0f8341f727ef40864d99d8206897be96be73dc34d4739c9c9f06"
+  end
 
-source :url => "https://python.org/ftp/python/#{version}/Python-#{version}.tgz"
+  source :url => "https://python.org/ftp/python/#{version}/Python-#{version}.tgz"
 
-relative_path "Python-#{version}"
+  relative_path "Python-#{version}"
 
-python_configure = ["./configure",
-                    "--prefix=#{install_dir}/embedded"]
+  python_configure = ["./configure",
+                      "--prefix=#{install_dir}/embedded"]
 
-if mac_os_x?
-  python_configure.push("--enable-ipv6",
-                        "--with-universal-archs=intel",
-                        "--enable-shared")
-elsif linux?
-  python_configure.push("--enable-shared",
-                        "--enable-ipv6")
-elsif aix?
-  # something here... 
-end
+  if mac_os_x?
+    python_configure.push("--enable-ipv6",
+                          "--with-universal-archs=intel",
+                          "--enable-shared")
+  elsif linux?
+    python_configure.push("--enable-shared",
+                          "--enable-ipv6")
+  elsif aix?
+    # something here...
+  end
 
-python_configure.push("--with-dbmliborder=")
+  python_configure.push("--with-dbmliborder=")
 
-build do
-  ship_license "PSFL"
+  build do
+    ship_license "PSFL"
 
-  env = case ohai["platform"]
-        when "aix"
-          aix_env
-        else
-          {
-            "CFLAGS" => "-I#{install_dir}/embedded/include -O2 -g -pipe",
-            "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib",
-          }
-        end
-  command python_configure.join(" "), :env => env
-  command "make -j #{workers}", :env => env
-  command "make install", :env => env
-  # delete "#{install_dir}/embedded/lib/python2.7/test"
+    env = case ohai["platform"]
+          when "aix"
+            aix_env
+          else
+            {
+              "CFLAGS" => "-I#{install_dir}/embedded/include -O2 -g -pipe",
+              "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib",
+            }
+          end
+    command python_configure.join(" "), :env => env
+    command "make -j #{workers}", :env => env
+    command "make install", :env => env
+    # delete "#{install_dir}/embedded/lib/python2.7/test"
 
-  link "#{install_dir}/embedded/bin/python3", "#{install_dir}/embedded/bin/python"
-  link "#{install_dir}/embedded/bin/pip3", "#{install_dir}/embedded/bin/pip"
+    link "#{install_dir}/embedded/bin/python3", "#{install_dir}/embedded/bin/python"
+    link "#{install_dir}/embedded/bin/pip3", "#{install_dir}/embedded/bin/pip"
+  end
+
+else
+  default_version "3.7.1"
+
+  source :url => "https://s3.amazonaws.com/dd-agent-omnibus/python-windows-#{version}-amd64.zip",
+         :sha256 => "26ee710e18334e37a881b94931ebc19e7b24018721c930fc7a5c6e6d28e149fd"
+
+  build do
+    command "XCOPY /YEHIR .\\embedded \"#{windows_safe_path(install_dir)}\\embedded\""
+  end
 end
