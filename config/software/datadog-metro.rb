378abdca9403ec4a1127e2b0fb47334f28217c47
Jaime Fullaondo <truthbk@gmail.com>
2019-01-07 14:35:41 +0100

    [go-metro] gopacket now patches a different file  (#281)
    * [metro] adding new metro patch for gopacket
    
    * [datadog-metro] fix up patch target - different file
    
    * [travis] fixing setup pinning bundler

--- config/software/datadog-metro.rb
+++ config/software/datadog-metro.rb
@@ -44,7 +44,7 @@ build do
     command "#{gobin} get -v -d gopkg.in/yaml.v2", :env => env, :cwd => "/var/cache/omnibus/src/datadog-metro"
     patch :source => "libpcap-static-link.patch", :plevel => 1,
           :acceptable_output => "Reversed (or previously applied) patch detected",
-          :target => "/var/cache/omnibus/src/datadog-metro/src/github.com/google/gopacket/pcap/pcap.go"
+          :target => "/var/cache/omnibus/src/datadog-metro/src/github.com/google/gopacket/pcap/pcap_unix.go"
     command "#{gobin} build -o #{install_dir}/bin/go-metro.bin github.com/DataDog/go-metro", :env => env, :cwd => "/var/cache/omnibus/src/datadog-metro"
   end
 end
