da4f191920804bb8d5bba770bcb633fd027480ec
Tim Smith <tsmith@chef.io>
2019-02-26 11:00:06 -0800

    Remove libsodium dep from chef
    We're not going to use rbnacl which required this. We'll get ed25519
    support via net-ssh 5.x
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- config/software/chef.rb
+++ config/software/chef.rb
@@ -53,7 +53,6 @@ dependency "bundler"
 dependency "ohai"
 dependency "appbundler"
 dependency "libarchive" # for archive resource
-dependency "libsodium" # for ed25519 support
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
