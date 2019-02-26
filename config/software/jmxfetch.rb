4050f72b9324948789e36500be2ac25dfd3dfb87
Jaime Fullaondo <truthbk@gmail.com>
2019-02-26 13:43:21 +0100

    [jmxfetch] update with 0.26.1 (#295)

--- config/software/jmxfetch.rb
+++ config/software/jmxfetch.rb
@@ -1,7 +1,7 @@
 name "jmxfetch"
 
 if windows?
-  default_version "0.26.0"
+  default_version "0.26.1"
 else
   jmx_version = ENV["JMX_VERSION"]
   if jmx_version.nil? || jmx_version.empty?
@@ -9,6 +9,10 @@ else
   else
     default_version jmx_version
   end
+end
+
+version "0.26.1" do
+  source sha256: "e25fdb6173ea357d6c161228bba7a4c0b60151c89e1bb094f17850d475e4616e"
 end
 
 version "0.26.0" do
