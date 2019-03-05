1d6bbcef569766b42b1494766d989c2e078e407e
Massimiliano Pippi <mpippi@gmail.com>
2019-03-05 21:25:13 +0100

    forgot to add back

--- config/software/cmake.rb
+++ config/software/cmake.rb
@@ -3,7 +3,8 @@ name "cmake"
 default_version "3.13.4"
 
 source :url => "https://github.com/Kitware/CMake/releases/download/v#{version}/cmake-#{version}.tar.gz",
-       :sha256 => "fdd928fee35f472920071d1c7f1a6a2b72c9b25e04f7a37b409349aef3f20e9b"
+       :sha256 => "fdd928fee35f472920071d1c7f1a6a2b72c9b25e04f7a37b409349aef3f20e9b",
+       :extract => :seven_zip
 
 relative_path "cmake-#{version}"
 
