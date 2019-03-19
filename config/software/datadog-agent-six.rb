89118210cea63109f96bd46366081c2797aeedd1
Derek Brown <derek.brown@datadoghq.com>
2019-03-19 11:55:48 -0700

    Get `six` directly from master rather than tarball

--- config/software/datadog-agent-six.rb
+++ config/software/datadog-agent-six.rb
@@ -1,5 +1,5 @@
 name "datadog-agent-six"
-default_version "0.1.0"
+default_version "master"
 
 dependency "python2"
 dependency "python3"
@@ -8,9 +8,7 @@ license "Apache"
 license_file "LICENSE"
 skip_transitive_dependency_licensing true
 
-source :url => "https://github.com/DataDog/datadog-agent-six/archive/v#{version}.tar.gz",
-       :sha256 => "ec6a63b38d99b6159a28f05c6c29c653a767c67323a58976bc2e05913b7b8733",
-       :extract => :seven_zip
+source :git => "git://github.com/DataDog/datadog-agent-six"
 
 relative_path "datadog-agent-six-#{version}"
 
