1d96e7b4cbf496aea23ca4b0e3450e490dd1badd
Derek Brown <derek.brown@datadoghq.com>
2019-03-19 09:40:16 -0700

    add windows def to six definition

--- config/software/datadog-agent-six.rb
+++ config/software/datadog-agent-six.rb
@@ -14,14 +14,27 @@ source :url => "https://github.com/DataDog/datadog-agent-six/archive/v#{version}
 
 relative_path "datadog-agent-six-#{version}"
 
-build do
-  env = {
-    "Python2_ROOT_DIR" => "#{install_dir}/embedded",
-    "Python3_ROOT_DIR" => "#{install_dir}/embedded",
-    "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib",
-  }
+if ohai["platform"] != "windows"
+  build do
+    env = {
+        "Python2_ROOT_DIR" => "#{install_dir}/embedded",
+        "Python3_ROOT_DIR" => "#{install_dir}/embedded",
+        "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib",
+    }
 
-  command "cmake -DCMAKE_INSTALL_PREFIX:PATH=#{install_dir}/embedded .", :env => env
-  command "make -j #{workers}"
-  command "make install"
+    command "cmake -DCMAKE_INSTALL_PREFIX:PATH=#{install_dir}/embedded .", :env => env
+    command "make -j #{workers}"
+    command "make install"
+  end
+else
+ build do
+    env = {
+        "Python2_ROOT_DIR" => "#{windows_safe_path(python_2_embedded)}",
+        "Python3_ROOT_DIR" => "#{windows_safe_path(python_3_embedded)}",
+    }
+
+    command "cmake -G \"Unix Makefiles\" .", :env => env
+    command "make -j #{workers}"
+    command "make install"
+  end
 end
