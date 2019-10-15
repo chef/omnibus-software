9e18e91660722aa5538747d98d6272fdea3e6ac4
Lance Albertson <lance@osuosl.org>
2019-10-15 17:28:38 -0400

    Update default version to 2.69 which is required for automake-1.16
    Also update to using sha256 checksums.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/autoconf.rb
+++ config/software/autoconf.rb
@@ -15,7 +15,7 @@
 #
 
 name "autoconf"
-default_version "2.68"
+default_version "2.69"
 
 license "GPL-3.0"
 license_file "COPYING"
@@ -25,10 +25,10 @@ skip_transitive_dependency_licensing true
 dependency "m4"
 
 version "2.69" do
-  source md5: "82d05e03b93e45f5a39b828dc9c6c29b"
+  source sha256: "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969"
 end
 version "2.68" do
-  source md5: "c3b5247592ce694f7097873aa07d66fe"
+  source sha256: "eff70a2916f2e2b3ed7fe8a2d7e63d72cf3a23684b56456b319c3ebce0705d99"
 end
 
 source url: "https://ftp.gnu.org/gnu/autoconf/autoconf-#{version}.tar.gz"
