f05b2f19350819a9c5ab274a0ef7053a3585dc16
Lincoln Baker <lbaker@chef.io>
2020-01-29 15:48:11 -0700

    Revert "WIP add additional rabbitmq versions"
    rabbitmq is going away...
    
    This reverts commit 6e3132b7f26d0951b784a336355b2004de69dbd4.

--- config/software/rabbitmq.rb
+++ config/software/rabbitmq.rb
@@ -23,16 +23,16 @@ skip_transitive_dependency_licensing true
 
 dependency "erlang"
 
-version("2.7.1")  { source md5: "34a5f9fb6f22e6681092443fcc80324f" }
-version("2.8.7")  { source md5: "35e8d78f8d7ae4372db23fe50db82c64" }
-version("3.3.4")  { source md5: "61a3822f3af0aaa30da7230dccb17067" }
-version("3.6.0")  { source md5: "61a3822f3af0aaa30da7230dccb17067" }
-version("3.6.15") { source md5: "1dd9a5d000e11064efec23c1a76e5d5c" }
 version("3.7.6")  { source md5: "d8db2d979a66e520b23be29d9b43af70" }
+version("3.6.15") { source md5: "1dd9a5d000e11064efec23c1a76e5d5c" }
+version("3.6.0")  { source md5: "61a3822f3af0aaa30da7230dccb17067" }
+version("3.3.4")  { source md5: "61a3822f3af0aaa30da7230dccb17067" }
+version("2.8.7")  { source md5: "35e8d78f8d7ae4372db23fe50db82c64" }
+version("2.7.1")  { source md5: "34a5f9fb6f22e6681092443fcc80324f" }
 
-version("3.6.6") do
-  source(sha256: "781d17a6c8bbfbcd749d23913218de38e692a5e3093cf47eecf499532ac25d61",
-            url: "http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.6/rabbitmq-server-generic-unix-3.6.6.tar.xz" )
+version("3.7.6") do
+  source(sha256: "23e61ec405fea9903c2de9532013feb124a5bc667a43a010c949276eda17e1d4",
+            url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.6/rabbitmq-server-generic-unix-3.7.6.tar.xz" )
 end
 
 version("3.6.15") do
@@ -40,61 +40,10 @@ version("3.6.15") do
             url: "https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.15/rabbitmq-server-generic-unix-3.6.15.tar.xz" )
 end
 
-version("3.7.6") do
-  source(sha256: "23e61ec405fea9903c2de9532013feb124a5bc667a43a010c949276eda17e1d4",
-            url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.6/rabbitmq-server-generic-unix-3.7.6.tar.xz" )
+version("3.6.6") do
+  source(sha256: "781d17a6c8bbfbcd749d23913218de38e692a5e3093cf47eecf499532ac25d61",
+            url: "http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.6/rabbitmq-server-generic-unix-3.6.6.tar.xz" )
 end
-
-################# testing multiple versions for update-erlang22 work
-# version("3.7.7") do
-#   source(sha256: "",
-#             url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.6/rabbitmq-server-generic-unix-3.7.6.tar.xz" )
-# end
-# version("3.7.8") do
-#   source(sha256: "",
-#             url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.6/rabbitmq-server-generic-unix-3.7.6.tar.xz" )
-# end
-# version("3.7.9") do
-#   source(sha256: "",
-#             url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.6/rabbitmq-server-generic-unix-3.7.6.tar.xz" )
-# end
-# version("3.7.10") do
-#   source(sha256: "",
-#             url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.6/rabbitmq-server-generic-unix-3.7.6.tar.xz" )
-# end
-# version("3.7.11") do
-#   source(sha256: "",
-#             url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.6/rabbitmq-server-generic-unix-3.7.6.tar.xz" )
-# end
-# version("3.7.12") do
-#   source(sha256: "",
-#             url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.6/rabbitmq-server-generic-unix-3.7.6.tar.xz" )
-# end
-# version("3.7.13") do
-#   source(sha256: "",
-#             url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.6/rabbitmq-server-generic-unix-3.7.6.tar.xz" )
-# end
-# version("3.7.14") do
-#   source(sha256: "",
-#             url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.6/rabbitmq-server-generic-unix-3.7.6.tar.xz" )
-# end
-version("3.7.15") do
-  source(sha256: "4d70e4d057c6e40022369584626fd7c2bdaa24997aae5cc7f8f182c6d46456cf",
-            url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.15/rabbitmq-server-generic-unix-3.7.15.tar.xz" )
-end
-version("3.7.16") do
-  source(sha256: "a34011f8ff1682a1601d4b8e0167ad39b91fd8f0fb35b484c41efde9f104ed08",
-            url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.16/rabbitmq-server-generic-unix-3.7.16.tar.xz" )
-end
-version("3.7.17") do
-  source(sha256: "83abd26dc2f27ea6eee85324163aecd36f1b861e58a027df83f631cbd89ead5d",
-            url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.17/rabbitmq-server-generic-unix-3.7.17.tar.xz" )
-end
-version("3.7.18") do
-  source(sha256: "30a8439671f44f0fc9611782fdfaabba5c1773da85fca279e2850ad28442a2d5",
-            url: "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.18/rabbitmq-server-generic-unix-3.7.18.tar.xz" )
-end
-#################
 
 relative_path "rabbitmq_server-#{version}"
 
