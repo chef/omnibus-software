b9e2d5778f30907bb945885ed5e62f26510498f4
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2020-04-06 08:07:38 -0700

    update xtrabackup

--- config/software/xtrabackup.rb
+++ config/software/xtrabackup.rb
@@ -1,24 +1,28 @@
-name "xtrabackup"
-default_version "2.4.9"
+name 'xtrabackup'
+default_version '2.4.13'
 
 skip_transitive_dependency_licensing true
+dependency 'libffi'
 
-source url: "https://s3.amazonaws.com/twindb-release/percona-xtrabackup-2.4.9.tar.gz",
-       md5: "fca658cb3b004d2a2df342a136125f84"
+version('2.4.12') { source md5: 'c086206421a77f7c1ad28771a75cf396'}
+version('2.4.13') { source md5: 'f8a5170e0e6a6b26125ba935298452c1'}
 
-relative_path "percona-xtrabackup-2.4.9"
-# whitelist_file /.*/
+source url: "https://s3.amazonaws.com/twindb-release/percona-xtrabackup-#{version}.tar.gz"
 
-workers = 1
+relative_path "percona-xtrabackup-#{version}"
+whitelist_file /.*/
+
+workers = 2
 
 build do
-  env = with_standard_compiler_flags(with_embedded_path)
-  command "cmake -DBUILD_CONFIG=xtrabackup_release " \
-        "-DWITH_MAN_PAGES=OFF -DDOWNLOAD_BOOST=1 " \
-        "-DWITH_BOOST=#{install_dir}/libboost " \
-        "-DWITH_SSL=system " \
-        "-DINSTALL_BINDIR=#{install_dir}/embedded/bin", env: env
+    env = with_standard_compiler_flags(with_embedded_path)
+    command 'cmake -DBUILD_CONFIG=xtrabackup_release ' \
+          '-DWITH_MAN_PAGES=OFF -DDOWNLOAD_BOOST=1 ' \
+          "-DWITH_BOOST=#{install_dir}/libboost " \
+          '-DWITH_SSL=system ' \
+          "-DINSTALL_BINDIR=#{install_dir}/embedded/bin", env: env
 
-  make "-j #{workers}", env: env
-  make "install", env: env
+    make "-j #{workers}", env: env
+    make 'install', env: env
+    delete "#{install_dir}/libboost"
 end
