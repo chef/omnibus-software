32b07a6f8c137c2bd0efb0a557137537092d0274
Tim Smith <tsmith84@gmail.com>
2020-08-04 20:52:54 -0700

    Update cmake to 3.18.1
    Update to the latest and greatest.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- config/software/cmake.rb
+++ config/software/cmake.rb
@@ -1,5 +1,5 @@
 #
-# Copyright 2012-2014 Chef Software, Inc.
+# Copyright:: Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,22 +15,22 @@
 #
 
 name "cmake"
-default_version "3.11.3"
+default_version "3.18.1"
 
 dependency "cacerts"
 
 license "BSD-3-Clause"
 skip_transitive_dependency_licensing true
-version("3.11.3") { source sha256: "287135b6beb7ffc1ccd02707271080bbf14c21d80c067ae2c0040e5f3508c39a" }
+version("3.18.1") { source sha256: "c0e3338bd37e67155b9d1e9526fec326b5c541f74857771b7ffed0c46ad62508" }
 minor_version = version.split(".")[0..1].join(".")
 
 if windows?
   if windows_arch_i386?
-    source url: "https://cmake.org/files/v#{minor_version}/cmake-#{version}-win32-x86.zip", sha256: "d444da334688451e467f72e7b3617900c4e39cb6dce44cb2ad650b0e7ced02d3"
+    source url: "https://cmake.org/files/v#{minor_version}/cmake-#{version}-win32-x86.zip", sha256: "1a20c049e094d9ad49caca4b4d713c75c924a3885ecec4ed3986344aab05b6eb"
     relative_path "cmake-#{version}-win32-x86"
     license_file "doc/cmake/Copyright.txt"
   else
-    source url: "https://cmake.org/files/v#{minor_version}/cmake-#{version}-win64-x64.zip", sha256: "d275d176d7a249c6156b260d19a8049a2032c7e3f1bde04fcf13ce9c7ca895cd"
+    source url: "https://cmake.org/files/v#{minor_version}/cmake-#{version}-win64-x64.zip", sha256: "2c6c06da43c1088fc3a673e4440c8ebb1531bb6511134892c0589aa0b94f11ad"
     relative_path "cmake-#{version}-win64-x64"
     license_file "doc/cmake/Copyright.txt"
   end
@@ -44,7 +44,7 @@ build do
   # It's hard-slash-impossible to build cmake on windows without cmake, so we don't even try
   if windows?
     copy "bin/cmake.exe", "#{install_dir}/embedded/bin"
-    copy "share/cmake-3.11", "#{install_dir}/embedded/share/"
+    copy "share/cmake-3.18", "#{install_dir}/embedded/share/"
     copy "share/aclocal/cmake.m4", "#{install_dir}/embedded/share/aclocal/"
   else
     env = with_standard_compiler_flags(with_embedded_path)
