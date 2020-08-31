6cd97a9572b767511fdc480736819abaa76a9b29
Timothy Ehlers <tehlers@expedia.com>
2020-08-31 12:52:53 -0500

    add missing deps
    Signed-off-by: Timothy Ehlers <tehlers@expedia.com>

--- config/software/ruby-cleanup.rb
+++ config/software/ruby-cleanup.rb
@@ -26,6 +26,8 @@ default_version "1.0.0"
 license :project_license
 skip_transitive_dependency_licensing true
 
+dependency "ruby"
+
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
