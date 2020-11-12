7d58ab18e3e9d6c16fb80eeb3578e9c128ad44c4
Tim Smith <tsmith84@gmail.com>
2020-11-12 10:43:00 -0800

    Update for omnibus 6
    The solaris11? helper is now solaris2?
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- config/software/perl.rb
+++ config/software/perl.rb
@@ -1,5 +1,5 @@
 #
-# Copyright 2014 Chef Software, Inc.
+# Copyright:: Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ build do
 
   patch source: "perl-#{version}-remove_lnsl.patch", plevel: 1, env: env
 
-  if solaris_11?
+  if solaris2?
     cc_command = "-Dcc='gcc -m64 -static-libgcc'"
   elsif aix?
     cc_command = "-Dcc='/opt/IBM/xlc/13.1.0/bin/cc_r -q64'"
