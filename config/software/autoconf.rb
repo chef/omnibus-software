2e4aec0790be96bcb06bca35958b9d66e697be4d
Swati Keshari <skeshari@msystechnologies.com>
2021-03-31 19:16:50 +0530

    Updating autoconf from 2.69 to 2.71
    The following versions are also now available:
    * 2.70
    * 2.71
    
    Signed-off-by: Swati Keshari <skeshari@msystechnologies.com>

--- config/software/autoconf.rb
+++ config/software/autoconf.rb
@@ -15,7 +15,7 @@
 #
 
 name "autoconf"
-default_version "2.69"
+default_version "2.71"
 
 license "GPL-3.0"
 license_file "COPYING"
@@ -24,7 +24,8 @@ skip_transitive_dependency_licensing true
 
 dependency "m4"
 
-version("2.69") { source sha256: "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969" }
+version("2.71") { source sha256: "431075ad0bf529ef13cb41e9042c542381103e80015686222b8a9d4abef42a1c" }
+version("2.70") { source sha256: "f05f410fda74323ada4bdc4610db37f8dbd556602ba65bc843edb4d4d4a1b2b7" }
 
 source url: "https://ftp.gnu.org/gnu/autoconf/autoconf-#{version}.tar.gz"
 
