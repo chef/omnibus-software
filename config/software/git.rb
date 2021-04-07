795b30bd77b8205f0e9d02da2cb7b6d6ae90969b
Swati Keshari <skeshari@msystechnologies.com>
2021-04-07 19:52:38 +0530

    Updating git from 2.29.2 to 2.31.1
    The following versions are also now available:
    * 2.29.3
    * 2.30.2
    * 2.31.1
    
    Signed-off-by: Swati Keshari <skeshari@msystechnologies.com>

--- config/software/git.rb
+++ config/software/git.rb
@@ -30,9 +30,7 @@ dependency "expat"
 
 relative_path "git-#{version}"
 
-
 # version_list: url=https://github.com/git/git/releases filter=*.tar.gz
-
 version("2.31.1") { source sha256: "b1c0e95e9861b5d1b9ad3d8deaa2d8c7f02304ffc1b5e8869dd9fb98f9a0d436" }
 version("2.30.2") { source sha256: "e637ff90a3465e519885c8c4b7a67657ab0b7b1820e9324d12e50ec2e38e4d0b" }
 version("2.29.3") { source sha256: "58049054e204d53318c183823b6f5aaf6c65ca8a2f4b1e118cf127fb42e882aa" }
