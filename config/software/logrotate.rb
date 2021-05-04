ffe95e07937272138040601ec407012bcd7f300f
jayashri garud <jgarud@msystechnologies.com>
2021-05-04 14:00:19 +0530

    correct checksum and version list path
    Signed-off-by: jayashri garud <jgarud@msystechnologies.com>

--- config/software/logrotate.rb
+++ config/software/logrotate.rb
@@ -25,10 +25,10 @@ dependency "popt"
 
 source url: "https://github.com/logrotate/logrotate/archive/#{version}.tar.gz"
 
-# versions_list: https://github.com/logrotate/logrotate/releases/ filter=*.tar.gz
-version("3.18.0") { source sha256: "ceddb34dd2f33c324413e6eacb548cc5b873276709154895a44cf6259d2d37b6" }
-version("3.17.0") { source sha256: "5db8cf4786e0abeeec64f852d605ef702bfcf18f4d7938dce7d7a00ad4de787c" }
-version("3.16.0") { source sha256: "e1bae2c72115cf16007bb98ea38752d1120010cfb70addd9867d952c7529de3b" }
+# versions_list: https://github.com/logrotate/logrotate/tags filter=*.tar.gz
+version("3.18.0") { source sha256: "66232111a2b02cce3c2436c141b7a2af78fa758aac326ab2b79f9a5b0d749142" }
+version("3.17.0") { source sha256: "c25ea219018b024988b791e91e9f6070c34d2056efa6ffed878067866c0ed765" }
+version("3.16.0") { source sha256: "bc6acfd09925045d48b5ff553c24c567cfd5f59d513c4ac34bfb51fa6b79dc8a" }
 version("3.9.2") { source sha256: "2de00c65e23fa9d7909cae6594e550b9abe9a7eb1553669ddeaca92d30f97009" }
 version("3.8.9") { source sha256: "be4df08cd09271b065f02c79a165cad247acc0506cd7f6cb80f5b5a4e0f8d517" }
 
