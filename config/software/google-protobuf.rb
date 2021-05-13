554ef3b6205d77df6f6d26ec486ff33b54a34fd8
jayashri garud <jgarud@msystechnologies.com>
2021-05-13 12:47:11 +0530

    Updating google-protobuf from 3.5.2 to 3.17.0 The following versions are also now available: * v3.17.0 * v3.16.0
    Signed-off-by: jayashri garud <jgarud@msystechnologies.com>

--- config/software/google-protobuf.rb
+++ config/software/google-protobuf.rb
@@ -21,11 +21,15 @@
 # with the gem.
 
 name "google-protobuf"
-default_version "v3.5.2"
+default_version "v3.17.0"
 
 dependency "ruby"
 
 source git: "https://github.com/google/protobuf.git"
+
+# versions_list: https://github.com/protocolbuffers/protobuf/tags filter=*.tar.gz
+version("v3.17.0")    { source sha256: "eaba1dd133ac5167e8b08bc3268b2d33c6e9f2dcb14ec0f97f3d3eed9b395863" }
+version("v3.16.0")    { source sha256: "7892a35d979304a404400a101c46ce90e85ec9e2a766a86041bb361f626247f5" }
 
 license :project_license
 
