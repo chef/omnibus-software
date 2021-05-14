82d89f102a013888dcc6f449eacd84d8c89056fa
jayashri garud <jgarud@msystechnologies.com>
2021-05-14 13:10:47 +0530

    update checksums from md5 to sha256
    Signed-off-by: jayashri garud <jgarud@msystechnologies.com>

--- config/software/erlang.rb
+++ config/software/erlang.rb
@@ -46,12 +46,12 @@ version("22.1.8")    { source sha256: "7302be70cee2c33689bf2c2a3e7cfee597415d0fb
 version("21.3.8.11") { source sha256: "aab77124285820608cd7a90f6b882e42bb5739283e10a8593d7f5bce9b30b16a" }
 version("21.1")      { source sha256: "7212f895ae317fa7a086fa2946070de5b910df5d41263e357d44b0f1f410af0f" }
 version("20.3.8.9")  { source sha256: "897dd8b66c901bfbce09ed64e0245256aca9e6e9bdf78c36954b9b7117192519" }
-version("20.0")      { source md5:    "2faed2c3519353e6bc2501ed4d8e6ae7" }
+version("20.0")      { source sha256: "22710927ad2e48a0964997bf5becb24abb1f4fed86f5f05af22a9e1df636b787" }
 version("19.3.6.11") { source sha256: "c857ea6d2c901bfb633d9ceeb5e05332475357f185dd5112b7b6e4db80072827" }
 version("18.3.4.9")  { source sha256: "25ef8ba3824cb726c4830abf32c2a2967925b1e33a8e8851dba596e933e2689a" }
-version("18.3")      { source md5:    "7e4ff32f97c36fb3dab736f8d481830b" }
-version("18.2")      { source md5:    "b336d2a8ccfbe60266f71d102e99f7ed" }
-version("18.1")      { source md5:    "fa64015fdd133e155b5b19bf90ac8678" }
+version("18.3")      { source sha256: "fdab8129a1cb935db09f1832e3a7d511a4aeb2b9bb3602ca6a7ccb9730d5c9c3" }
+version("18.2")      { source sha256: "43de28ac307d2ecf26b6134647f8de1cdf7b419517514a7d5c2af04284fb3fd0" }
+version("18.1")      { source sha256: "e4a147228a6b7fa60dce05c8adfb3cbc254d97cf6e45456d93d93adbde8b0f11" }
 
 build do
   if version.satisfies?(">= 18.3")
