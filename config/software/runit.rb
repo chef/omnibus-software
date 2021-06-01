43281caa0546a5dc65317b704351ae9be96f13cc
jayashri garud <jgarud@msystechnologies.com>
2021-06-01 14:48:41 +0530

    Updating runit from 2.1.1 to 2.1.2 The following versions are also now available: * 2.1.2 * 2.1.1
    Signed-off-by: jayashri garud <jgarud@msystechnologies.com>

--- config/software/runit.rb
+++ config/software/runit.rb
@@ -15,12 +15,13 @@
 #
 
 name "runit"
-default_version "2.1.1"
+default_version "2.1.2"
 
 license "BSD-3-Clause"
 license_file "../package/COPYING"
 skip_transitive_dependency_licensing true
 
+# versions_list: http://smarden.org/runit/ filter=*.tar.gz
 version "2.1.2" do
   source md5: "6c985fbfe3a34608eb3c53dc719172c4"
 end
