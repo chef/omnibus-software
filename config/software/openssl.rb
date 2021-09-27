6646b4015576f8cce234674e39187c82706919d2
Lamont Granquist <lamont@scriptkiddie.org>
2021-09-27 09:10:20 -0700

    Turn off ssl weak ciphers
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- config/software/openssl.rb
+++ config/software/openssl.rb
@@ -1,5 +1,5 @@
 #
-# Copyright:: Chef Software Inc.
+# Copyright:: Copyright (c) Chef Software Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -83,6 +83,7 @@ build do
     "no-rc5",
     "no-ssl2",
     "no-ssl3",
+    "no-weak-ssl-ciphers",
     "no-zlib",
     "shared",
   ]
