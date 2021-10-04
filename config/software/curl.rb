d2d0a661ab102a523b68b6937c18bf426281f718
jayashri garud <jgarud@msystechnologies.com>
2021-10-04 17:57:00 +0530

    Updating curl from 7.79.0 to 7.79.1
    Signed-off-by: jayashri garud <jgarud@msystechnologies.com>

--- config/software/curl.rb
+++ config/software/curl.rb
@@ -15,7 +15,7 @@
 #
 
 name "curl"
-default_version "7.79.0"
+default_version "7.79.1"
 
 dependency "zlib"
 dependency "openssl"
@@ -26,6 +26,7 @@ license_file "COPYING"
 skip_transitive_dependency_licensing true
 
 # version_list: url=https://curl.se/download/ filter=*.tar.gz
+version("7.79.1") { source sha256: "370b11201349816287fb0ccc995e420277fbfcaf76206e309b3f60f0eda090c2" }
 version("7.79.0") { source sha256: "aff0c7c4a526d7ecc429d2f96263a85fa73e709877054d593d8af3d136858074" }
 version("7.78.0") { source sha256: "ed936c0b02c06d42cf84b39dd12bb14b62d77c7c4e875ade022280df5dcc81d7" }
 version("7.77.0") { source sha256: "b0a3428acb60fa59044c4d0baae4e4fc09ae9af1d8a3aa84b2e3fbcd99841f77" }
