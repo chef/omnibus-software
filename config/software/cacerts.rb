fc069770d1762662d9c4a393594813b9d91cf3b3
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-10-28 13:21:18 +0200

    [cacerts] 2021-10-26 update (#408)

--- config/software/cacerts.rb
+++ config/software/cacerts.rb
@@ -29,9 +29,9 @@ name "cacerts"
 # cacerts bundle changes.
 # This allows us to always use up-to-date cacerts, without breaking all builds
 # when they change.
-default_version "2021-09-30"
+default_version "2021-10-26"
 source url: "https://curl.se/ca/cacert-#{version}.pem",
-       sha256: "f524fc21859b776e18df01a87880efa198112214e13494275dbcbd9bcb71d976",
+       sha256: "ae31ecb3c6e9ff3154cb7a55f017090448f88482f0e94ac927c0c67a1f33b9cf",
        target_filename: "cacert.pem"
 
 relative_path "cacerts-#{version}"
