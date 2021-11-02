82031b321d31d873eff973f618d53b884d924545
skeshari12 <skeshari@msystechnologies.com>
2021-11-02 17:17:22 +0530

    update autoconf version from 2.69 to 2.71
    Signed-off-by: skeshari12 <skeshari@msystechnologies.com>

--- config/software/autoconf.rb
+++ config/software/autoconf.rb
@@ -16,7 +16,7 @@
 # expeditor/ignore: deprecated 2021-04
 
 name "autoconf"
-default_version "2.69"
+default_version "2.71"
 
 license "GPL-3.0"
 license_file "COPYING"
@@ -25,6 +25,9 @@ skip_transitive_dependency_licensing true
 
 dependency "m4"
 
+# version_list: url:https://ftp.gnu.org/gnu/autoconf filter=*.tar.gz
+
+version("2.71") { source sha256: "431075ad0bf529ef13cb41e9042c542381103e80015686222b8a9d4abef42a1c" }
 version("2.69") { source sha256: "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969" }
 
 source url: "https://ftp.gnu.org/gnu/autoconf/autoconf-#{version}.tar.gz"
