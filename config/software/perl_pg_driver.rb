819fb605fa8a0762c2a9d85b94bc9dcbd20f7abd
skeshari12 <skeshari@msystechnologies.com>
2021-11-18 17:42:32 +0530

    Update perl_pg_driver from 3.14.2 to 3.15.0
    Signed-off-by: skeshari12 <skeshari@msystechnologies.com>
    
    Update perl_pg_driver from 3.14.2 to 3.15.0
    
    Signed-off-by: skeshari12 <skeshari@msystechnologies.com>

--- config/software/perl_pg_driver.rb
+++ config/software/perl_pg_driver.rb
@@ -15,7 +15,7 @@
 #
 
 name "perl_pg_driver"
-default_version "3.14.2"
+default_version "3.15.0"
 
 dependency "perl"
 dependency "cpanminus"
@@ -27,9 +27,10 @@ license_file "LICENSES/artistic.txt"
 
 # version_list: url=https://cpan.metacpan.org/authors/id/T/TU/TURNSTEP/ filter=*.tar.gz
 
-version("3.14.2") { source md5: "6b8fe657f8cc0be8cc2c178f42c1d4ed" }
-version("3.5.3")  { source md5: "21cdf31a8d1f77466920375aa766c164" }
-version("3.3.0")  { source md5: "547de1382a47d66872912fe64282ff55" }
+version("3.15.0") { source sha256: "69cc19870f9d935f16530be39d0ed60afadc5e560e29c3a17af4498e7e3082d9" }
+version("3.14.2") { source sha256: "c973e98458960a78ec54032a71b3840f178418dd7e69d063e462a0f10ec68e4d" }
+version("3.5.3")  { source sha256: "7e98a9b975256a4733db1c0e974cad5ad5cb821489323e395ed97bd058e0a90e" }
+version("3.3.0")  { source sha256: "52f43de5b2d916d447d7ed252b127f728b226dc88db57d4fe9712e21d3586ffe" }
 
 source url: "http://search.cpan.org/CPAN/authors/id/T/TU/TURNSTEP/DBD-Pg-#{version}.tar.gz"
 
