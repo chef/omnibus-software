bf89557a9a9e38a52af94b7d05eccde8a22d7d0c
poornima <poorndm@progress.com>
2022-01-27 21:29:05 +0530

    Add Version-Inspector
    Signed-off-by: poornima <poorndm@progress.com>

---
+++ .expeditor/Version_Inspector/update_prod_list.sh
@@ -0,0 +1,32 @@
+#to update the prod_list.json 
+#cd omnibus-software/config/software
+set -x 
+prod_file="prod_list.json"
+echo "{" >>$prod_file
+for FILE in *.rb; do 
+	echo "File name is :$FILE"
+	prod=$(echo "$FILE" |cut -d "." -f1); 
+        dv=$( grep "default_version" $FILE|  cut -d " " -f2) 
+	if echo $dv | grep -qE '[0-9]\.[0-9]+' ;then
+		url=$(grep "https" $FILE | grep tar | tail -1 |  cut -d ":" -f2 -f3 | cut -d "-" -f1)
+		if [ ! -z "$url" ];then
+			echo "\"$prod\": {" >> $prod_file; 
+			echo "\"default_version\": $dv ,">> $prod_file; 
+			url1=$(dirname $url)
+			echo "\"url\": $url1\"," >> $prod_file ; 
+			if [ $prod == "cmake" ] || [ $prod == "nodejs" ] || [ $prod == "openssl-fips" ] || [ $prod == "postgresql" ] ;then
+				expr1="^v\\\\\d[\\\\\.\\\\\d]*\\\\\d"
+				echo $expr1
+			else
+				expr1="^($prod-)\\\\\d[\\\\\.\\\\\d]*\\\\\d(\\\\\.tar\\\\\.gz)$"
+				echo "EXPR=$expr1"
+			fi
+ 			echo "\"expr1\": \"$expr1\"">>$prod_file 
+			echo "},">>$prod_file		
+		fi
+
+	fi
+ done;
+$(printf '%s\n' '$' 's/.$//' wq | ex $prod_file)
+ echo "}" >>$prod_file 
+
