fbc2c9436ac3f470df3211d2f9e94342f900db98
poornima <poorndm@progress.com>
2022-03-21 15:12:53 +0530

    Update curl - 7.82.0
    Signed-off-by: poornima <poorndm@progress.com>

--- config/software/curl.rb
+++ config/software/curl.rb
@@ -15,7 +15,7 @@
 #
 
 name "curl"
-default_version "7.81.0"
+default_version "7.82.0"
 
 dependency "zlib"
 dependency "openssl"
@@ -26,6 +26,7 @@ license_file "COPYING"
 skip_transitive_dependency_licensing true
 
 # version_list: url=https://curl.se/download/ filter=*.tar.gz
+version("7.82.0") { source sha256: "910cc5fe279dc36e2cca534172c94364cf3fcf7d6494ba56e6c61a390881ddce" }
 version("7.81.0") { source sha256: "ac8e1087711084548d788ef18b9b732c8de887457b81f616fc681d1044b32f98" }
 version("7.80.0") { source sha256: "dab997c9b08cb4a636a03f2f7f985eaba33279c1c52692430018fae4a4878dc7" }
 version("7.79.1") { source sha256: "370b11201349816287fb0ccc995e420277fbfcaf76206e309b3f60f0eda090c2" }
