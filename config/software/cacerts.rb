ff00acd30a4e905058c7ae3c9caf624bc6aa377b
skeshari12 <skeshari@msystechnologies.com>
2022-04-28 16:35:27 +0530

    Update cacert from 2022-03-29 to 2022-04-26 The following versions are also available : * 2022-03-29 * 2022-02-01 * 2021-10-26 * 2021-09-30 * 2021-07-05 * 2021-05-25 * 2021-01-19
    Signed-off-by: skeshari12 <skeshari@msystechnologies.com>

--- config/software/cacerts.rb
+++ config/software/cacerts.rb
@@ -20,11 +20,12 @@ license "MPL-2.0"
 license_file "https://www.mozilla.org/media/MPL/2.0/index.815ca599c9df.txt"
 skip_transitive_dependency_licensing true
 
-default_version "2022-03-29"
+default_version "2022-04-26"
 
 source url: "https://curl.se/ca/cacert-#{version}.pem"
 
 # versions_list: https://curl.se/docs/caextract.html
+version("2022-04-26") { source sha256: "08df40e8f528ed283b0e480ba4bcdbfdd2fdcf695a7ada1668243072d80f8b6f" }
 version("2022-03-29") { source sha256: "1979e7fe618c51ed1c9df43bba92f977a0d3fe7497ffa2a5e80dfc559a1e5a29" }
 version("2022-02-01") { source sha256: "1d9195b76d2ea25c2b5ae9bee52d05075244d78fcd9c58ee0b6fac47d395a5eb" }
 version("2021-10-26") { source sha256: "ae31ecb3c6e9ff3154cb7a55f017090448f88482f0e94ac927c0c67a1f33b9cf" }
