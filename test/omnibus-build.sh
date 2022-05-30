28f6997d22e8cfff90d73002baf155c6c488e2e2
Jeremiah Snapp <jeremiah@chef.io>
2022-05-30 12:00:40 -0400

    Replace load-omnibus-toolchain usage in verify pipeline
    load-omnibus-toolchain was removed from omnibus-toolchain.
    
    Signed-off-by: Jeremiah Snapp <jeremiah@chef.io>

--- test/omnibus-build.sh
+++ test/omnibus-build.sh
@@ -3,8 +3,34 @@
 # shellcheck disable=SC1090
 . ~/.bashrc
 
-# shellcheck disable=SC1091
-. /opt/omnibus-toolchain/bin/load-omnibus-toolchain
+echo "--- Setting Omnibus build environment variables"
+
+export PATH="/opt/omnibus-toolchain/bin:${PATH}"
+echo "PATH=${PATH}"
+
+if [[ -f "/opt/omnibus-toolchain/embedded/ssl/certs/cacert.pem" ]]; then
+  export SSL_CERT_FILE="/opt/omnibus-toolchain/embedded/ssl/certs/cacert.pem"
+  echo "SSL_CERT_FILE=${SSL_CERT_FILE}"
+fi
+
+###################################################################
+# Query tool versions
+###################################################################
+echo ""
+echo ""
+echo "========================================"
+echo "= Tool Versions"
+echo "========================================"
+echo ""
+echo "Bash.........$(bash --version | head -1)"
+echo "Bundler......$(bundle --version | head -1)"
+echo "GCC..........$(gcc --version | head -1)"
+echo "Git..........$(git --version | head -1)"
+echo "Make.........$(make --version | head -1)"
+echo "Ruby.........$(ruby --version | head -1)"
+echo "RubyGems.....$(gem --version | head -1)"
+echo ""
+echo "========================================"
 
 rm -f Gemfile.lock
 rm -rf .bundle
@@ -13,9 +39,12 @@ if [[ $CI == true ]]; then
   DEBUG=1 bundle config set --local without development
 fi
 
+echo "--- Running bundle install"
+
 DEBUG=1 bundle install
 
 if [[ $CI == true ]]; then
+  echo "--- Building"
   bundle exec omnibus build test
   exit $?
 fi
