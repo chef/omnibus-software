c32452fd1998d8444dff9d6af99911ea179dff9b
Alexandre Menasria <47357713+amenasria@users.noreply.github.com>
2022-07-25 17:21:14 +0200

    [cacerts] 2022-07-19 update (#427)
    * [cacerts] 2022-07-19 update
    
    * Fix version and sha

--- config/software/cacerts.rb
+++ config/software/cacerts.rb
@@ -29,9 +29,9 @@ name "cacerts"
 # cacerts bundle changes.
 # This allows us to always use up-to-date cacerts, without breaking all builds
 # when they change.
-default_version "2022-04-26"
+default_version "2022-07-19"
 source url: "https://curl.se/ca/cacert-#{version}.pem",
-       sha256: "08df40e8f528ed283b0e480ba4bcdbfdd2fdcf695a7ada1668243072d80f8b6f",
+       sha256: "6ed95025fba2aef0ce7b647607225745624497f876d74ef6ec22b26e73e9de77",
        target_filename: "cacert.pem"
 
 relative_path "cacerts-#{version}"
