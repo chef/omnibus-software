acfb81022cb56d1d5cf7c232a35a9a7b27417fad
John McCrae <john.mccrae@progress.com>
2022-08-08 09:51:06 -0700

    updating the windows platform from omnibus
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- test/omnibus-build.sh
+++ test/omnibus-build.sh
@@ -45,7 +45,6 @@ DEBUG=1 bundle install
 
 if [[ $CI == true ]]; then
   echo "--- Building"
-  git config --global --add safe.directory /omnibus-software
   bundle exec omnibus build test
   exit $?
 fi
