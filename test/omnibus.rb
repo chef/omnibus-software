1c965fd17c031df4ee1b5c2fa7c63d60a0168e24
John McCrae <john.mccrae@progress.com>
2022-08-08 09:39:28 -0700

    updating the windows platform from omnibus
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- test/omnibus.rb
+++ test/omnibus.rb
@@ -28,9 +28,8 @@
 
 # Windows architecture defaults - set to x86 unless otherwise specified.
 # ------------------------------
-# env_omnibus_windows_arch = (ENV["OMNIBUS_WINDOWS_ARCH"] || "").downcase
-# env_omnibus_windows_arch = :x64 unless %w{x86 x64}.include?(env_omnibus_windows_arch)
-env_omnibus_windows_arch = :x64
+env_omnibus_windows_arch = (ENV["OMNIBUS_WINDOWS_ARCH"] || "").downcase
+env_omnibus_windows_arch = :x64 unless %w{x86 x64}.include?(env_omnibus_windows_arch)
 
 windows_arch env_omnibus_windows_arch
 
