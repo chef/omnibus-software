219b9f08f8791015c2de396f48309f691d488807
poorndm <poorndm@progress.com>
2022-11-04 14:14:28 +0530

    Update bash version to 5.2
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/bash.rb
+++ config/software/bash.rb
@@ -15,17 +15,18 @@
 #
 
 name "bash"
-default_version "5.1.16"
+default_version "5.2"
 
 dependency "libiconv"
 dependency "ncurses"
 
 # version_list: url=https://ftp.gnu.org/gnu/bash/ filter=*.tar.gz
-version("5.2")    { source sha256: "a139c166df7ff4471c5e0733051642ee5556c1cc8a4a78f145583c5c81ab32fb" }
+
 version("5.0")    { source sha256: "b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d" }
 version("5.1")    { source sha256: "cc012bc860406dcf42f64431bcd3d2fa7560c02915a601aba9cd597a39329baa" }
 version("5.1.8")  { source sha256: "0cfb5c9bb1a29f800a97bd242d19511c997a1013815b805e0fdd32214113d6be" }
 version("5.1.16") { source sha256: "5bac17218d3911834520dad13cd1f85ab944e1c09ae1aba55906be1f8192f558" }
+version("5.2")    { source sha256: "a139c166df7ff4471c5e0733051642ee5556c1cc8a4a78f145583c5c81ab32fb" }
 
 license "GPL-3.0"
 license_file "COPYING"
