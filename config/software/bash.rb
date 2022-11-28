ff4d7fc18634d1ca907d621c70160a8cfedb9147
poorndm <poorndm@progress.com>
2022-11-28 16:51:44 +0530

    update ld option
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/bash.rb
+++ config/software/bash.rb
@@ -71,7 +71,7 @@ build do
     env["ac_cv_func_working_mktime"] = "yes"
     if version >= "5.2"
       command configure_command.join(" "), env: env
-      make "-jV #{workers}", env: env
+      make "-j -V #{workers}", env: env
       make "-j #{workers} install", env: env
     end
   end
