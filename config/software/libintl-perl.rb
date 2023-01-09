8f22dc460a015daf8360666aeedbad73c83bb44b
poorndm <poorndm@progress.com>
2023-01-09 11:39:49 +0530

    Update libintl-perl-1.33
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/libintl-perl.rb
+++ config/software/libintl-perl.rb
@@ -40,6 +40,7 @@ dependency "cpanminus"
 
 # version_list: url=https://cpan.metacpan.org/authors/id/G/GU/GUIDO/ filter=libintl-perl-*.tar.gz
 
+version("1.33") { source sha256: "5126eda9ccd0eeb10db82ddef63cbcaf7dbd771e78cc0fb110cc3b5a6b8679e7" }
 version("1.32") { source sha256: "80108298f2564ecbfc7110a3042008e665ed00c2e155b36b0188e6c1135ceba5" }
 version("1.23") { source sha256: "60da16356c2fa89a0c542c825d626c8c2811202b6002b56d8574b928a1379ffa" }
 
