cf8d8247cce5a2025935fd36788cdf6a8df07069
David du Colombier <djc@datadoghq.com>
2023-04-12 09:56:44 +0200

    [bzip2] Replace with upstream version (#444)
    * [bzip2] Replace with upstream version
    
    * [bzip2] Add install_shared_libraries.patch
    
    This patch adds installation of shared libraries in the Makefile.

--- config/software/bzip2.rb
+++ config/software/bzip2.rb
@@ -1,6 +1,5 @@
 #
-# Copyright:: Copyright (c) 2013-2014 Chef Software, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2013-2018 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,34 +17,40 @@
 # This library object is required for building Python with the bz2 module,
 # and should be picked up automatically when building Python.
 
-# This is a Linux/OSX only DSL
-
 name "bzip2"
 default_version "1.0.8"
+
+license "BSD-2-Clause"
+license_file "LICENSE"
+skip_transitive_dependency_licensing true
 
 dependency "zlib"
 dependency "openssl"
 
-source url: "https://s3.amazonaws.com/dd-agent/bzip2/bzip2-#{version}.tar.gz",
-       sha256: "ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269"
+# version_list: url=https://sourceware.org/pub/bzip2/ filter=*.tar.gz
+version("1.0.8") { source sha256: "ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269" }
+
+source url: "https://fossies.org/linux/misc/#{name}-#{version}.tar.gz"
 
 relative_path "#{name}-#{version}"
 
-prefix = "#{install_dir}/embedded"
-libdir = "#{prefix}/lib"
-
-env = {
-  "LDFLAGS" => "-L#{libdir} -I#{prefix}/include",
-  "CFLAGS" => "-L#{libdir} -I#{prefix}/include -fPIC",
-  "LD_RUN_PATH" => libdir,
-}
-
 build do
-  license "BSD-style"
-  license_file "https://sourceware.org/git/?p=bzip2.git;a=blob_plain;f=LICENSE;h=81a37eab7a5be1a34456f38adb74928cc9073e9b;hb=HEAD"
-  patch source: "makefile_take_env_vars.patch"
-  patch source: "soname_install_dir.patch" if ohai["platform_family"] == "mac_os_x"
-  command "make PREFIX=#{prefix} VERSION=#{version}", env: env
-  command "make PREFIX=#{prefix} VERSION=#{version} -f Makefile-libbz2_so", env: env
-  command "make install VERSION=#{version} PREFIX=#{prefix}", env: env
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  # Avoid warning where .rodata cannot be used when making a shared object
+  env["CFLAGS"] << " -fPIC" unless aix?
+
+  # The list of arguments to pass to make
+  args = "PREFIX='#{install_dir}/embedded' VERSION='#{version}'"
+  args << " CFLAGS='-qpic=small -qpic=large -O2 -g -D_ALL_SOURCE -D_LARGE_FILES'" if aix?
+
+  patch source: "makefile_take_env_vars.patch", plevel: 1, env: env
+  patch source: "makefile_no_bins.patch", plevel: 1, env: env # removes various binaries we don't want to ship
+  patch source: "soname_install_dir.patch", env: env if mac_os_x?
+  patch source: "aix_makefile.patch", env: env if aix?
+  patch source: "install_shared_libraries.patch"
+
+  make "#{args}", env: env
+  make "#{args} -f Makefile-libbz2_so", env: env
+  make "#{args} install", env: env
 end
