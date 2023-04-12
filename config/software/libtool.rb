94eb627ad7d6a1195865bec67f04b07ba9023e2d
David du Colombier <djc@datadoghq.com>
2023-04-12 09:57:03 +0200

    [libtool] Replace with upstream version (#447)
    * [libtool] Replace with upstream version
    
    * [libtool] Remove unused versions

--- config/software/libtool.rb
+++ config/software/libtool.rb
@@ -1,5 +1,5 @@
 #
-# Copyright 2012-2014 Chef Software, Inc.
+# Copyright:: Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,22 +15,23 @@
 #
 
 name "libtool"
-default_version "2.4"
+default_version "2.4.7"
 
+license "GPL-2.0"
+license_file "COPYING"
 skip_transitive_dependency_licensing true
 
 dependency "config_guess"
 
-version("2.4")   { source sha256: "13df57ab63a94e196c5d6e95d64e53262834fe780d5e82c28f177f9f71ddf62e" }
+# version_list: url=https://ftp.gnu.org/gnu/libtool/ filter=*.tar.gz
+
+version("2.4.7") { source sha256: "04e96c2404ea70c590c546eba4202a4e12722c640016c12b9b2f1ce3d481e9a8" }
 
 source url: "https://ftp.gnu.org/gnu/libtool/libtool-#{version}.tar.gz"
 
 relative_path "libtool-#{version}"
 
 build do
-  license "GPL-2.0"
-  license_file "./COPYING"
-
   env = with_standard_compiler_flags(with_embedded_path)
 
   update_config_guess
@@ -38,12 +39,15 @@ build do
 
   if aix?
     env["M4"] = "/opt/freeware/bin/m4"
+  elsif solaris2?
+    # We hit this bug on Solaris11 platforms bug#14291: libtool 2.4.2 fails to build due to macro_revision  reversion
+    # The problem occurs with LANG=en_US.UTF-8 but not with LANG=C
+    env["LANG"] = "C"
   end
 
-  mkdir "/tmp/build/embedded"
   command "./configure" \
-          " --prefix=/tmp/build/embedded", env: env
+          " --prefix=#{install_dir}/embedded", env: env
 
-  command "make -j #{workers}", env: env
-  command "make install", env: env
+  make env: env
+  make "install", env: env
 end
