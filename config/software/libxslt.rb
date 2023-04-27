f88914d62f04bb51f983e324713e0bb3f9b00d7e
poorndm <poorndm@progress.com>
2023-04-27 20:53:58 +0530

    Fix solaris patch issue
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/libxslt.rb
+++ config/software/libxslt.rb
@@ -42,9 +42,13 @@ build do
   update_config_guess
 
   env = with_standard_compiler_flags(with_embedded_path)
-
-  patch source: "libxslt-solaris-configure.patch", env: env if solaris2? || omnios? || smartos?
-
+  
+  if version.satisfies?(">= 1.1.37")
+    patch source: "libxslt-solaris-configure-1.1.37.patch", env: env if solaris2? || omnios? || smartos?
+  else
+    patch source: "libxslt-solaris-configure.patch", env: env if solaris2? || omnios? || smartos?
+  end
+  
   if windows?
     patch source: "libxslt-windows-relocate.patch", env: env
   end
