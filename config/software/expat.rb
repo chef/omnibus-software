1b5ece7df9766444ac331d3699cddf79ab042b59
David du Colombier <djc@datadoghq.com>
2023-05-15 17:55:35 +0200

    [expat] Replace with upstream version (#450)
    * [expat] Replace with upstream version
    
    * [expat] Remove unused versions
    
    * [expat] Don't build static library

--- config/software/expat.rb
+++ config/software/expat.rb
@@ -1,21 +1,58 @@
+#
+# Copyright 2014 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 name "expat"
-default_version "2.1.1"
+default_version "2.5.0"
 
-relative_path "expat-2.1.1"
+relative_path "expat-#{version}"
+dependency "config_guess"
 
-source url: "http://downloads.sourceforge.net/project/expat/expat/2.1.1/expat-2.1.1.tar.bz2?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fexpat%2F&ts=1465411104&use_mirror=heanet",
-       md5: "7380a64a8e3a9d66a9887b01d0d7ea81"
+license "MIT"
+license_file "COPYING"
+skip_transitive_dependency_licensing true
 
-env = {
-  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-}
+# version_list: url=https://github.com/libexpat/libexpat/releases filter=*.tar.gz
+source url: "https://github.com/libexpat/libexpat/releases/download/R_#{version.gsub(".", "_")}/expat-#{version}.tar.gz"
+
+version("2.5.0") { source sha256: "6b902ab103843592be5e99504f846ec109c1abb692e85347587f237a4ffa1033" }
 
 build do
-  command ["./configure",
-           "--prefix=#{install_dir}/embedded"].join(" "), env: env
+  env = with_standard_compiler_flags(with_embedded_path)
 
-  command "make -j #{workers}", env: env
-  command "make install"
+  update_config_guess(target: "conftools")
+
+  # AIX needs two fixes to compile the latest version.
+  #  1. We need to add -lm to link in the proper math declarations
+  #  2. Since we are using xlc to compile, we need to use qvisibility instead of fvisibility
+  #     Refer to https://www.ibm.com/docs/en/xl-c-and-cpp-aix/16.1?topic=descriptions-qvisibility-fvisibility
+  if aix?
+    env["LDFLAGS"] << " -lm"
+    if version <= "2.4.1"
+      patch source: "configure_xlc_visibility.patch", plevel: 1, env: env
+    else
+      patch source: "configure_xlc_visibility_2.4.7.patch", plevel: 1, env: env
+    end
+  end
+
+  command "./configure" \
+          " --disable-static" \
+          " --without-examples" \
+          " --without-tests" \
+          " --prefix=#{install_dir}/embedded", env: env
+
+  make "-j #{workers}", env: env
+  make "install", env: env
 end
