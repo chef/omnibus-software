46a82a4f940ea887bb266d4e29a6ac6cd8e6a256
Poornima <94897054+poorndm@users.noreply.github.com>
2023-05-17 17:46:41 +0530

    Adding server-open-jre version 11.0.19+7 (#1797)
    * Update open jre-11.0.19+7
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    * correction -internal source url
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    ---------
    
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/server-open-jre.rb
+++ config/software/server-open-jre.rb
@@ -17,7 +17,7 @@ dependency "zlib"
 dependency "patchelf"
 
 name "server-open-jre"
-default_version "11.0.18+10"
+default_version "11.0.19+7"
 
 unless _64_bit?
   raise "Server-open-jre can only be installed on x86_64 systems."
@@ -37,6 +37,15 @@ whitelist_file "jre/bin/appletviewer"
 license_warning = "By including the JRE, you accept the terms of AdoptOpenJRE."
 
 # version_list: url=https://github.com/adoptium/temurin11-binaries/releases filter=*.tar.gz
+
+version "11.0.19+7" do
+  source url: "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.19%2B7/OpenJDK11U-jre_x64_linux_hotspot_11.0.19_7.tar.gz",
+  sha256: "32dcf760664f93531594b72ce9226e9216567de5705a23c9ff5a77c797948054",
+  warning: license_warning,
+  unsafe: true
+  internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.gz",
+           authorization: "X-JFrog-Art-Api:#{ENV["ARTIFACTORY_TOKEN"]}"
+end
 
 version "11.0.18+10" do
   source url: "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.18%2B10/OpenJDK11U-jre_x64_linux_hotspot_11.0.18_10.tar.gz",
