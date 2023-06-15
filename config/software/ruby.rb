c7d7e32a10ff6342e29634aef1e99ab7e5e98d16
poorndm <poorndm@progress.com>
2023-06-15 14:38:46 +0530

    add config-patch to 3.0.6
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -142,7 +142,7 @@ build do
     case version
     when "3.0.1"
       patch source: "ruby-3.0.1-configure.patch", plevel: 1, env: patch_env
-    when "3.0.5"
+    when "3.0.5","3.0.6"
       patch source: "ruby-3.0.5-configure.patch", plevel: 1, env: patch_env
     else
       patch source: "ruby-3.0.2-configure.patch", plevel: 1, env: patch_env
