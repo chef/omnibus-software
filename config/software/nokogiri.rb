e0d59991d72122e6cfe9ae006df48f47edd032d4
poorndm <poorndm@progress.com>
2023-07-11 19:58:19 +0530

    set patch_env
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/nokogiri.rb
+++ config/software/nokogiri.rb
@@ -56,11 +56,12 @@ end
 #
 build do
   env = with_standard_compiler_flags(with_embedded_path)
-
   gem_command = [ "install nokogiri" ]
   gem_command << "--version '#{version}'" unless version.nil?
 
   if rhel? && platform_version.satisfies?("< 7")
+    patch_env = env.dup
+    patch_env["PATH"] = "/opt/freeware/bin:#{env["PATH"]}"
     patch source: "nokogiri-on-el6.patch", plevel: 1, env: patch_env
   end
   # windows uses the 'fat' precompiled binaries'
