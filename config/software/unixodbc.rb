ae1693e56b82c903e5cb5d479ad4940e3505abfb
Slavek Kabrda <slavek.kabrda@gmail.com>
2023-08-02 18:08:20 +0200

    Make unixodbc depend on postgresql to fix postgresql build (#467)
    * Make unixodbc depend on postgresql to fix postgresql build
    
    * Update config/software/unixodbc.rb
    
    Co-authored-by: Kylian Serrania <kylian.serrania@datadoghq.com>
    
    ---------
    
    Co-authored-by: Kylian Serrania <kylian.serrania@datadoghq.com>

--- config/software/unixodbc.rb
+++ config/software/unixodbc.rb
@@ -4,6 +4,16 @@ default_version "2.3.9"
 version("2.3.7") { source sha256: "45f169ba1f454a72b8fcbb82abd832630a3bf93baa84731cf2949f449e1e3e77" }
 version("2.3.9") { source sha256: "52833eac3d681c8b0c9a5a65f2ebd745b3a964f208fc748f977e44015a31b207" }
 
+# This is a very ugly hack - due to numerous conflicting macros (in unixodbc
+# installed headers and postgresql headers used during build), it is almost
+# impossible to build postgresql when unixodbc is already installed. For
+# example, we hit https://trac.macports.org/ticket/4559.
+# Therefore we make unixodbc depend on postgresql to ensure postgresql is
+# always built first.
+# We do require postgresql everywhere where we need unixodbc, so there's
+# no harm in doing that.
+dependency "postgresql"
+
 source url: "https://www.unixodbc.org/unixODBC-#{version}.tar.gz"
 
 relative_path "unixODBC-#{version}"
