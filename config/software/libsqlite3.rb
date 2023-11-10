3588337804ae9e66962cbb6c6d19f933986f1a48
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2023-11-10 09:56:28 +0100

    sqlite fixups (#493)
    * sqlite: fix configure flag
    
    * sqlite: use regular make commands
    
    * sqlite: use the same environment other packages already use
    
    * sqlite: depend on zlib
    
    This was the case before we merged the 2 recipes and zlib is already built very
    early in the build so this shouldn't change a thing in term of behavior

--- config/software/libsqlite3.rb
+++ config/software/libsqlite3.rb
@@ -2,17 +2,14 @@ name "libsqlite3"
 default_version "3.43.1"
 
 dependency "config_guess"
+dependency "zlib"
 
 source url: "https://www.sqlite.org/2023/sqlite-autoconf-3430101.tar.gz",
        sha256: "098984eb36a684c90bc01c0eb7bda3273c327cbc3673d7d0bc195028c19fb7b0"
 
 relative_path "sqlite-autoconf-3430100"
 
-env = {
-  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-}
+env = with_standard_compiler_flags(with_embedded_path)
 
 build do
   license "Public-Domain"
@@ -24,10 +21,10 @@ build do
     "--enable-shared",
     "--enable-pic",
     "--disable-editline",
-    "--disable-readling",
+    "--disable-readline",
   ]
   configure(*configure_options, env: env)
-  command "make -j #{workers}", env: { "LD_RUN_PATH" => "#{install_dir}/embedded/lib" }
-  command "make install"
+  make "-j #{workers}", env: env
+  make "install"
   delete "#{install_dir}/embedded/bin/sqlite3"
 end
