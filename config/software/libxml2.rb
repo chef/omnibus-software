5eb839bc22dc133d52b1d28a3904b74956554d1a
poorndm <poorndm@progress.com>
2023-11-16 14:56:20 +0530

    update flag
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/libxml2.rb
+++ config/software/libxml2.rb
@@ -57,7 +57,7 @@ build do
   ]
 
   if windows?
-    configure_command << "--disable-shared"
+    # configure_command << "--disable-shared"
     env["CFLAGS"] << " -DIN_LIBXML"
   end
   update_config_guess
