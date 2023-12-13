280a8a7987044c55b7754f1aa76413a55131c97e
muthuja <muthujabavaji.vempalli@progress.com>
2023-12-13 14:10:31 +0530

    version 4.4.1 added to the version list
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- config/software/make.rb
+++ config/software/make.rb
@@ -22,8 +22,9 @@ license_file "COPYING"
 
 # version_list: url=https://ftp.gnu.org/gnu/make/ filter=*.tar.gz
 
-version("4.4") { source sha256: "581f4d4e872da74b3941c874215898a7d35802f03732bdccee1d4a7979105d18" }
-version("4.3") { source sha256: "e05fdde47c5f7ca45cb697e973894ff4f5d79e13b750ed57d7b66d8defc78e19" }
+version("4.4.1") { source sha256: "dd16fb1d67bfab79a72f5e8390735c49e3e8e70b4945a15ab1f81ddb78658fb3" }
+version("4.4")   { source sha256: "581f4d4e872da74b3941c874215898a7d35802f03732bdccee1d4a7979105d18" }
+version("4.3")   { source sha256: "e05fdde47c5f7ca45cb697e973894ff4f5d79e13b750ed57d7b66d8defc78e19" }
 
 source url: "https://ftp.gnu.org/gnu/make/make-#{version}.tar.gz"
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.gz",
