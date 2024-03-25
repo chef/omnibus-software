d867906a0e67a6732f4683b94188fbf7e67db09d
John McCrae <john.mccrae@progress.com>
2024-03-25 14:38:09 -0700

    Updating resolv.rb to load conditionally
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -166,7 +166,6 @@ build do
     patch source: "ruby-win32_resolv.patch", plevel: 1, env: patch_env
   end
 
-
   # RHEL6 has a base compiler that does not support -fstack-protector-strong, but we
   # cannot build modern ruby on the RHEL6 base compiler, and the configure script
   # determines that it supports that flag and so includes it and then ultimately
