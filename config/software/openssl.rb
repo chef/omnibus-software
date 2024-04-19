e51e7acee4edc096a389594e91b4575ae6937c0e
John McCrae <john.mccrae@progress.com>
2024-04-19 13:46:20 -0700

    Got it! have to adjust FIPS now, moving FIPS support back to the main openssl.rb file for v3 and laster in onnibus-software
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- config/software/openssl.rb
+++ config/software/openssl.rb
@@ -211,7 +211,7 @@ build do
   end
 
   if version.start_with?("3") && fips_mode?
-    make "install openssl fipsinstall", env: env
+    make "install install_fips", env: env
   else
     make "install", env: env
   end
