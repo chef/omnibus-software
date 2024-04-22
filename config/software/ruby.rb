79bc9286dab34e8c0fe7a512cd0dc0d261d8249a
poorndm <poorndm@progress.com>
2024-04-22 22:23:45 +0530

    OpenSSL 3.0.9 on Ruby 3.1
    * only include `openssl-fips` (which is the legacy <3.0 FIPS) if the `version` of `openssl` given to `openssl.rb` is `< 3.0.0` (or _not_ `>= 3.0.0`) [openssl.rb:24](https://github.com/chef/omnibus-software/pull/1903/files#diff-05cbe0a6a9b14d404660e58981b5e95f7ec556e4d6673d9a84fad51efd8dc84eR24)
    * run `make install_fips` and edit the .cnf file locations (and dump provider details) [openssl.rb:212-235](https://github.com/chef/omnibus-software/pull/1903/files#diff-05cbe0a6a9b14d404660e58981b5e95f7ec556e4d6673d9a84fad51efd8dc84eR212-R235)
    * install ruby gem manually with openssl tips [ruby.rb:318-325](https://github.com/chef/omnibus-software/pull/1903/files#diff-8ff31cf08a88774679ed15595ec42f8fea9608671ca50be341953489045e8e25R318-R325)
    * scan directories for the state and versions of ruby and openssl binaries on the system [ruby.rb:366-387](https://github.com/chef/omnibus-software/pull/1903/files#diff-8ff31cf08a88774679ed15595ec42f8fea9608671ca50be341953489045e8e25R366-R387)
    
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -42,22 +42,20 @@ dependency "libyaml"
 dependency "ncurses" if freebsd?
 
 # version_list: url=https://cache.ruby-lang.org/pub/ruby/ filter=*.tar.gz
-version("3.3.1")      { source sha256: "8dc2af2802cc700cd182d5430726388ccf885b3f0a14fcd6a0f21ff249c9aa99" }
-version("3.3.0")      { source sha256: "96518814d9832bece92a85415a819d4893b307db5921ae1f0f751a9a89a56b7d" }
-version("3.2.2")      { source sha256: "96c57558871a6748de5bc9f274e93f4b5aad06cd8f37befa0e8d94e7b8a423bc" }
-version("3.2.0")      { source sha256: "daaa78e1360b2783f98deeceb677ad900f3a36c0ffa6e2b6b19090be77abc272" }
-version("3.1.5")      { source sha256: "3685c51eeee1352c31ea039706d71976f53d00ab6d77312de6aa1abaf5cda2c5" }
-version("3.1.4")      { source sha256: "a3d55879a0dfab1d7141fdf10d22a07dbf8e5cdc4415da1bde06127d5cc3c7b6" }
-version("3.1.3")      { source sha256: "5ea498a35f4cd15875200a52dde42b6eb179e1264e17d78732c3a57cd1c6ab9e" }
-version("3.1.2")      { source sha256: "61843112389f02b735428b53bb64cf988ad9fb81858b8248e22e57336f24a83e" }
-version("3.1.1")      { source sha256: "fe6e4782de97443978ddba8ba4be38d222aa24dc3e3f02a6a8e7701c0eeb619d" }
-
-version("3.0.6")      { source sha256: "6e6cbd490030d7910c0ff20edefab4294dfcd1046f0f8f47f78b597987ac683e" }
-version("3.0.5")      { source sha256: "9afc6380a027a4fe1ae1a3e2eccb6b497b9c5ac0631c12ca56f9b7beb4848776" }
-version("3.0.4")      { source sha256: "70b47c207af04bce9acea262308fb42893d3e244f39a4abc586920a1c723722b" }
-version("3.0.3")      { source sha256: "3586861cb2df56970287f0fd83f274bd92058872d830d15570b36def7f1a92ac" }
-version("3.0.2")      { source sha256: "5085dee0ad9f06996a8acec7ebea4a8735e6fac22f22e2d98c3f2bc3bef7e6f1" }
-version("3.0.1")      { source sha256: "369825db2199f6aeef16b408df6a04ebaddb664fb9af0ec8c686b0ce7ab77727" }
+version("3.3.1") { source sha256: "8dc2af2802cc700cd182d5430726388ccf885b3f0a14fcd6a0f21ff249c9aa99" }
+version("3.3.0") { source sha256: "96518814d9832bece92a85415a819d4893b307db5921ae1f0f751a9a89a56b7d" }
+version("3.2.2") { source sha256: "96c57558871a6748de5bc9f274e93f4b5aad06cd8f37befa0e8d94e7b8a423bc" }
+version("3.2.0") { source sha256: "daaa78e1360b2783f98deeceb677ad900f3a36c0ffa6e2b6b19090be77abc272" }
+version("3.1.4") { source sha256: "a3d55879a0dfab1d7141fdf10d22a07dbf8e5cdc4415da1bde06127d5cc3c7b6" }
+version("3.1.3") { source sha256: "5ea498a35f4cd15875200a52dde42b6eb179e1264e17d78732c3a57cd1c6ab9e" }
+version("3.1.2") { source sha256: "61843112389f02b735428b53bb64cf988ad9fb81858b8248e22e57336f24a83e" }
+version("3.1.1") { source sha256: "fe6e4782de97443978ddba8ba4be38d222aa24dc3e3f02a6a8e7701c0eeb619d" }
+version("3.0.6") { source sha256: "6e6cbd490030d7910c0ff20edefab4294dfcd1046f0f8f47f78b597987ac683e" }
+version("3.0.5") { source sha256: "9afc6380a027a4fe1ae1a3e2eccb6b497b9c5ac0631c12ca56f9b7beb4848776" }
+version("3.0.4") { source sha256: "70b47c207af04bce9acea262308fb42893d3e244f39a4abc586920a1c723722b" }
+version("3.0.3") { source sha256: "3586861cb2df56970287f0fd83f274bd92058872d830d15570b36def7f1a92ac" }
+version("3.0.2") { source sha256: "5085dee0ad9f06996a8acec7ebea4a8735e6fac22f22e2d98c3f2bc3bef7e6f1" }
+version("3.0.1") { source sha256: "369825db2199f6aeef16b408df6a04ebaddb664fb9af0ec8c686b0ce7ab77727" }
 
 source url: "https://cache.ruby-lang.org/pub/ruby/#{version.match(/^(\d+\.\d+)/)[0]}/ruby-#{version}.tar.gz"
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.gz",
@@ -120,7 +118,8 @@ elsif windows?
   end
   env["CPPFLAGS"] = env["CFLAGS"]
   env["CXXFLAGS"] = env["CFLAGS"]
-else # including linux
+else
+  # including linux
   env["CFLAGS"] << " -O3 -g -pipe"
 end
 
@@ -286,6 +285,7 @@ build do
 
   # Remove this if clause once Ruby < 3.1 is not supported in combination with
   # OpenSSL >= 3.0
+  # if (version.satisfies?("< 3.1") || fips_mode?) &&
   if version.satisfies?("< 3.1") &&
       project.overrides[:openssl] &&
       ChefUtils::VersionString.new(project.overrides[:openssl][:version]).satisfies?(">= 3.0")
@@ -302,17 +302,27 @@ build do
   make "-j #{workers}", env: env
   make "-j #{workers} install", env: env
 
+  # set this here because two different clauses might use it
+  openssl_gem_version = project.overrides.dig(:ruby, :openssl_gem) || "3.0.0"
+
   # Remove this if clause once Ruby < 3.1 is not supported in combination with
   # OpenSSL >= 3.0
-  if version.satisfies?("< 3.1") &&
+  if (version.satisfies?("< 3.1") || fips_mode?) &&
       project.overrides[:openssl] &&
       ChefUtils::VersionString.new(project.overrides[:openssl][:version]).satisfies?(">= 3.0")
 
     # use the same version as ruby 3.1.2 version has as default, so that the chef gemfile inclusion of the
     # same openssl gem version is redundant for ruby 3.1[.2] projects
-    openssl_gem_version = project.overrides.dig(:ruby, :openssl_gem) || "3.0.0"
     command "curl https://rubygems.org/downloads/openssl-#{openssl_gem_version}.gem --output openssl-#{openssl_gem_version}.gem"
-    command "#{install_dir}/embedded/bin/gem install openssl-#{openssl_gem_version}.gem --no-document"
+
+    # add OPENSSL_FIPS to the environment _if_ fips is active
+    fips_env = fips_mode? ? env.merge({ "OPENSSL_FIPS" => "1" }) : env
+
+    command "git clone https://github.com/ruby/openssl.git", cwd: "#{install_dir}"
+    command "gem build openssl.gemspec", cwd: "#{install_dir}/openssl"
+    command "gem install openssl-#{openssl_gem_version}.gem --no-document -- --with-openssl-dir=#{install_dir}/embedded", env: fips_env, cwd: "#{install_dir}/openssl"
+
+    command "#{install_dir}/embedded/bin/gem info openssl"
   end
 
   if windows?
@@ -353,4 +363,26 @@ build do
 
   end
 
+  if fips_mode?
+    puts "Validating FIPS_MODE build"
+    if windows?
+      puts "Finding all the rubies installed and checking their fips_mode status"
+      find_command = %{
+        Get-ChildItem c:/opscode -name 'ruby.exe' -recurse | ForEach-Object {
+          & $_ -e "require 'openssl'; puts OpenSSL::OPENSSL_VERSION_NUMBER.to_s(16); puts OpenSSL::OPENSSL_LIBRARY_VERSION; OpenSSL.fips_mode = 1; puts 'FIPS mode successfully activated for Ruby' + RUBY_VERSION"
+        }
+        Write-Output "done looking at rubies"
+      }
+    else
+      find_command = %(
+        find /opt -name 'ruby' | grep 'bin/ruby' | while read ruby; do
+          echo "Checking $ruby"
+          sum $ruby
+          $ruby -v -e "require 'openssl'; puts OpenSSL::OPENSSL_VERSION_NUMBER.to_s(16); puts OpenSSL::OPENSSL_LIBRARY_VERSION; OpenSSL.fips_mode = 1; puts 'FIPS mode successfully activated for Ruby '+ RUBY_VERSION"
+        done
+        echo "done looking at rubies"
+      )
+    end
+    command find_command
+  end
 end
