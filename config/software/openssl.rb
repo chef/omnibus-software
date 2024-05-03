7c2f18ae5cc7ffd7559e22ed383d280a952c7b27
John McCrae <john.mccrae@progress.com>
2024-05-03 15:28:58 -0700

    Correcting the Ruby compile to pull in the correct openssl files
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- config/software/openssl.rb
+++ config/software/openssl.rb
@@ -247,6 +247,11 @@ build do
         copy "#{msys_path}/usr/local/lib64/ossl-modules/#{file}", "#{install_dir}/embedded/bin/#{file}"
       end
 
+      command "require 'base64'
+      cmd = %{Get-ChildItem -path c:\\ -file openssl.so -recurse}
+      encoded_cmd = Base64.strict_encode64(cmd.encode('utf-16le'))
+      find = `powershell.exe -encodedCommand #{encoded_cmd}`"
+
     #   # Needed now that we switched to msys2 and have not figured out how to tell
     #   # it how to statically link yet
     #   dlls = [
