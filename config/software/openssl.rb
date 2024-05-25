4bbfcabba8b16c3a265edc4474825bad857c55c4
John <john.mccrae@progress.com>
2024-05-25 08:48:12 -0700

    Tweaking openssl to match chef-18
    Signed-off-by: John <john.mccrae@progress.com>

--- config/software/openssl.rb
+++ config/software/openssl.rb
@@ -306,7 +306,7 @@ build do
         command "echo '**********************************************'"
         command "ls -al  #{install_dir}/embedded/lib/engines-3"
         command "ls -al #{install_dir}/embedded/lib/ossl-modules"
-        command "echo "$OMNIBUS_FIPS_MODE""
+        command "printenv | more "
         command "sudo find / -type f -name fips.so > file.txt"
         command "cat ./file.txt"
         command "echo '**********************************************'"
