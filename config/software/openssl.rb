0bb20cfd3fc39998adb219f3085057c1c57f7990
John <john.mccrae@progress.com>
2024-06-21 16:26:55 -0700

    Updating freebsd to omit the devcrypto engine which is currently breaking openssl compilation
    Signed-off-by: John <john.mccrae@progress.com>

--- config/software/openssl.rb
+++ config/software/openssl.rb
@@ -161,7 +161,7 @@ build do
 
   if version.start_with? "1.0"
     patch source: "openssl-1.0.1f-do-not-build-docs.patch", env: patch_env
-  elsif (version.start_with? "1.0.2" && freebsd?)
+  elsif version.start_with? "1.0.2"
     patch source: "openssl-1.0.2zi-freebsd-nocryptodev.patch", env: patch_env
   elsif version.start_with? "1.1"
     patch source: "openssl-1.1.0f-do-not-install-docs.patch", env: patch_env
