224568bf8c0ce37f3565abb086844226204f6dec
poorndm <poorndm@progress.com>
2024-07-22 20:50:43 +0530

    modify compiler flags
    Signed-off-by: poorndm <poorndm@progress.com>
    
    Fix syntax
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    remove extra space
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    update for keydb build
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    modified LDFLAG to fix libatomic linking issue
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    update ldflag
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    modify keydb version - 6.3.4
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    set flags to suse
    
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/keydb.rb
+++ config/software/keydb.rb
@@ -34,14 +34,21 @@ relative_path "KeyDB-#{version}"
 
 # version_list: url=https://github.com/Snapchat/KeyDB/archive/refs/tags/ filter=*.tar.gz
 version("6.3.4") { source sha256: "229190b251f921e05aff7b0d2f04b5676c198131e2abbec1e2cfb2e61215e2f3" }
+version("6.3.1") { source sha256: "851b91e14dc3e9c973a1870acdc5f2938ad51a12877e64e7716d9e9ae91ce389" }
 
 build do
-  env = with_standard_compiler_flags(with_embedded_path)
+  env = with_standard_compiler_flags(with_embedded_path).merge(
+    "PREFIX" => "#{install_dir}/embedded"
+  )
   env["CFLAGS"] << " -I#{install_dir}/embedded/include"
   env["LDFLAGS"] << " -L#{install_dir}/embedded/lib"
 
+  if suse?
+    env["CFLAGS"] << " -fno-lto"
+    env["CXXFLAGS"] << " -fno-lto"
+  end
   update_config_guess
 
-  command "make" , env: env
-  command "make install", env: env
+  make "-j #{workers}", env: env
+  make "install", env: env
 end
