8397c223fa8e3b60de96ee3977e5fd5db2923453
Thomas Powell <thomas.powell@progress.com>
2024-07-29 14:31:37 -0400

    ruby 3.4.0-preview1 for non-Windows
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -42,6 +42,7 @@ dependency "libyaml"
 dependency "ncurses" if freebsd?
 
 # version_list: url=https://cache.ruby-lang.org/pub/ruby/ filter=*.tar.gz
+version("3.4.0-preview1") { source sha256: "1a3c322e90cb22e5fba0b5d257bb2be9988affa3867eba7642ed981fdde895bb" }
 version("3.3.1") { source sha256: "8dc2af2802cc700cd182d5430726388ccf885b3f0a14fcd6a0f21ff249c9aa99" }
 version("3.3.0") { source sha256: "96518814d9832bece92a85415a819d4893b307db5921ae1f0f751a9a89a56b7d" }
 version("3.2.2") { source sha256: "96c57558871a6748de5bc9f274e93f4b5aad06cd8f37befa0e8d94e7b8a423bc" }
