c7702ba8cb5fb09ee2a3f58e423397edac07bc12
Thomas Powell <thomas.powell@progress.com>
2024-10-24 17:02:13 -0400

    Print the whole tree
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- config/software/openssl.rb
+++ config/software/openssl.rb
@@ -232,6 +232,6 @@ build do
     command "#{windows? ? "Perl.exe" : ""} ./util/wrap.pl -fips #{install_dir}/embedded/bin/openssl list -provider-path providers -provider fips -providers"
     command "#{install_dir}/embedded/bin/openssl list -providers"
     command "echo '🤔🐴💩 <<< openssl dir >>>'"
-    command "find #{install_dir} -name '*ssl*'"
+    command "find #{install_dir}"
   end
 end
