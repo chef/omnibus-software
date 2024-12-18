8da5839e575a58e2459eacea10882d885ffa8c50
John McCrae <john.mccrae@progress.com>
2024-12-18 10:03:52 -0600

    updating libxslt to get past a CVE
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- config/software/libxslt.rb
+++ config/software/libxslt.rb
@@ -65,7 +65,7 @@ build do
     "--without-crypto",
     "--without-profiler",
     "--without-debugger",
-    "--Wno-deprecated-declarations",
+    "--without-deprecated-declarations",
   ]
 
   configure(*configure_commands, env: env)
