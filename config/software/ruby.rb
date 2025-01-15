5f265d971845889671533213c726afe5655d071e
poorndm <poorndm@progress.com>
2025-01-15 23:23:59 +0530

    modified tag
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -338,7 +338,7 @@ build do
 
     command "git clone https://github.com/ruby/openssl.git", cwd: "#{install_dir}"
     # Checkout the specific tag for version 3.2.0
-    command "cd #{install_dir}/openssl && git checkout tags/3.2.0"
+    command "cd #{install_dir}/openssl && git checkout tags/v3.2.0"
     command "gem build openssl.gemspec", cwd: "#{install_dir}/openssl"
     command "gem install openssl-#{openssl_gem_version}.gem --no-document -- --with-openssl-dir=#{install_dir}/embedded", env: fips_env, cwd: "#{install_dir}/openssl"
 
