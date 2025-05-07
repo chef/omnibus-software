6bc82b772ce311ef45a433d5cf31e2108fb8c9f2
poorndm <poorndm@progress.com>
2025-05-07 18:00:17 +0530

    Add libxcrypt
    Signed-off-by: poorndm <poorndm@progress.com>
    
    Syntax correction
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    update internal-source url
    
    Update Commit sha
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    Fix error
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    Corrected libcrypt Shasum
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    generate config script - Missing in libcrypt root dir
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    Fix syntax error
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    modified the src path
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    File name correction
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    update compiler flag
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    disable enable-hashes
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    disable enable-hashes
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    Update libxcrypt- version 4.4.36
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    Update version to latest
    
    Signed-off-by: poorndm <poorndm@progress.com>

---
+++ config/software/libxcrypt.rb
@@ -0,0 +1,53 @@
+#
+# Copyright:: Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "libxcrypt"
+default_version "4.4.38" # Updated for modern .so.2 implementation
+
+license "LGPL-2.1"
+license_file "COPYING.LIB"
+skip_transitive_dependency_licensing true
+
+version("4.4.38") { source sha256: "80304b9c306ea799327f01d9a7549bdb28317789182631f1b54f4511b4206dd6" }
+version("4.4.36") { source sha256: "e5e1f4caee0a01de2aee26e3138807d6d3ca2b8e67287966d1fefd65e1fd8943" }
+
+source url: "https://github.com/besser82/libxcrypt/releases/download/v#{version}/libxcrypt-#{version}.tar.xz"
+internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.xz",
+                authorization: "X-JFrog-Art-Api:#{ENV["ARTIFACTORY_TOKEN"]}"
+
+relative_path "libxcrypt-#{version}"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  configure_command = [
+    "./configure",
+    "--prefix=#{install_dir}/embedded",
+    "--disable-werror",
+    "--enable-obsolete-api=no",
+    "--disable-static",
+    "--with-pic",
+  ]
+
+  command configure_command.join(" "), env: env
+  make "-j #{workers}", env: env
+  make "install", env: env
+
+  # Post-install verification
+  command "readelf -d #{install_dir}/embedded/lib/libcrypt.so | grep 'SONAME.*libcrypt.so.2'", env: env
+  # command "objdump -T #{install_dir}/embedded/lib/libcrypt.so | grep 'GLIBC_2.2.5.*crypt_r'", env: env
+end
+
