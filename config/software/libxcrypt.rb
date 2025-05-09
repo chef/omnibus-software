69332b6ec30b860f23778a2b4813e8318b1f17a7
poorndm <poorndm@progress.com>
2025-05-09 12:19:58 +0530

    update compiler flag
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/libxcrypt.rb
+++ config/software/libxcrypt.rb
@@ -36,7 +36,7 @@ build do
     "./configure",
     "--prefix=#{install_dir}/embedded",
     "--disable-werror",
-    "--enable-hashes=strong,glibc", # Modern hash algorithms
+    "--enable-hashes=glibc",
     "--enable-obsolete-api=no",     # Disable legacy .so.1 symbols
     "--disable-static",
     "--with-pic",
