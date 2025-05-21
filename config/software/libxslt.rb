0c7ee1397510a4bcf77c916a6b02db9f580f51f2
poorndm <poorndm@progress.com>
2025-05-21 10:26:53 +0530

    add libxslt-1.1.40
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/libxslt.rb
+++ config/software/libxslt.rb
@@ -28,6 +28,7 @@ dependency "config_guess"
 # versions_list: url=https://download.gnome.org/sources/libxslt/1.1/ filter=*.tar.xz
 version("1.1.43") { source sha256: "5a3d6b383ca5afc235b171118e90f5ff6aa27e9fea3303065231a6d403f0183a" }
 version("1.1.42") { source sha256: "85ca62cac0d41fc77d3f6033da9df6fd73d20ea2fc18b0a3609ffb4110e1baeb" }
+version("1.1.40") { source sha256: "194715db023035f65fb566402f2ad2b5eab4c29d541f511305c40b29b1f48d13" }
 version("1.1.39") { source sha256: "2a20ad621148339b0759c4d4e96719362dee64c9a096dbba625ba053846349f0" }
 version("1.1.37") { source sha256: "3a4b27dc8027ccd6146725950336f1ec520928f320f144eb5fa7990ae6123ab4" }
 version("1.1.36") { source sha256: "12848f0a4408f65b530d3962cd9ff670b6ae796191cfeff37522b5772de8dc8e" }
