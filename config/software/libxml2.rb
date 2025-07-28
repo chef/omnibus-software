e204784d682fa07a6f58d2aa263e05d5aa18f046
Sachin <sachin.jain@chef.io>
2025-07-28 16:48:49 +0530

    Add libxml2 2.14.3 version to the version list
    - Added libxml2 version 2.14.3 with SHA256 checksum
    - Updated internal_sources.yml with the new version entry
    - Follows same pattern as other version additions

--- config/software/libxml2.rb
+++ config/software/libxml2.rb
@@ -26,6 +26,7 @@ dependency "liblzma"
 dependency "config_guess"
 
 # version_list: url=https://download.gnome.org/sources/libxml2/ filter=*.tar.xz
+version("2.14.3")  { source sha256: "6de55cacc8c2bc758f2ef6f93c313cb30e4dd5d84ac5d3c7ccbd9344d8cc6833" }
 version("2.14.2")  { source sha256: "353f3c83535d4224a4e5f1e88c90b5d4563ea8fec11f6407df640fd28fc8b8c6" }
 version("2.13.8")  { source sha256: "277294cb33119ab71b2bc81f2f445e9bc9435b893ad15bb2cd2b0e859a0ee84a" }
 version("2.13.5")  { source sha256: "74fc163217a3964257d3be39af943e08861263c4231f9ef5b496b6f6d4c7b2b6" }
