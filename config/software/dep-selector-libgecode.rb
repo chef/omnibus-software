72160ae18393a62ad58fc68c1dd0da01fe5f6909
poorndm <poorndm@progress.com>
2025-09-22 16:25:25 +0530

    modified Flags for ubuntu
    Signed-off-by: poorndm <poorndm@progress.com>

--- config/software/dep-selector-libgecode.rb
+++ config/software/dep-selector-libgecode.rb
@@ -1,48 +1,31 @@
-#
-# Copyright 2014-2018 Chef Software, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# expeditor/ignore: deprecated 2021-04
-
 name "dep-selector-libgecode"
 default_version "1.3.1"
-
 license "Apache-2.0"
 license_file "https://raw.githubusercontent.com/chef/dep-selector-libgecode/master/LICENSE"
-# dep-selector-libgecode does not have any dependencies. We only install it from
-# rubygems here.
 skip_transitive_dependency_licensing true
-
 dependency "ruby"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  # On some RHEL-based systems, the default GCC that's installed is 4.1. We
-  # need to use 4.4, which is provided by the gcc44 and gcc44-c++ packages.
-  # These do not use the gcc binaries so we set the flags to point to the
-  # correct version here.
-  if File.exist?("/usr/bin/gcc44")
-    env["CC"]  = "gcc44"
-    env["CXX"] = "g++44"
+  # On some RHEL-based systems, the default GCC that's installed is 4.1.
+  # Ubuntu 24.04 has GCC 13 which may create compatibility issues with some libraries.
+  # Optionally fallback to use system Gecode to avoid building vendored source.
+  if ENV['USE_SYSTEM_GECODE'] == '1'
+    gem "install dep-selector-libgecode --version '#{version}' --no-document -- --use-system-gecode", env: env
+  else
+    # For Ubuntu 24.04, explicitly set newer flags to ensure compatibility
+    env["CFLAGS"] = "#{env["CFLAGS"]} -fcommon -D_GLIBCXX_USE_CXX11_ABI=0" if `lsb_release -rs`.chomp == '24.04'
+    
+    # Use gcc44/g++44 if present (mostly for older RHEL)
+    if File.exist?("/usr/bin/gcc44")
+      env["CC"]  = "gcc44"
+      env["CXX"] = "g++44"
+    end
+    
+    env["ARFLAGS"] = "rv #{env["ARFLAGS"]}" if env["ARFLAGS"]
+    env["PROG_TAR"] = "bsdtar" if windows?
+    
+    gem "install dep-selector-libgecode --version '#{version}' --no-document", env: env
   end
-
-  # Ruby DevKit ships with BSD Tar
-  env["PROG_TAR"] = "bsdtar" if windows?
-  env["ARFLAGS"] = "rv #{env["ARFLAGS"]}" if env["ARFLAGS"]
-
-  gem "install dep-selector-libgecode" \
-      " --version '#{version}'" \
-      "  --no-document", env: env
 end
