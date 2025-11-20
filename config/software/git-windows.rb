53275ad3635826491e6d9db36f51773bec9e98c1
Sachin <sachin.jain@chef.io>
2025-11-20 20:01:14 +0530

    adds support for Git for Windows version 2.48.1 to omnibus-software.
    Signed-off-by: Sachin <sachin.jain@chef.io>

--- config/software/git-windows.rb
+++ config/software/git-windows.rb
@@ -39,6 +39,7 @@ internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/PortableGit-#{versi
 
 if windows_arch_i386?
   # version_list: url=https://github.com/git-for-windows/git/releases filter=PortableGit-*-32-bit.7z.exe
+  version("2.48.1") { source sha256: "63ddbc7ea11ea8a1375f39f45d38f928dbec564360ad12dd11d0d649474063a0" }
   version("2.47.0") { source sha256: "b2def285b907ce1d47abd2df8df83df629b768defe08c1fcd4ad91582fc6606b" }
   version("2.41.0") { source sha256: "e1360e94cb292862fb023018578a1029022a09278b160f7264c6dc444f65c9ca" }
   version("2.33.0") { source sha256: "c3b6f1a8f8c1b5be2175b7190d35926dce07a58294780291326a437ef0694676" }
@@ -49,6 +50,7 @@ if windows_arch_i386?
   version("2.27.0") { source sha256: "8cbe1e3b57eb9d02e92cff12089454f2cf090c02958080d62e199ef8764542d3" }
 else
   # version_list: url=https://github.com/git-for-windows/git/releases filter=PortableGit-*-64-bit.7z.exe
+  version("2.48.1") { source sha256: "a4335111b3363871cac632be93d7466154d8eb08782ff55103866b67d6722257" }
   version("2.47.0") { source sha256: "0b7fcd76902ebde5b4c00ebae597d7f65dff8c3dd0ae59f5059e1aaa3adace87" }
   version("2.41.0") { source sha256: "fcbaeffd24fdf435a1f7844825253509136377915e6720aa66aa256ec1f83c30" }
   version("2.33.0") { source sha256: "12c10fad2c2db17d9867dbbacff1adc8be50868b793a73d451c2b878914bb32d" }
